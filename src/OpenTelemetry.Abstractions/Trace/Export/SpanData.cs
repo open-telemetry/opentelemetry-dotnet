357d83c39082111cfd35c0af808c4ace1ed67ef6
Tim Wegner <twegner@microsoft.com>
2019-05-21 09:33:57 -0700

    Removing unecessary/unused code

--- src/OpenTelemetry.Abstractions/Trace/Export/SpanData.cs
+++ src/OpenTelemetry.Abstractions/Trace/Export/SpanData.cs
@@ -123,12 +123,6 @@ namespace OpenTelemetry.Trace.Export
                 events = TimedEvents<IEvent>.Create(new List<ITimedEvent<IEvent>>(), 0);
             }
 
-            var eventsList = new List<ITimedEvent<IEvent>>();
-            foreach (ITimedEvent<IEvent> timedEvent in events.Events)
-            {
-                eventsList.Add(timedEvent);
-            }
-
             return new SpanData(
                 context,
                 parentSpanId,
