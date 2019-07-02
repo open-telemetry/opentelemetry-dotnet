42c307a382458cd0ae8f6cac0dc50bd7cb7eddd0
Michael (Enzian) Ingold <github@michaelingold.ch>
2019-07-02 11:44:49 +0200

    SA1517	Code should not contain blank lines at start of file

--- src/OpenTelemetry.Exporter.Stackdriver/StackdriverExporter.cs
+++ src/OpenTelemetry.Exporter.Stackdriver/StackdriverExporter.cs
@@ -1,5 +1,4 @@
-﻿
-// <copyright file="StackdriverExporter.cs" company="OpenTelemetry Authors">
+﻿// <copyright file="StackdriverExporter.cs" company="OpenTelemetry Authors">
 // Copyright 2018, OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
