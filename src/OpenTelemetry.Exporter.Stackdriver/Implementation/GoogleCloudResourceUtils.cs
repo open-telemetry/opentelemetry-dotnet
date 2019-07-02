977f574549054fdb3668d21f867cf112471eacf6
Michael (Enzian) Ingold <github@michaelingold.ch>
2019-07-02 12:35:14 +0200

    SA1638	File header file name documentation should match file name

--- src/OpenTelemetry.Exporter.Stackdriver/Implementation/GoogleCloudResourceUtils.cs
+++ src/OpenTelemetry.Exporter.Stackdriver/Implementation/GoogleCloudResourceUtils.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="ApplicationInsightsExporter.cs" company="OpenTelemetry Authors">
+﻿// <copyright file="GoogleCloudResourceUtils.cs" company="OpenTelemetry Authors">
 // Copyright 2018, OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
