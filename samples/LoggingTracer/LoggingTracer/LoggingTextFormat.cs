d053e7506e7fa6e3ca4a9e90f6672d7f3929d08b
Christoph Neumüller <c.neumueller@gmail.com>
2019-07-23 18:42:55 +0200

    Add LoggingTracer sample (#92)
    * move and rename "Samples" project to "samples\Exporters"
    
    * add "LoggingTracer" as an example of an API implementation
    
    * add readme to solution file
    
    * remove Samples project from solution
    
    * fix scoping
    
    * add "LoggingTracer" as an example of an API implementation
    
    * add readme to solution file
    
    * fix scoping
    
    * changed back to 2.1. was an accidental change.
    
    * added "Demo" to project names
    
    * adapted LoggingTracer to API changes. stripped down asp.net core example to bare minimum.
    
    * cleaned up LoggingTracer sample. not more NotImplementedExceptions, adhere to code style better (usings), don't log getters
    
    * consistency fixes
    
    * made console sample async, code style
    
    * adapt to API changes

---
+++ samples/LoggingTracer/LoggingTracer/LoggingTextFormat.cs
@@ -0,0 +1,23 @@
+﻿namespace LoggingTracer
+{
+    using System;
+    using System.Collections.Generic;
+    using OpenTelemetry.Context.Propagation;
+    using OpenTelemetry.Trace;
+
+    public sealed class LoggingTextFormat : ITextFormat
+    {
+        public ISet<string> Fields => null;
+
+        public SpanContext Extract<T>(T carrier, Func<T, string, IEnumerable<string>> getter)
+        {
+            Logger.Log($"LoggingTextFormat.Extract(...)");
+            return SpanContext.Blank;
+        }
+
+        public void Inject<T>(SpanContext spanContext, T carrier, Action<T, string, string> setter)
+        {
+            Logger.Log($"LoggingTextFormat.Inject({spanContext}, ...)");
+        }
+    }
+}
