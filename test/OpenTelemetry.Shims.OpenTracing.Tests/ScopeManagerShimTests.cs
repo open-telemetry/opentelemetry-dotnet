4ce8cc04c4fc9659a46f46d76a752f47f56be588
Peter Wiese <petercwiese@gmail.com>
2019-08-29 14:28:05 -0700

    Initial implementation of the OpenTracing bridge.
    This is, where possible, a direct port of the current Java shim. There are some holes/TODOS. Notably:
    
    -Missing the Binary injection/extraction of the span context. This doesn't exist with the current implementation of OpenTracing yet.
    -Missing explicit timestamps for calls to OpenTracing ISpan::Log.
    -Missing explicit timestamps for calls to OpenTracing ISpan::Finish
    -Various calls to set/get baggage are not implemented. Requires OpenTelemetry DistributedContext to be finalized.
    
    Other oddities worth nothing:
    
    -The SpanBuilderShim has some conditional code for calling OpenTel SetCreateChild(true|false)
    -The ScopeManagerShim is actively tracking scopes returned by calls to OpenTel WithSpan and associated those with the OpenTel ISpan used when calling WithSpan.
    
    I tested this shim against an ASP.NET Core 3 Preview 8 service wired up with OpenTracing netcore and grpc collectors and exporting to Jaeger backend.
    
    The included tests are unit tests only; no E2E tests.

---
+++ test/OpenTelemetry.Shims.OpenTracing.Tests/ScopeManagerShimTests.cs
@@ -0,0 +1,94 @@
+﻿// <copyright file="ScopeManagerShimTests.cs" company="OpenTelemetry Authors">
+// Copyright 2018, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+namespace OpenTelemetry.Shims.OpenTracing.Tests
+{
+    using System;
+    using Moq;
+    using OpenTelemetry.Trace;
+    using Xunit;
+
+    public class ScopeManagerShimTests
+    {
+        [Fact]
+        public void CtorArgumentValidation()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ScopeManagerShim(null));
+        }
+
+        [Fact]
+        public void Active_IsNull()
+        {
+            var tracerMock = new Mock<ITracer>();
+            tracerMock.Setup(x => x.CurrentSpan).Returns(Trace.BlankSpan.Instance);
+
+            var shim = new ScopeManagerShim(tracerMock.Object);
+
+            Assert.Null(shim.Active);
+        }
+
+        [Fact]
+        public void Active_IsNotNull()
+        {
+            var tracerMock = new Mock<ITracer>();
+            var shim = new ScopeManagerShim(tracerMock.Object);
+            var openTracingSpan = new SpanShim(Defaults.GetOpenTelemetrySpanMock());
+            var scopeMock = new Mock<Context.IScope>();
+
+            tracerMock.Setup(x => x.WithSpan(openTracingSpan.Span)).Returns(scopeMock.Object);
+            tracerMock.Setup(x => x.CurrentSpan).Returns(openTracingSpan.Span);
+
+            var scope = shim.Activate(openTracingSpan, true);
+            Assert.NotNull(scope);
+
+            var activeScope = shim.Active;
+            Assert.Equal(scope, activeScope);
+        }
+
+        [Fact]
+        public void Activate_SpanMustBeShim()
+        {
+            var tracerMock = new Mock<ITracer>();
+            var shim = new ScopeManagerShim(tracerMock.Object);
+
+            Assert.Throws<ArgumentException>(() => shim.Activate(new Mock<global::OpenTracing.ISpan>().Object, true));
+        }
+
+        [Fact]
+        public void Activate()
+        {
+            var tracerMock = new Mock<ITracer>();
+            var shim = new ScopeManagerShim(tracerMock.Object);
+            var scopeMock = new Mock<Context.IScope>();
+            var spanShim = new SpanShim(Defaults.GetOpenTelemetryMockSpan().Object);
+
+            tracerMock.Setup(x => x.WithSpan(spanShim.Span)).Returns(scopeMock.Object);
+
+            using (shim.Activate(spanShim, true))
+            {
+#if DEBUG
+                Assert.Equal(1, shim.SpanScopeTableCount);
+#endif
+            }
+
+#if DEBUG
+            Assert.Equal(0, shim.SpanScopeTableCount);
+#endif
+            tracerMock.Verify(x => x.WithSpan(spanShim.Span), Times.Once);
+            scopeMock.Verify(x => x.Dispose(), Times.Once);
+        }
+    }
+}
