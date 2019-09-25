3089590882fb475b00209e41c3c4c0adfe61b83e
Liudmila Molkova <lmolkova@microsoft.com>
2019-09-24 19:17:04 -0700

    up

--- test/OpenTelemetry.Tests/Impl/Trace/Export/BatchingSpanProcessorTests.cs
+++ test/OpenTelemetry.Tests/Impl/Trace/Export/BatchingSpanProcessorTests.cs
@@ -23,7 +23,7 @@ namespace OpenTelemetry.Trace.Export.Test
     using System.Threading;
     using System.Threading.Tasks;
     using OpenTelemetry.Testing.Export;
-    using OpenTelemetry.Abstractions.Utils;
+    using OpenTelemetry.Utils;
     using OpenTelemetry.Trace;
     using OpenTelemetry.Trace.Config;
     using OpenTelemetry.Trace.Export;
