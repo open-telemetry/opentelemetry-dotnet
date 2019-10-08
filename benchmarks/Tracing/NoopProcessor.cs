3370812445f8cbdbe931c5d0423327ef558dc2c8
Liudmila Molkova <lmolkova@microsoft.com>
2019-10-07 19:41:54 -0700

    Basic benchmarks

---
+++ benchmarks/Tracing/NoopProcessor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Trace.Export;
+
+namespace Benchmarks.Tracing
+{
+    internal class NoopProcessor : SpanProcessor
+    {
+        public NoopProcessor() : base(new NoopExporter())
+        {
+        }
+
+        public override void OnStart(Span span)
+        {
+        }
+
+        public override void OnEnd(Span span)
+        {
+        }
+
+        public override Task ShutdownAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        private class NoopExporter : SpanExporter
+        {
+            public override Task<ExportResult> ExportAsync(IEnumerable<Span> batch, CancellationToken cancellationToken)
+            {
+                return Task.FromResult(ExportResult.Success);
+            }
+
+            public override Task ShutdownAsync(CancellationToken cancellationToken)
+            {
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
---
+++ benchmarks/Tracing/SpanCreationScenarios.cs
@@ -0,0 +1,37 @@
+﻿using OpenTelemetry.Trace;
+
+namespace BenchmarkSdk.Tracing
+{
+    internal class SpanCreationScenarios
+    {
+        public static ISpan CreateSpan(ITracer tracer)
+        {
+            var span = tracer.SpanBuilder("span").StartSpan();
+            span.End();
+            return span;
+        }
+
+        public static ISpan CreateSpan_Attributes(ITracer tracer)
+        {
+            var span = tracer.SpanBuilder("span")
+                .StartSpan();
+            span.SetAttribute("attribute1", "1");
+            span.SetAttribute("attribute2", 2);
+            span.SetAttribute("attribute3", 3.0);
+            span.SetAttribute("attribute4", false);
+            span.End();
+            return span;
+        }
+
+        public static ISpan CreateSpan_Propagate(ITracer tracer)
+        {
+            var span = tracer.SpanBuilder("span")
+                .StartSpan();
+            using (tracer.WithSpan(span))
+            {
+
+            }
+            return span;
+        }
+    }
+}
