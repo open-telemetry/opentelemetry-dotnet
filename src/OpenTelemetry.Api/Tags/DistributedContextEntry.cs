cfbe4415cbcd7e353057aee03c872779bdecc78f
Sergey Kanzhelev <S.Kanzhelev@live.com>
2019-10-30 12:47:47 -0700

    Update src/OpenTelemetry.Api/Tags/DistributedContextEntry.cs
    Co-Authored-By: Bruno Garcia <bruno@brunogarcia.com>

--- src/OpenTelemetry.Api/Tags/DistributedContextEntry.cs
+++ src/OpenTelemetry.Api/Tags/DistributedContextEntry.cs
@@ -35,7 +35,7 @@ namespace OpenTelemetry.Tags
         /// <summary>
         /// Initializes a new instance of the <see cref="DistributedContextEntry"/> class with the key and value.
         /// </summary>
-        /// <param name="key">Key name for the tag.</param>
+        /// <param name="key">Key name for the entry.</param>
         /// <param name="value">Value associated with the key name.</param>
         /// <param name="metadata">Entry metadata.</param>
         public DistributedContextEntry(string key, string value, in EntryMetadata metadata)
