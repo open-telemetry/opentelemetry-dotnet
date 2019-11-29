64e76a1824001c3c294ac19a0a094c650dcd9977
Oleksii Valuiskyi <alex.valuiskyi@gmail.com>
2019-11-29 08:52:36 +0200

    fix constants

--- src/OpenTelemetry.Api/Trace/SpanAttributeConstants.cs
+++ src/OpenTelemetry.Api/Trace/SpanAttributeConstants.cs
@@ -27,10 +27,10 @@ namespace OpenTelemetry.Trace
         public static readonly string HttpHostKey = "http.host";
         public static readonly string HttpUrlKey = "http.url";
         public static readonly string HttpRouteKey = "http.route";
+        public static readonly string HttpFlavorKey = "http.flavor";
 
         public static readonly string DatabaseType = "db.type";
         public static readonly string DatabaseInstance = "db.instance";
         public static readonly string DatabaseStatement = "db.statement";
-        public static readonly string HttpFlavorKey = "http.flavor";
     }
 }
