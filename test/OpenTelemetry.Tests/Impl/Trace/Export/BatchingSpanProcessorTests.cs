d40d38f1f25fa2df6f1f7631a8e6b6ab4bfbd6c6
Liudmila Molkova <lmolkova@microsoft.com>
2019-12-03 17:22:19 -0800

    Fixes ProcessorDoesNotBlockOnExporter

--- test/OpenTelemetry.Tests/Impl/Trace/Export/BatchingSpanProcessorTests.cs
+++ test/OpenTelemetry.Tests/Impl/Trace/Export/BatchingSpanProcessorTests.cs
@@ -223,8 +223,9 @@ namespace OpenTelemetry.Trace.Export.Test
 
         [Fact]
         public void ProcessorDoesNotBlockOnExporter()
-        {
-            var spanExporter = new TestExporter(_ => Thread.Sleep(500));
+        { 
+            var resetEvent = new ManualResetEvent(false);
+            var spanExporter = new TestExporter(_ => resetEvent.WaitOne(TimeSpan.FromSeconds(10)));
             using (var factory = TracerFactory.Create(b => b
                 .AddProcessorPipeline(p => p
                     .SetExporter(spanExporter)
@@ -241,6 +242,8 @@ namespace OpenTelemetry.Trace.Export.Test
                 sw.Stop();
 
                 Assert.InRange(sw.Elapsed, TimeSpan.Zero, TimeSpan.FromMilliseconds(100));
+
+                resetEvent.Set();
 
                 var exported = WaitForSpans(spanExporter, 1, DefaultTimeout);
 
