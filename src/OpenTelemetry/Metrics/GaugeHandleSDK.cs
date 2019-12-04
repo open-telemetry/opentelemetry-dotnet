b42af6e5ad269abb0ff17fe690d0bed124249cea
Cijo Thomas <cithomas@microsoft.com>
2019-12-03 16:48:32 -0800

    add gaugesdk

---
+++ src/OpenTelemetry/Metrics/GaugeHandleSDK.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="GaugeHandleSDK.cs" company="OpenTelemetry Authors">
+// Copyright 2018, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using OpenTelemetry.Context;
+using OpenTelemetry.Trace;
+
+namespace OpenTelemetry.Metrics
+{
+    public class GaugeHandleSDK<T> : GaugeHandle<T>
+        where T : struct
+    {
+        private readonly GaugeAggregator<T> gaugeAggregator = new GaugeAggregator<T>();
+
+        internal GaugeHandleSDK()
+        {
+            if (typeof(T) != typeof(long) && typeof(T) != typeof(double))
+            {
+                throw new Exception("Invalid Type");
+            }
+        }
+
+        public override void Set(in SpanContext context, T value)
+        {
+            this.gaugeAggregator.Update(value);
+        }
+
+        public override void Set(in DistributedContext context, T value)
+        {
+            this.gaugeAggregator.Update(value);
+        }
+
+        internal GaugeAggregator<T> GetSumAggregator()
+        {
+            return this.gaugeAggregator;
+        }
+    }
+}
---
+++ src/OpenTelemetry/Metrics/GaugeSDK.cs
@@ -0,0 +1,64 @@
+﻿// <copyright file="GaugeSDK.cs" company="OpenTelemetry Authors">
+// Copyright 2018, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace OpenTelemetry.Metrics
+{
+    public class GaugeSDK<T> : Gauge<T>
+        where T : struct
+    {        
+        private readonly IDictionary<LabelSet, GaugeHandleSDK<T>> gaugeHandles = new ConcurrentDictionary<LabelSet, GaugeHandleSDK<T>>();
+        private string metricName;
+
+        public GaugeSDK()
+        {
+            if (typeof(T) != typeof(long) && typeof(T) != typeof(double))
+            {
+                throw new Exception("Invalid Type");
+            }
+        }
+
+        public GaugeSDK(string name) : this()
+        {
+            this.metricName = name;
+        }
+
+        public override GaugeHandle<T> GetHandle(LabelSet labelset)
+        {
+            if (!this.gaugeHandles.TryGetValue(labelset, out var handle))
+            {
+                handle = new GaugeHandleSDK<T>();
+
+                this.gaugeHandles.Add(labelset, handle);
+            }
+
+            return handle;
+        }
+
+        public override GaugeHandle<T> GetHandle(IEnumerable<KeyValuePair<string, string>> labels)
+        {
+            return this.GetHandle(new LabelSet(labels));
+        }
+
+        internal IDictionary<LabelSet, GaugeHandleSDK<T>> GetAllHandles()
+        {
+            return this.gaugeHandles;
+        }
+    }
+}
