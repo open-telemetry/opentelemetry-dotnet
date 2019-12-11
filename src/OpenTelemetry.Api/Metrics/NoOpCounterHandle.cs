d9c0e51155e8de42a36f80891f06a34d35b1f046
Cijo Thomas <cithomas@microsoft.com>
2019-12-11 13:43:02 -0800

    NoOp meters (#393)
    * Added NoOp Meter and a basic test for the same. This should eventually be replaced with similar approach used in Tracer.
    
    * minor

---
+++ src/OpenTelemetry.Api/Metrics/NoOpCounterHandle.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="NoOpCounterHandle.cs" company="OpenTelemetry Authors">
+// Copyright 2018, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using OpenTelemetry.Context;
+using OpenTelemetry.Trace;
+
+namespace OpenTelemetry.Metrics
+{
+    /// <summary>
+    /// No-Op handle.
+    /// </summary>
+    /// <typeparam name="T">The type of counter. Only long and double are supported now.</typeparam>
+    public sealed class NoOpCounterHandle<T> : CounterHandle<T>
+        where T : struct
+    {
+        /// <summary>
+        /// No op counter handle instance.
+        /// </summary>
+        public static readonly NoOpCounterHandle<T> Instance = new NoOpCounterHandle<T>();
+
+        /// <inheritdoc/>
+        public override void Add(in SpanContext context, T value)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override void Add(in DistributedContext context, T value)
+        {
+        }
+    }
+}
---
+++ src/OpenTelemetry.Api/Metrics/NoOpGauge.cs
@@ -0,0 +1,47 @@
+﻿// <copyright file="NoOpGauge.cs" company="OpenTelemetry Authors">
+// Copyright 2018, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Collections.Generic;
+using OpenTelemetry.Context;
+using OpenTelemetry.Trace;
+
+namespace OpenTelemetry.Metrics
+{
+    /// <summary>
+    /// A no-op gauge instrument.
+    /// </summary>
+    /// <typeparam name="T">The type of gauge. Only long and double are supported now.</typeparam>
+    public sealed class NoOpGauge<T> : Gauge<T>
+        where T : struct
+    {
+        /// <summary>
+        /// No op gauge instance.
+        /// </summary>
+        public static readonly NoOpGauge<T> Instance = new NoOpGauge<T>();
+
+        /// <inheritdoc/>
+        public override GaugeHandle<T> GetHandle(LabelSet labelset)
+        {
+            return NoOpGaugeHandle<T>.Instance;
+        }
+
+        /// <inheritdoc/>
+        public override GaugeHandle<T> GetHandle(IEnumerable<KeyValuePair<string, string>> labels)
+        {
+            return NoOpGaugeHandle<T>.Instance;
+        }
+    }
+}
