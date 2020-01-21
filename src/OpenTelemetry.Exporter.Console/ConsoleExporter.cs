bd75ae6bb8d221cd79cc0e85b16b1bee50f9e783
Mike Goldsmith <goldsmith.mike@gmail.com>
2020-01-21 17:01:53 +0000

    Add simple console exporter (#439)
    * add generic logger exporter
    
    * remove string builder entry in ExportAsync
    
    * use logger spans
    
    * add null checking for attriute and events
    
    * rename LoggingExporter to LoggerExporter
    
    * refactor LoggerExporter into ConsoleExporter
    
    reduces potential risk of depending on ILogger and having to maintain / use incorrectly. This exporter is designed during learning / debug scenarios.
    
    * add experimental to package tags
    
    * rename LoggingOptions to ConsoleOptions
    
    * refactor ConsoleExporter to use System.Text.JsonSerializer
    
    - increases minimum framework version to `net461`
    
    * add readme to ConsoleExporter
    
    * update tracer name to be console-test
    
    * write each span JSOM directly to Console instad of using StringBuilder
    
    * update ConsoleExporter to use SpanData
    
    Co-authored-by: Sergey Kanzhelev <S.Kanzhelev@live.com>

---
+++ src/OpenTelemetry.Exporter.Console/ConsoleExporter.cs
@@ -0,0 +1,56 @@
+﻿// <copyright file="ConsoleExporter.cs" company="OpenTelemetry Authors">
+// Copyright 2018, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Trace.Export;
+
+namespace OpenTelemetry.Exporter.Console
+{
+    public class ConsoleExporter : SpanExporter
+    {
+        private readonly JsonSerializerOptions serializerOptions;
+
+        public ConsoleExporter(ConsoleExporterOptions options)
+        {
+            this.serializerOptions = new JsonSerializerOptions
+            {
+                WriteIndented = options.Pretty,
+            };
+
+            this.serializerOptions.Converters.Add(new JsonStringEnumConverter());
+        }
+
+        public override Task<ExportResult> ExportAsync(IEnumerable<SpanData> batch, CancellationToken cancellationToken)
+        {
+            foreach (var span in batch)
+            {
+                System.Console.WriteLine(JsonSerializer.Serialize(span, this.serializerOptions));
+            }
+
+            return Task.FromResult(ExportResult.Success);
+        }
+
+        public override Task ShutdownAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
