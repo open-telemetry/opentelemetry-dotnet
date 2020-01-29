63d9bf145cb5ae161a0c649492d0255b6e5ed9ef
SychevIgor <SychevIgor@users.noreply.github.com>
2020-01-29 23:18:16 +0300

    Asp.net core based sample of ApplicationInsights export (#433)
    * Asp.net core based sample of ApplicationInsights export
    
    * links to samples
    
    * update links
    
    * fixed solution
    
    * fix merge issues
    
    Co-authored-by: Sergey Kanzhelev <S.Kanzhelev@live.com>

---
+++ samples/Exporters/Web/Startup.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using OpenTelemetry.Trace.Configuration;
+
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                if (File.Exists(xmlPath))
+                {
+                    c.IncludeXmlComments(xmlPath);
+                }
+            });
+
+            services.AddOpenTelemetry((sp, builder) =>
+            {
+                builder
+                    //.SetSampler(Samplers.AlwaysSample)
+                    .UseApplicationInsights(telemetryConfiguration =>
+                    {
+                        var instrumentationKey = this.Configuration.GetValue<string>("ApplicationInsights:InstrumentationKey");
+                        telemetryConfiguration.InstrumentationKey = instrumentationKey;
+                    })
+                    .AddRequestCollector()
+                    .AddDependencyCollector();
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
