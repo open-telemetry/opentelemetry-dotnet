c1d72e6927b6bec86fc8018ed2a96719cf707a7d
Mikel Blanchard <mblanchard@macrosssoftware.com>
2020-03-17 13:37:01 -0700

    Add Sql Collector (Part 2) (#536)
    * Picking up where alexvaluyskiy left off with the SqlClientCollector. Worked on PR feedback. Added unit tests.
    
    * Updated README.
    
    * Fixed inconsistent comments.
    
    * Code review.
    
    * Code review #2.
    
    * More code review.
    
    * Code review feedback.
    
    Co-authored-by: Sergey Kanzhelev <S.Kanzhelev@live.com>

---
+++ src/OpenTelemetry.Collector.Dependencies/SqlClientCollectorOptions.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="SqlClientCollectorOptions.cs" company="OpenTelemetry Authors">
+// Copyright 2018, OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+using System.Data;
+
+namespace OpenTelemetry.Collector.Dependencies
+{
+    /// <summary>
+    /// Options for <see cref="SqlClientCollector"/>.
+    /// </summary>
+    public class SqlClientCollectorOptions
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SqlClientCollectorOptions"/> class.
+        /// </summary>
+        public SqlClientCollectorOptions()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether or not the <see cref="SqlClientCollector"/> should capture the names of <see cref="CommandType.StoredProcedure"/> commands. Default value: True.
+        /// </summary>
+        public bool CaptureStoredProcedureCommandName { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether or not the <see cref="SqlClientCollector"/> should capture the text of <see cref="CommandType.Text"/> commands. Default value: False.
+        /// </summary>
+        public bool CaptureTextCommandContent { get; set; }
+    }
+}
