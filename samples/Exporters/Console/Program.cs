d2f3972e71ec158540cee2b3559dc9c8b08c8b8c
Mike Goldsmth <goldsmith.mike@gmail.com>
2020-05-06 20:51:33 +0100

    remove stackdriver exporter

--- samples/Exporters/Console/Program.cs
+++ samples/Exporters/Console/Program.cs
@@ -37,14 +37,13 @@ namespace Samples
         /// <param name="args">Arguments from command line.</param>
         public static void Main(string[] args)
         {
-            Parser.Default.ParseArguments<JaegerOptions, ZipkinOptions, PrometheusOptions, HttpClientOptions, StackdriverOptions, LightStepOptions, ZPagesOptions, ConsoleOptions, OtlpOptions>(args)
+            Parser.Default.ParseArguments<JaegerOptions, ZipkinOptions, PrometheusOptions, HttpClientOptions, LightStepOptions, ZPagesOptions, ConsoleOptions, OtlpOptions>(args)
                 .MapResult(
                     (JaegerOptions options) => TestJaeger.Run(options.Host, options.Port),
                     (ZipkinOptions options) => TestZipkin.Run(options.Uri),
                     (PrometheusOptions options) => TestPrometheus.RunAsync(options.Port, options.PushIntervalInSecs, options.DurationInMins),
                     (HttpClientOptions options) => TestHttpClient.Run(),
                     (RedisOptions options) => TestRedis.Run(options.Uri),
-                    (StackdriverOptions options) => TestStackdriver.Run(options.ProjectId),
                     (LightStepOptions options) => TestLightstep.Run(options.AccessToken),
                     (ZPagesOptions options) => TestZPages.Run(),
                     (ConsoleOptions options) => TestConsole.Run(options),
@@ -61,13 +60,6 @@ namespace Samples
     {
         [Option('t', "accessToken", HelpText = "Please specify the access token for your LightStep project", Required = true)]
         public string AccessToken { get; set; }
-    }
-
-    [Verb("stackdriver", HelpText = "Specify the options required to test Stackdriver exporter", Hidden = false)]
-    internal class StackdriverOptions
-    {
-        [Option('p', "projectId", HelpText = "Please specify the projectId of your GCP project", Required = true)]
-        public string ProjectId { get; set; }
     }
 
     [Verb("jaeger", HelpText = "Specify the options required to test Jaeger exporter")]
