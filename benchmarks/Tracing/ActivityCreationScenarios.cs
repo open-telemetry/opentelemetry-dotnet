fa69fb0a2b5863f31ad9f604749f2b7b96fbdbb6
Cijo Thomas <cithomas@microsoft.com>
2020-06-12 15:16:58 -0700

    Add basic benchmark project for Activity based SDK. (#725)
    * Add basic benchmark project for Activity based SDK.
    
    * readme separate

---
+++ benchmarks/Tracing/ActivityCreationScenarios.cs
@@ -0,0 +1,54 @@
+﻿// <copyright file="ActivityCreationScenarios.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Diagnostics;
+using OpenTelemetry.Trace;
+
+namespace Benchmarks.Tracing
+{
+    internal class ActivityCreationScenarios
+    {
+        public static Activity CreateActivity(ActivitySource source)
+        {
+            var activity = source.StartActivity("name");
+            activity?.Stop();
+            return activity;
+        }
+
+        public static Activity CreateActivityWithAttributes(ActivitySource source)
+        {
+            var activity = source.StartActivity("name");
+            activity?.AddTag("tag1", "value1");
+            activity?.AddTag("tag2", "value2");
+            activity?.AddTag("customPropTag1", "somecustomValue");
+            activity?.Stop();
+            return activity;
+        }
+
+        public static Activity CreateActivityWithAttributesAndCustomProperty(ActivitySource source)
+        {
+            var activity = source.StartActivity("name");
+            activity?.AddTag("tag1", "value1");
+            activity?.AddTag("tag2", "value2");
+
+            // use custom property instead of tags
+            // activity?.AddTag("customPropTag1", "somecustomValue");
+            activity?.SetCustomProperty("customPropTag1", "somecustomValue");
+            activity?.Stop();
+            return activity;
+        }
+    }
+}
