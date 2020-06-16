9f3bb81b4a96c8b7f57705a0e4bef1b85b8c97bc
Eddy Nakamura <eddynaka@gmail.com>
2020-06-16 01:58:15 -0300

    Enable .NET 452 - part 01 (#722)
    * Enable .NET 452 - part 01
    
    * removing warning and adding net452 to shims.OpenTracing
    
    * adding net452 condition to dependencies code
    
    * undoing non-related changes to pull request
    
    * adding sytem.tuple to dependencies.test for net 452. changing condition to netframework
    
    Co-authored-by: Cijo Thomas <cithomas@microsoft.com>

--- test/OpenTelemetry.Instrumentation.Dependencies.Tests/SqlClientTests.cs
+++ test/OpenTelemetry.Instrumentation.Dependencies.Tests/SqlClientTests.cs
@@ -18,7 +18,11 @@ using System;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
+#if NET452
+using System.Data.SqlClient;
+#else
 using Microsoft.Data.SqlClient;
+#endif
 using Moq;
 using OpenTelemetry.Instrumentation.Dependencies.Implementation;
 using OpenTelemetry.Trace;
