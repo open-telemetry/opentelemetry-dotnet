13b1a5d2fc12a3e9cf90f80f79da8fc490132be0
Rajkumar Rangaraj <rajrang@microsoft.com>
2020-07-02 10:50:48 -0700

    Add a ConsoleActivityExporter/ZipkinActivityExporter extension method with samples (#759)
    * Create testrajrang
    
    * Added overload methods to Console and Zipkin exporter
    
    * Refactored and change variable names
    
    * Incorporating PR feedback
    
    * Incorporating Mikel's feedback

---
+++ src/OpenTelemetry.Exporter.Zipkin/OpenTelemetryBuilderExtensions.cs
@@ -0,0 +1,52 @@
+﻿// <copyright file="OpenTelemetryBuilderExtensions.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using OpenTelemetry.Exporter.Zipkin;
+
+namespace OpenTelemetry.Trace.Configuration
+{
+    /// <summary>
+    /// Extension methods to simplify registering of Zipkin exporter.
+    /// </summary>
+    public static class OpenTelemetryBuilderExtensions
+    {
+        /// <summary>
+        /// Registers a Zipkin exporter that will receive <see cref="System.Diagnostics.Activity"/> instances.
+        /// </summary>
+        /// <param name="builder"><see cref="OpenTelemetryBuilder"/> builder to use.</param>
+        /// <param name="configure">Exporter configuration options.</param>
+        /// <param name="processorConfigure">Activity processor configuration.</param>
+        /// <returns>The instance of <see cref="OpenTelemetryBuilder"/> to chain the calls.</returns>
+        public static OpenTelemetryBuilder UseZipkinActivityExporter(this OpenTelemetryBuilder builder, Action<ZipkinTraceExporterOptions> configure = null, Action<ActivityProcessorPipelineBuilder> processorConfigure = null)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            return builder.AddProcessorPipeline(pipeline =>
+            {
+                var options = new ZipkinTraceExporterOptions();
+                configure?.Invoke(options);
+
+                var activityExporter = new ZipkinActivityExporter(options);
+                pipeline.SetExporter(activityExporter);
+                processorConfigure?.Invoke(pipeline);
+            });
+        }
+    }
+}
