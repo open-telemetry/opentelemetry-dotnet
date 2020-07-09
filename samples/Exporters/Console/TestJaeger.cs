f2696f402f4a9ab0efb771db41e736392f105421
Cijo Thomas <cithomas@microsoft.com>
2020-07-09 11:24:12 -0700

    Remove span based examples from console examples.

--- samples/Exporters/Console/TestJaeger.cs
+++ samples/Exporters/Console/TestJaeger.cs
@@ -23,14 +23,9 @@ namespace Samples
 {
     internal class TestJaeger
     {
-        internal static object Run(string host, int port, bool useActivitySource)
+        internal static object Run(string host, int port)
         {
-            if (useActivitySource)
-            {
-                return RunWithActivity(host, port);
-            }
-
-            return RunWithSdk(host, port);
+            return RunWithActivity(host, port);
         }
 
         internal static object RunWithActivity(string host, int port)
@@ -55,67 +50,13 @@ namespace Samples
             {
                 sample.Start();
 
-                Console.WriteLine("Sample is running on the background, press ENTER to stop");
+                Console.WriteLine("Traces are being created and exported" +
+                    "to Jaeger in the background. Use Jaeger to view them." +
+                    "Press ENTER to stop.");
                 Console.ReadLine();
             }
 
             return null;
-        }
-
-        internal static object RunWithSdk(string host, int port)
-        {
-            // Create a tracer.
-            using var tracerFactory = TracerFactory.Create(
-                builder => builder.UseJaeger(o =>
-                {
-                    o.ServiceName = "jaeger-test";
-                    o.AgentHost = host;
-                    o.AgentPort = port;
-                }));
-            var tracer = tracerFactory.GetTracer("jaeger-test");
-
-            // Create a scoped span. It will end automatically when using statement ends
-            using (tracer.StartActiveSpan("Main", out var span))
-            {
-                span.SetAttribute("custom-attribute", 55);
-                Console.WriteLine("About to do a busy work");
-                for (int i = 0; i < 10; i++)
-                {
-                    DoWork(i, tracer);
-                }
-
-                Console.WriteLine("Completed doing busy work");
-                Console.WriteLine("Press Enter key to exit.");
-            }
-
-            return null;
-        }
-
-        private static void DoWork(int i, Tracer tracer)
-        {
-            // Start another span. If another span was already started, it'll use that span as the parent span.
-            // In this example, the main method already started a span, so that'll be the parent span, and this will be
-            // a child span.
-            using (tracer.StartActiveSpan("DoWork", out var span))
-            {
-                // Simulate some work.
-                try
-                {
-                    Console.WriteLine("Doing busy work");
-                    Thread.Sleep(1000);
-                }
-                catch (ArgumentOutOfRangeException e)
-                {
-                    // Set status upon error
-                    span.Status = Status.Internal.WithDescription(e.ToString());
-                }
-
-                // Annotate our span to capture metadata about our operation
-                var attributes = new Dictionary<string, object>();
-                attributes.Add("use", "demo");
-                attributes.Add("iteration", i);
-                span.AddEvent(new Event("Invoking DoWork", attributes));
-            }
         }
     }
 }
