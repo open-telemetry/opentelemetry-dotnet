b0ec56e0114842c313cadf18c016e96cfbaea14a
Reiley Yang <reyang@microsoft.com>
2020-07-17 21:56:43 -0700

    markdown sanity check

---
+++ build/docfx.cmd
@@ -0,0 +1,17 @@
+SETLOCAL
+SETLOCAL ENABLEEXTENSIONS
+
+type build\docfx.json > docfx.json
+type build\toc.yml > toc.yml
+docfx build docfx.json > docfx.log
+@IF NOT %ERRORLEVEL% == 0 (
+  type docfx.log
+  ECHO Error: docfx build failed. 1>&2
+  EXIT /B %ERRORLEVEL%
+)
+@type docfx.log
+@type docfx.log | findstr /C:"Build succeeded."
+@IF NOT %ERRORLEVEL% == 0 (
+  ECHO Error: you have introduced build warnings. 1>&2
+  EXIT /B %ERRORLEVEL%
+)
