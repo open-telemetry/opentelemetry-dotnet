13d7c8bc52ab4ee2aed271858b1f9e3e1f3365e8
Eddy Nakamura <ednakamu@microsoft.com>
2020-07-31 13:07:18 -0300

    dotnet-format check
    updating name
    
    updating order
    
    Update dotnet-format.yml
    
    updating job name
    
    Updating files based on dotnet-format
    
    updating sanitycheck
    
    updating os

--- examples/AspNet/Controllers/WeatherForecastController.cs
+++ examples/AspNet/Controllers/WeatherForecastController.cs
@@ -4,9 +4,8 @@ using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web.Http;
 using System.Threading.Tasks;
-
+using System.Web.Http;
 using Examples.AspNet.Models;
 
 namespace Examples.AspNet.Controllers
