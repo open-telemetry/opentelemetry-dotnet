bd738f04477f6e1989ee3f871e2041d4f87b5c7f
Alan West <3676547+alanwest@users.noreply.github.com>
2020-08-09 23:59:10 -0700

    End-to-end example applications (#936)
    * Suite of example applications demonstrating context propagation with RabbitMQ
    
    * Put System.* namespaces first
    
    * Newline at EOF
    
    * Fix markdownlint errors
    
    * markdownlint fix
    
    * Refactor WorkerService separating OpenTelemetry related logic from RabbitMQ boilerplate
    
    * Refactor WebApi separating OpenTelemetry related logic from RabbitMQ boilerplate
    
    * Use environment variables for RabbitMQ user/pass
    
    * Add some comments
    
    * Fix RabbitMQ default user/pass
    
    * Use correct ActivityKind
    
    * Add attributes following messaging specification
    
    * Clearing a path through the RabbitMQ jungle
    
    * Log something when sending/receiving a message
    
    * Code style clean up
    
    * Default logging to info level
    
    * Create queue if it does not exist
    
    * Rename controller to SendMessageController
    
    * Refine the readme
    
    * MarkdownCop
    
    * Fix merge snafu
    
    * Add null checks on activity
    
    * Add a missed null check
    
    * Use AddOpenTelemetry in ConfigureServices
    
    Co-authored-by: Cijo Thomas <cithomas@microsoft.com>
    Co-authored-by: Mikel Blanchard <mblanchard@macrosssoftware.com>

---
+++ examples/MicroserviceExample/README.md
@@ -0,0 +1,59 @@
+# OpenTelemetry Example Application
+
+This set of projects is an example distributed application comprised of two
+components:
+
+1. An ASP.NET Core Web API
+2. A background Worker Service
+
+The application demonstrates a number of OpenTelemetry concepts:
+
+* OpenTelemetry APIs for distributed context propagation.
+* Basic conventions of how messaging systems are handled in OpenTelemetry.
+
+The Web API publishes messages to RabbitMQ which the Worker Service consumes.
+Distributed context propagation is achieved using OpenTelemetry APIs to inject
+and extract trace context in the headers of the published messages.
+
+The Zipkin exporter is configured for viewing the distributed traces.
+
+## Running the example
+
+A running instance of RabbitMQ and Zipkin are required. These can easily be
+spun up in docker containers.
+
+The `WebApi` and `WorkerService` projects can be run from this directory as
+follows:
+
+```shell
+dotnet run --project WebApi
+dotnet run --project WorkerService
+```
+
+Instead of running the projects individually, if you are using Docker Desktop,
+a `docker-compose` file is provided. This makes standing up the Zipkin and
+RabbitMQ dependencies easy, as well as starting both applications.
+
+To run the example using `docker-compose`, run the following from this
+directory:
+
+```shell
+docker-compose up --build
+```
+
+With everything running:
+
+* [Invoke the Web API](http://localhost:5000/SendMessage) to send a message.
+* If you have run RabbitMQ and Zipkin with default settings:
+  * View your traces with Zipkin [here](http://localhost:9411/zipkin)
+  * Manage RabbitMQ [here](http://localhost:15672/)
+    * user = guest
+    * password = guest
+
+## References
+
+* [Docker Desktop](https://www.docker.com/products/docker-desktop)
+* [OpenTelemetry Project](https://opentelemetry.io/)
+* [RabbitMQ](https://www.rabbitmq.com/)
+* [Worker Service](https://docs.microsoft.com/en-us/azure/azure-monitor/app/worker-service)
+* [Zipkin](https://zipkin.io)
---
+++ examples/MicroserviceExample/Utils/Messaging/MessageReceiver.cs
@@ -0,0 +1,110 @@
+﻿// <copyright file="MessageReceiver.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Context.Propagation;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Utils.Messaging
+{
+    public class MessageReceiver : IDisposable
+    {
+        private static readonly ActivitySource ActivitySource = new ActivitySource(nameof(MessageReceiver));
+        private static readonly ITextFormat TextFormat = new TraceContextFormat();
+
+        private readonly ILogger<MessageReceiver> logger;
+        private readonly IConnection connection;
+        private readonly IModel channel;
+
+        public MessageReceiver(ILogger<MessageReceiver> logger)
+        {
+            this.logger = logger;
+            this.connection = RabbitMqHelper.CreateConnection();
+            this.channel = RabbitMqHelper.CreateModelAndDeclareTestQueue(this.connection);
+        }
+
+        public void Dispose()
+        {
+            this.channel.Dispose();
+            this.connection.Dispose();
+        }
+
+        public void StartConsumer()
+        {
+            RabbitMqHelper.StartConsumer(this.channel, this.ReceiveMessage);
+        }
+
+        public void ReceiveMessage(BasicDeliverEventArgs ea)
+        {
+            // Extract the ActivityContext of the upstream parent from the message headers.
+            var parentContext = TextFormat.Extract(ea.BasicProperties, this.ExtractTraceContextFromBasicProperties);
+
+            // Start an activity with a name following the semantic convention of the OpenTelemetry messaging specification.
+            // https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/trace/semantic_conventions/messaging.md#span-name
+            var activityName = $"{ea.RoutingKey} receive";
+
+            using (var activity = ActivitySource.StartActivity(activityName, ActivityKind.Consumer, parentContext))
+            {
+                try
+                {
+                    var message = Encoding.UTF8.GetString(ea.Body.Span.ToArray());
+
+                    this.logger.LogInformation($"Message received: [{message}]");
+
+                    if (activity != null)
+                    {
+                        activity.AddTag("message", message);
+
+                        // The OpenTelemetry messaging specification defines a number of attributes. These attributes are added here.
+                        RabbitMqHelper.AddMessagingTags(activity);
+                    }
+
+                    // Simulate some work
+                    Thread.Sleep(1000);
+                }
+                catch (Exception ex)
+                {
+                    this.logger.LogError(ex, "Message processing failed.");
+                }
+            }
+        }
+
+        private IEnumerable<string> ExtractTraceContextFromBasicProperties(IBasicProperties props, string key)
+        {
+            try
+            {
+                if (props.Headers.TryGetValue(key, out var value))
+                {
+                    var bytes = value as byte[];
+                    return new[] { Encoding.UTF8.GetString(bytes) };
+                }
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError(ex, "Failed to extract trace context: {ex}");
+            }
+
+            return Enumerable.Empty<string>();
+        }
+    }
+}
