74ea4663111346d8f21f4c7f8ef8f2debcf8ca9e
Eddy Nakamura <ednakamu@microsoft.com>
2020-08-13 17:11:29 -0300

    updating more files

--- src/OpenTelemetry.Shims.OpenTracing/SpanBuilderShim.cs
+++ src/OpenTelemetry.Shims.OpenTracing/SpanBuilderShim.cs
@@ -191,7 +191,7 @@ namespace OpenTelemetry.Shims.OpenTracing
 
             if (this.error)
             {
-                span.Status = Trace.Status.Unknown;
+                span.SetStatus(Trace.Status.Unknown);
             }
 
             return new SpanShim(span);
