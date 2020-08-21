26a874296b2d3b272cf4b2af5c7f04fff6dfaeaa
xiang17 <xili9@microsoft.com>
2020-08-21 15:02:01 -0700

    Add project template for W3C Trace Context

---
+++ test/OpenTelemetry.Instrumentation.W3cTraceContext.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenTelemetry.Instrumentation.W3cTraceContext.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
