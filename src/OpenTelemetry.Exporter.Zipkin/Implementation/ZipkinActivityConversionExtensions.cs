0d261ec9d7c6d388be0bbe668f8b68b78afc8c2a
Eddy Nakamura <ednakamu@microsoft.com>
2020-08-28 15:11:02 -0300

    net.peer.port will be exported as well

--- src/OpenTelemetry.Exporter.Zipkin/Implementation/ZipkinActivityConversionExtensions.cs
+++ src/OpenTelemetry.Exporter.Zipkin/Implementation/ZipkinActivityConversionExtensions.cs
@@ -211,12 +211,13 @@ namespace OpenTelemetry.Exporter.Zipkin.Implementation
 
                 PooledList<KeyValuePair<string, object>>.Add(ref state.Tags, new KeyValuePair<string, object>(key, strVal));
             }
-            else if (attribute.Value is int intVal && attribute.Key == SemanticConventions.AttributeNetPeerPort)
-            {
-                state.Port = intVal;
-            }
             else
             {
+                if (attribute.Value is int intVal && attribute.Key == SemanticConventions.AttributeNetPeerPort)
+                {
+                    state.Port = intVal;
+                }
+
                 PooledList<KeyValuePair<string, object>>.Add(ref state.Tags, attribute);
             }
 
