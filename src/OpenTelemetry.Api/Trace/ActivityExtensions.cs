88c7aa9058edcacf917903c29515ca5bed38e054
Mikel Blanchard <mblanchard@macrosssoftware.com>
2020-09-01 22:27:37 -0700

    Added GetTagValue, benchmarks, and tests.

--- src/OpenTelemetry.Api/Trace/ActivityExtensions.cs
+++ src/OpenTelemetry.Api/Trace/ActivityExtensions.cs
@@ -15,9 +15,10 @@
 // </copyright>
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Linq.Expressions;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using OpenTelemetry.Internal;
 
@@ -28,6 +29,14 @@ namespace OpenTelemetry.Trace
     /// </summary>
     public static class ActivityExtensions
     {
+        private static readonly object EmptyActivityTagObjects = typeof(Activity).GetField("s_emptyTagObjects", BindingFlags.Static | BindingFlags.NonPublic).GetValue(null);
+
+        private static readonly Enumerator<IEnumerable<KeyValuePair<string, object>>, KeyValuePair<string, object>, KeyValuePair<string, object>>.AllocationFreeForEachDelegate
+            ActivityTagObjectsEnumerator = DictionaryEnumerator<string, object, KeyValuePair<string, object>>.BuildAllocationFreeForEachDelegate(
+                typeof(Activity).GetField("_tags", BindingFlags.Instance | BindingFlags.NonPublic).FieldType);
+
+        private static readonly DictionaryEnumerator<string, object, KeyValuePair<string, object>>.ForEachDelegate GetTagValueCallbackRef = GetTagValueCallback;
+
         /// <summary>
         /// Sets the status of activity execution.
         /// Activity class in .NET does not support 'Status'.
@@ -76,6 +85,31 @@ namespace OpenTelemetry.Trace
         }
 
         /// <summary>
+        /// Gets the value of a specific tag on an <see cref="Activity"/>.
+        /// </summary>
+        /// <param name="activity">Activity instance.</param>
+        /// <param name="tagName">Case-sensitive tag name to retrieve.</param>
+        /// <returns>Tag value or null if a match was not found.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static object GetTagValue(this Activity activity, string tagName)
+        {
+            var tagObjects = activity.TagObjects;
+            if (ReferenceEquals(tagObjects, EmptyActivityTagObjects))
+            {
+                return null;
+            }
+
+            KeyValuePair<string, object> state = new KeyValuePair<string, object>(tagName, null);
+
+            ActivityTagObjectsEnumerator(
+                tagObjects,
+                ref state,
+                GetTagValueCallbackRef);
+
+            return state.Value;
+        }
+
+        /// <summary>
         /// Record Exception.
         /// </summary>
         /// <param name="activity">Activity instance.</param>
@@ -100,6 +134,17 @@ namespace OpenTelemetry.Trace
             }
 
             activity?.AddEvent(new ActivityEvent(SemanticConventions.AttributeExceptionEventName, default, tagsCollection));
+        }
+
+        private static bool GetTagValueCallback(ref KeyValuePair<string, object> state, KeyValuePair<string, object> item)
+        {
+            if (item.Key == state.Key)
+            {
+                state = item;
+                return false;
+            }
+
+            return true;
         }
     }
 }
