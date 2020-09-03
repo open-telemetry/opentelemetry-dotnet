d000382bd41d7c9c6c433e50fe26edf4db7500d6
Alan West <3676547+alanwest@users.noreply.github.com>
2020-09-02 17:43:49 -0700

    Add Grpc.Net.Client example to example Console project

---
+++ examples/Console/TestGrpcNetClient.cs
@@ -0,0 +1,49 @@
+﻿// <copyright file="TestHttpClient.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Diagnostics;
+using Examples.GrpcService;
+using Grpc.Net.Client;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+
+namespace Examples.Console
+{
+    internal class TestGrpcNetClient
+    {
+        internal static object Run()
+        {
+            using var openTelemetry = Sdk.CreateTracerProviderBuilder()
+                .AddGrpcClientInstrumentation()
+                .AddSource("grpc-net-client-test")
+                .AddConsoleExporter(opt => opt.DisplayAsJson = false)
+                .Build();
+
+            var source = new ActivitySource("grpc-net-client-test");
+            using (var parent = source.StartActivity("Main", ActivityKind.Server))
+            {
+                using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+                var client = new Greeter.GreeterClient(channel);
+                var reply = client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" }).GetAwaiter().GetResult();
+                System.Console.WriteLine($"Message received: {reply.Message}");
+            }
+
+            System.Console.ReadLine();
+
+            return null;
+        }
+    }
+}
