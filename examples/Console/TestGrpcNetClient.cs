2c82f952bc4409a74447155fd20c7f773d056d7a
Alan West <3676547+alanwest@users.noreply.github.com>
2020-09-02 17:47:00 -0700

    Fix file header

--- examples/Console/TestGrpcNetClient.cs
+++ examples/Console/TestGrpcNetClient.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="TestHttpClient.cs" company="OpenTelemetry Authors">
+﻿// <copyright file="TestGrpcNetClient.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
