bbb08d94789c7951970a9fef2f390b85724b5a0b
Alan West <3676547+alanwest@users.noreply.github.com>
2020-09-02 17:45:39 -0700

    Remove comments from original template

--- examples/GrpcService/Startup.cs
+++ examples/GrpcService/Startup.cs
@@ -34,8 +34,6 @@ namespace Examples.GrpcService
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddGrpc();
@@ -71,7 +69,6 @@ namespace Examples.GrpcService
             }
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
