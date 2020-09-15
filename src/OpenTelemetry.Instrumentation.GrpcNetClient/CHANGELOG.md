a5a2eb0a8aa3f4e7211f8423e6bcf58e42952c76
Alan West <3676547+alanwest@users.noreply.github.com>
2020-09-14 18:01:40 -0700

    Remove tags added by gRPC library in favor of OpenTelemetry tags (#1260)
    * Remove grpc.method and grpc.status_code tags
    
    * Remove unused usings
    
    * Remove TODO that is probably a WONTDO
    
    Co-authored-by: Cijo Thomas <cithomas@microsoft.com>

--- src/OpenTelemetry.Instrumentation.GrpcNetClient/CHANGELOG.md
+++ src/OpenTelemetry.Instrumentation.GrpcNetClient/CHANGELOG.md
@@ -2,6 +2,12 @@
 
 ## Unreleased
 
+* The `grpc.method` and `grpc.status_code` attributes
+  added by the library are removed from the span. The information from these
+  attributes is contained in other attributes that follow the conventions of
+  OpenTelemetry.
+  ([#1260](https://github.com/open-telemetry/opentelemetry-dotnet/pull/1260)).
+
 ## 0.5.0-beta.2
 
 Released 2020-08-28
--- src/OpenTelemetry.Instrumentation.GrpcNetClient/Implementation/GrpcClientDiagnosticListener.cs
+++ src/OpenTelemetry.Instrumentation.GrpcNetClient/Implementation/GrpcClientDiagnosticListener.cs
@@ -15,7 +15,6 @@
 // </copyright>
 using System;
 using System.Diagnostics;
-using System.Globalization;
 using System.Net.Http;
 using OpenTelemetry.Trace;
 
@@ -69,6 +68,9 @@ namespace OpenTelemetry.Instrumentation.GrpcNetClient.Implementation
                 {
                     activity.SetTag(SemanticConventions.AttributeRpcService, rpcService);
                     activity.SetTag(SemanticConventions.AttributeRpcMethod, rpcMethod);
+
+                    // Remove the grpc.method tag added by the gRPC .NET library
+                    activity.SetTag(GrpcTagHelper.GrpcMethodTagName, null);
                 }
 
                 var uriHostNameType = Uri.CheckHostName(request.RequestUri.Host);
@@ -90,6 +92,9 @@ namespace OpenTelemetry.Instrumentation.GrpcNetClient.Implementation
             if (activity.IsAllDataRequested)
             {
                 activity.SetStatus(GrpcTagHelper.GetGrpcStatusCodeFromActivity(activity));
+
+                // Remove the grpc.status_code tag added by the gRPC .NET library
+                activity.SetTag(GrpcTagHelper.GrpcStatusCodeTagName, null);
             }
 
             this.activitySource.Stop(activity);
