edf078e10580f95859bc81f82945cedc3baddc07
Eddy Nakamura <ednakamu@microsoft.com>
2020-10-06 05:59:22 -0300

    improve docs for enrich action

--- src/OpenTelemetry.Instrumentation.AspNet/AspNetInstrumentationOptions.cs
+++ src/OpenTelemetry.Instrumentation.AspNet/AspNetInstrumentationOptions.cs
@@ -46,6 +46,12 @@ namespace OpenTelemetry.Instrumentation.AspNet
         /// <summary>
         /// Gets or sets an action to enrich an Activity.
         /// </summary>
+        /// <remarks>
+        /// <para><see cref="Activity"/>: the activity being enriched.</para>
+        /// <para>string: the name of the event.</para>
+        /// <para>object: the raw object from which additional information can be extracted to enrich the activity.
+        /// The type of this object depends on the event, which is given by the above parameter.</para>
+        /// </remarks>
         public Action<Activity, string, object> Enrich { get; set; }
     }
 }
