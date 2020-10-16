b80a6c4298ab3477beac66f63f7b03b2c0a9cccc
Eddy Nakamura <ednakamu@microsoft.com>
2020-10-16 14:34:50 -0300

    Fixing null reference in propertyfetcher (#1350)
    replacing fetch for tryfetch
    
    fixing netcoreapp3.1 issue in htp instrumentation
    
    adding back Fetch method
    
    adding null validation; adding null test

--- test/OpenTelemetry.Tests/Instrumentation/PropertyFetcherTest.cs
+++ test/OpenTelemetry.Tests/Instrumentation/PropertyFetcherTest.cs
@@ -27,8 +27,7 @@ namespace OpenTelemetry.Tests.Instrumentation
         {
             var activity = new Activity("test");
             var fetch = new PropertyFetcher<string>("DisplayName");
-            var result = fetch.Fetch(activity);
-
+            Assert.True(fetch.TryFetch(activity, out string result));
             Assert.Equal(activity.DisplayName, result);
         }
 
@@ -37,13 +36,20 @@ namespace OpenTelemetry.Tests.Instrumentation
         {
             var activity = new Activity("test");
             var fetch = new PropertyFetcher<string>("DisplayName2");
-            var result = fetch.Fetch(activity);
+            Assert.False(fetch.TryFetch(activity, out string result));
 
             var fetchInt = new PropertyFetcher<int>("DisplayName2");
-            var resultInt = fetchInt.Fetch(activity);
+            Assert.False(fetchInt.TryFetch(activity, out int resultInt));
 
             Assert.Equal(default, result);
             Assert.Equal(default, resultInt);
+        }
+
+        [Fact]
+        public void FetchNullProperty()
+        {
+            var fetch = new PropertyFetcher<string>("null");
+            Assert.False(fetch.TryFetch(null, out _));
         }
     }
 }
