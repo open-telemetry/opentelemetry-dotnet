59eafd090dd2db6da2f2eac33b60f47bdbf5262d
Rajkumar Rangaraj <rajrang@microsoft.com>
2020-10-16 11:20:31 -0700

    Update copyright

--- src/OpenTelemetry.Shared/IPersistentStorage.cs
+++ src/OpenTelemetry.Shared/IPersistentStorage.cs
@@ -1,4 +1,4 @@
-// <copyright file="IOfflineStorage.cs" company="OpenTelemetry Authors">
+// <copyright file="IPersistentStorage.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
