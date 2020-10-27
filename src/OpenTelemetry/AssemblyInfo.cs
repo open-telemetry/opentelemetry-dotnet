41cd4f5d9c611b3d15187016bf1a70993464bc4e
Mikel Blanchard <mblanchard@macrosssoftware.com>
2020-10-27 16:39:43 -0700

    OtlpExporter peer.service resolution (#1392)
    * Refactor peer service logic into a shared location. Support building peer.service tag in OtlpExporter.
    
    * Added unit tests for OtlpExporter peer service support. Refactored a few things around to resolve types ending up in multiple assemblies.
    
    * Refactored Zipkin to use the new PeerServiceResolver.
    
    * Updated changelog.
    
    Co-authored-by: Eddy Nakamura <ednakamu@microsoft.com>
    Co-authored-by: Cijo Thomas <cithomas@microsoft.com>

--- src/OpenTelemetry/AssemblyInfo.cs
+++ src/OpenTelemetry/AssemblyInfo.cs
@@ -17,20 +17,5 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("OpenTelemetry.Tests" + AssemblyInfo.PublicKey)]
-[assembly: InternalsVisibleTo("OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests" + AssemblyInfo.PublicKey)]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2" + AssemblyInfo.MoqPublicKey)]
 [assembly: InternalsVisibleTo("Benchmarks" + AssemblyInfo.PublicKey)]
-
-#if SIGNED
-internal static class AssemblyInfo
-{
-    public const string PublicKey = ", PublicKey=002400000480000094000000060200000024000052534131000400000100010051c1562a090fb0c9f391012a32198b5e5d9a60e9b80fa2d7b434c9e5ccb7259bd606e66f9660676afc6692b8cdc6793d190904551d2103b7b22fa636dcbb8208839785ba402ea08fc00c8f1500ccef28bbf599aa64ffb1e1d5dc1bf3420a3777badfe697856e9d52070a50c3ea5821c80bef17ca3acffa28f89dd413f096f898";
-    public const string MoqPublicKey = ", PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7";
-}
-#else
-internal static class AssemblyInfo
-{
-    public const string PublicKey = "";
-    public const string MoqPublicKey = "";
-}
-#endif
