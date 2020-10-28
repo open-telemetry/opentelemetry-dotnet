ea29179c90a533175f89d0527a5e5a10130b45c5
Eddy Nakamura <ednakamu@microsoft.com>
2020-10-27 22:32:39 -0300

    fixing remarks text

--- src/OpenTelemetry.Instrumentation.AspNetCore/AspNetCoreInstrumentationOptions.cs
+++ src/OpenTelemetry.Instrumentation.AspNetCore/AspNetCoreInstrumentationOptions.cs
@@ -58,7 +58,7 @@ namespace OpenTelemetry.Instrumentation.AspNetCore
         /// Gets or sets a value indicating whether the exception will be recorded as ActivityEvent or not.
         /// </summary>
         /// <remarks>
-        /// https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/trace/semantic_conventions/exceptions.md
+        /// https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/trace/semantic_conventions/exceptions.md.
         /// </remarks>
         public bool RecordException { get; set; }
     }
