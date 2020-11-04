9e5f8990a551f01ce4d731b6acf8b8abe6833f7d
Eddy Nakamura <ednakamu@microsoft.com>
2020-11-04 11:13:33 -0300

    SpanAttributeConstants is internal

--- src/OpenTelemetry.Api/Trace/SpanAttributeConstants.cs
+++ src/OpenTelemetry.Api/Trace/SpanAttributeConstants.cs
@@ -19,7 +19,7 @@ namespace OpenTelemetry.Trace
     /// <summary>
     /// Defines well-known span attribute keys.
     /// </summary>
-    public static class SpanAttributeConstants
+    internal static class SpanAttributeConstants
     {
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public const string StatusCodeKey = "otel.status_code";
