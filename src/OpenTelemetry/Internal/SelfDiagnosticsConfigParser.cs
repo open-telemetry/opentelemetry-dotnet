ad357651452ce352ecc18038eb53f482c39d86fc
xiang17 <xili9@microsoft.com>
2020-11-05 14:44:07 -0800

    [SelfDiagnosticsModule] Reads configuration file and creates log file (#1468)
    * Add ConfigRefresher
    
    * Refactor parser related code into ConfigParser
    
    * Add comments
    
    * Remove ThreadLocal in ConfigParser and fix initialization order in ConfigRefresher constructor
    
    * Use compiled regualr expressions
    
    * Add comment for a race condition during configuration file refresh when the file changed
    
    Co-authored-by: Cijo Thomas <cithomas@microsoft.com>

---
+++ src/OpenTelemetry/Internal/SelfDiagnosticsConfigParser.cs
@@ -0,0 +1,124 @@
+// <copyright file="SelfDiagnosticsConfigParser.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using System.Diagnostics.Tracing;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace OpenTelemetry.Internal
+{
+    internal class SelfDiagnosticsConfigParser
+    {
+        private const string ConfigFileName = "DiagnosticsConfiguration.json";
+        private const int FileSizeLowerLimit = 1024;  // Lower limit for log file size in KB: 1MB
+        private const int FileSizeUpperLimit = 128 * 1024;  // Upper limit for log file size in KB: 128MB
+
+        /// <summary>
+        /// ConfigBufferSize is the maximum bytes of config file that will be read.
+        /// </summary>
+        private const int ConfigBufferSize = 4 * 1024;
+
+        private static readonly Regex LogDirectoryRegex = new Regex(
+            @"""LogDirectory""\s*:\s*""(?<LogDirectory>.*?)""", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        private static readonly Regex FileSizeRegex = new Regex(
+            @"""FileSize""\s*:\s*(?<FileSize>\d+)", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        private static readonly Regex EventLevelRegex = new Regex(
+            @"""EventLevel""\s*:\s*""(?<EventLevel>.*?)""", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
+        // This class is called in SelfDiagnosticsConfigRefresher.UpdateMemoryMappedFileFromConfiguration
+        // in both main thread and the worker thread.
+        // In theory the variable won't be access at the same time because worker thread first Task.Delay for a few seconds.
+        private byte[] configBuffer;
+
+        public bool TryGetConfiguration(out string logDirectory, out int fileSizeInKB, out EventLevel eventLevel)
+        {
+            logDirectory = null;
+            fileSizeInKB = 0;
+            eventLevel = EventLevel.LogAlways;
+            try
+            {
+                using FileStream file = File.Open(ConfigFileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete);
+                var buffer = this.configBuffer;
+                if (buffer == null)
+                {
+                    buffer = new byte[ConfigBufferSize]; // Fail silently if OOM
+                    this.configBuffer = buffer;
+                }
+
+                file.Read(buffer, 0, buffer.Length);
+                string configJson = Encoding.UTF8.GetString(buffer);
+                if (!TryParseLogDirectory(configJson, out logDirectory))
+                {
+                    return false;
+                }
+
+                if (!TryParseFileSize(configJson, out fileSizeInKB))
+                {
+                    return false;
+                }
+
+                if (fileSizeInKB < FileSizeLowerLimit)
+                {
+                    fileSizeInKB = FileSizeLowerLimit;
+                }
+
+                if (fileSizeInKB > FileSizeUpperLimit)
+                {
+                    fileSizeInKB = FileSizeUpperLimit;
+                }
+
+                if (!TryParseEventLevel(configJson, out var eventLevelString))
+                {
+                    return false;
+                }
+
+                eventLevel = (EventLevel)Enum.Parse(typeof(EventLevel), eventLevelString);
+                return true;
+            }
+            catch (Exception)
+            {
+                // do nothing on failure to open/read/parse config file
+            }
+
+            return false;
+        }
+
+        internal static bool TryParseLogDirectory(string configJson, out string logDirectory)
+        {
+            var logDirectoryResult = LogDirectoryRegex.Match(configJson);
+            logDirectory = logDirectoryResult.Groups["LogDirectory"].Value;
+            return logDirectoryResult.Success && !string.IsNullOrWhiteSpace(logDirectory);
+        }
+
+        internal static bool TryParseFileSize(string configJson, out int fileSizeInKB)
+        {
+            fileSizeInKB = 0;
+            var fileSizeResult = FileSizeRegex.Match(configJson);
+            return fileSizeResult.Success && int.TryParse(fileSizeResult.Groups["FileSize"].Value, out fileSizeInKB);
+        }
+
+        internal static bool TryParseEventLevel(string configJson, out string eventLevel)
+        {
+            var eventLevelResult = EventLevelRegex.Match(configJson);
+            eventLevel = eventLevelResult.Groups["EventLevel"].Value;
+            return eventLevelResult.Success && !string.IsNullOrWhiteSpace(eventLevel);
+        }
+    }
+}
---
+++ test/OpenTelemetry.Tests/Internal/SelfDiagnosticsConfigParserTest.cs
@@ -0,0 +1,95 @@
+// <copyright file="SelfDiagnosticsConfigParserTest.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using Xunit;
+
+namespace OpenTelemetry.Internal.Tests
+{
+    public class SelfDiagnosticsConfigParserTest
+    {
+        [Fact]
+        [Trait("Platform", "Any")]
+        public void SelfDiagnosticsConfigParser_TryParseFilePath_Success()
+        {
+            string configJson = "{ \t \n "
+                                + "\t    \"LogDirectory\" \t : \"Diagnostics\", \n"
+                                + "FileSize \t : \t \n"
+                                + " 1024 \n}\n";
+            Assert.True(SelfDiagnosticsConfigParser.TryParseLogDirectory(configJson, out string logDirectory));
+            Assert.Equal("Diagnostics", logDirectory);
+        }
+
+        [Fact]
+        [Trait("Platform", "Any")]
+        public void SelfDiagnosticsConfigParser_TryParseFilePath_MissingField()
+        {
+            string configJson = @"{
+                    ""path"": ""Diagnostics"",
+                    ""FileSize"": 1024
+                    }";
+            Assert.False(SelfDiagnosticsConfigParser.TryParseLogDirectory(configJson, out string logDirectory));
+        }
+
+        [Fact]
+        [Trait("Platform", "Any")]
+        public void SelfDiagnosticsConfigParser_TryParseFileSize()
+        {
+            string configJson = @"{
+                    ""LogDirectory"": ""Diagnostics"",
+                    ""FileSize"": 1024
+                    }";
+            Assert.True(SelfDiagnosticsConfigParser.TryParseFileSize(configJson, out int fileSize));
+            Assert.Equal(1024, fileSize);
+        }
+
+        [Fact]
+        [Trait("Platform", "Any")]
+        public void SelfDiagnosticsConfigParser_TryParseFileSize_CaseInsensitive()
+        {
+            string configJson = @"{
+                    ""LogDirectory"": ""Diagnostics"",
+                    ""fileSize"" :
+                                   2048
+                    }";
+            Assert.True(SelfDiagnosticsConfigParser.TryParseFileSize(configJson, out int fileSize));
+            Assert.Equal(2048, fileSize);
+        }
+
+        [Fact]
+        [Trait("Platform", "Any")]
+        public void SelfDiagnosticsConfigParser_TryParseFileSize_MissingField()
+        {
+            string configJson = @"{
+                    ""LogDirectory"": ""Diagnostics"",
+                    ""size"": 1024
+                    }";
+            Assert.False(SelfDiagnosticsConfigParser.TryParseFileSize(configJson, out int fileSize));
+        }
+
+        [Fact]
+        [Trait("Platform", "Any")]
+        public void SelfDiagnosticsConfigParser_TryParseEventLevel()
+        {
+            string configJson = @"{
+                    ""LogDirectory"": ""Diagnostics"",
+                    ""FileSize"": 1024,
+                    ""EventLevel"": ""Error""
+                    }";
+            Assert.True(SelfDiagnosticsConfigParser.TryParseEventLevel(configJson, out string eventLevelString));
+            Assert.Equal("Error", eventLevelString);
+        }
+    }
+}
