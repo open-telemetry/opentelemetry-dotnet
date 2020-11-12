4ab6da0812b3c6896cfa4944e1ddc85cfeb8c2c2
Prashant Srivastava <50466688+srprash@users.noreply.github.com>
2020-11-12 10:16:05 -0800

    Adding support for default resource creation with environment variable (#1436)
    * Adding default and envVar resource. Initial commit
    
    * Adding Create method for resource creation with default resource and relevant unit tests.
    
    * Adding isNullOrEmpty check to env var value to remove redundant condition.
    
    * TracerProviderBuilder now calls Resource.Create to get the default resource with EnvVar attributes as well
    
    * The default reource now includes the attributes from env var as well. Refactoring unit tests for the same.
    
    * Adding ResourceDetector interface and public methods to add attributes from default and env var resources
    
    * Missed out new resource files. Adding
    
    * adding some more tests
    
    * Adding missing import
    
    * Updating public api list
    
    * Refactoring method name for new methods
    
    * Making IResourceDetector and corresponding impl internal until spec is official about detectors
    
    * Refactoring return
    
    * Was using wrong Sdk in test. Fixed.
    
    * Adding error handling to OtelEnvResourceDetector
    
    Co-authored-by: Cijo Thomas <cithomas@microsoft.com>

---
+++ src/OpenTelemetry/Resources/OtelEnvResourceDetector.cs
@@ -0,0 +1,69 @@
+// <copyright file="OtelEnvResourceDetector.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using OpenTelemetry.Internal;
+
+namespace OpenTelemetry.Resources
+{
+    internal class OtelEnvResourceDetector : IResourceDetector
+    {
+        private const string OTelResourceEnvVarKey = "OTEL_RESOURCE_ATTRIBUTES";
+        private const char AttributeListSplitter = ',';
+        private const char AttributeKeyValueSplitter = '=';
+
+        public Resource Detect()
+        {
+            var resource = Resource.Empty;
+
+            try
+            {
+                string envResourceAttributeValue = Environment.GetEnvironmentVariable(OTelResourceEnvVarKey);
+                if (!string.IsNullOrEmpty(envResourceAttributeValue))
+                {
+                    var attributes = ParseResourceAttributes(envResourceAttributeValue);
+                    resource = new Resource(attributes);
+                }
+            }
+            catch (Exception ex)
+            {
+                OpenTelemetrySdkEventSource.Log.ResourceDetectorFailed("OtelEnvResourceDetector", ex.Message);
+            }
+
+            return resource;
+        }
+
+        private static IEnumerable<KeyValuePair<string, object>> ParseResourceAttributes(string resourceAttributes)
+        {
+            var attributes = new List<KeyValuePair<string, object>>();
+
+            string[] rawAttributes = resourceAttributes.Split(AttributeListSplitter);
+            foreach (string rawKeyValuePair in rawAttributes)
+            {
+                string[] keyValuePair = rawKeyValuePair.Split(AttributeKeyValueSplitter);
+                if (keyValuePair.Length != 2)
+                {
+                    continue;
+                }
+
+                attributes.Add(new KeyValuePair<string, object>(keyValuePair[0].Trim(), keyValuePair[1].Trim()));
+            }
+
+            return attributes;
+        }
+    }
+}
