23c134fea19b4562f4b8d50226d0c7cb8a9714db
Utkarsh Umesan Pillai <utpilla@microsoft.com>
2021-01-29 17:32:49 -0800

    Removed unnecessary using statements

--- test/OpenTelemetry.Tests/Trace/ExportProcessorTest.cs
+++ test/OpenTelemetry.Tests/Trace/ExportProcessorTest.cs
@@ -14,9 +14,7 @@
 // limitations under the License.
 // </copyright>
 
-using System.Collections.Generic;
 using System.Diagnostics;
-using OpenTelemetry.Exporter;
 using OpenTelemetry.Tests;
 using Xunit;
 
