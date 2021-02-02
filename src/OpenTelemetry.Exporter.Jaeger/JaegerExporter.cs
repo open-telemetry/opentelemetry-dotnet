d3228c582f80ed271c633caa9b88fd44c776955b
Austin Tan <austinwtan@gmail.com>
2021-02-01 16:40:00 -0800

    re-adding servicename to Ctor for Jaeger

--- src/OpenTelemetry.Exporter.Jaeger/JaegerExporter.cs
+++ src/OpenTelemetry.Exporter.Jaeger/JaegerExporter.cs
@@ -56,6 +56,9 @@ namespace OpenTelemetry.Exporter.Jaeger
             this.thriftClient = new JaegerThriftClient(this.protocolFactory.GetProtocol(this.clientTransport));
             this.memoryTransport = new InMemoryTransport(16000);
             this.memoryProtocol = this.protocolFactory.GetProtocol(this.memoryTransport);
+
+            string serviceName = (string)this.ParentProvider.GetDefaultResource().Attributes.Where(
+                    pair => pair.Key == ResourceSemanticConventions.AttributeServiceName).FirstOrDefault().Value;
             this.Process = new Process(serviceName, options.ProcessTags);
         }
 
