b6cad984b9dc8682c76b954143aca52d092294d9
Cijo Thomas <cithomas@microsoft.com>
2021-02-04 16:12:46 -0800

    more comment.

--- src/OpenTelemetry.Exporter.OpenTelemetryProtocol/OtlpExporterOptions.cs
+++ src/OpenTelemetry.Exporter.OpenTelemetryProtocol/OtlpExporterOptions.cs
@@ -25,7 +25,10 @@ namespace OpenTelemetry.Exporter
     public class OtlpExporterOptions
     {
         /// <summary>
-        /// Gets or sets the target to which the exporter is going to send traces or metrics.
+        /// Gets or sets the target to which the exporter is going to send traces.
+        /// Must be a valid Uri with scheme (http) and host, and
+        /// may contain a port and path. Secure connection(https) is not
+        /// supported.
         /// </summary>
         public Uri Endpoint { get; set; } = new Uri("http://localhost:4317");
 
