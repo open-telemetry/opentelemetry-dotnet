fa9cf92f63b2a22b71e04565754a1470d011bb4b
Reiley Yang <reyang@microsoft.com>
2021-03-06 07:19:49 -0800

    Add SetErrorStatusOnException option to TracerProviderSdk (#1858)
    * add SetErrorStatusOnUnhandledException option to TracerProviderSdk
    
    * changelog
    
    * add doc
    
    * markdown lint
    
    * fix nits
    
    * markdown lint
    
    * update public api file
    
    * fix nits in the changelog
    
    * add test cases
    
    * improve example
    
    * improve doc
    
    * rename
    
    * change the wording in the doc
    
    * use RecordException in the example
    
    * tweak the doc
    
    * address review comment from Tom
    
    * adjust wording
    
    * Revert "adjust wording"
    
    This reverts commit 9bf74a336dfad203b5d78e632b28b9c70a32073a.
    
    * better exception message
    
    * update example/doc
    
    * more test cases
    
    * address corner case in test
    
    Co-authored-by: Cijo Thomas <cithomas@microsoft.com>

---
+++ test/OpenTelemetry.Tests/Trace/ExceptionProcessorTest.cs
@@ -0,0 +1,120 @@
+// <copyright file="ExceptionProcessorTest.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using System.Diagnostics;
+using OpenTelemetry.Tests;
+using Xunit;
+
+namespace OpenTelemetry.Trace.Tests
+{
+    public class ExceptionProcessorTest
+    {
+        private const string ActivitySourceName = "ExceptionProcessorTest";
+
+        [Fact]
+        public void ActivityStatusSetToErrorWhenExceptionProcessorEnabled()
+        {
+            using var activitySource = new ActivitySource(ActivitySourceName);
+            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .AddSource(ActivitySourceName)
+                .SetSampler(new AlwaysOnSampler())
+                .AddProcessor(new ExceptionProcessor())
+                .Build();
+
+            Activity activity1 = null;
+            Activity activity2 = null;
+            Activity activity3 = null;
+            Activity activity4 = null;
+            Activity activity5 = null;
+
+            try
+            {
+                using (activity1 = activitySource.StartActivity("Activity1"))
+                {
+                    using (activity2 = activitySource.StartActivity("Activity2"))
+                    {
+                        throw new Exception("Oops!");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                using (activity3 = activitySource.StartActivity("Activity3"))
+                {
+                }
+            }
+            finally
+            {
+                using (activity4 = activitySource.StartActivity("Activity4"))
+                {
+                }
+            }
+
+            try
+            {
+                throw new Exception("Oops!");
+            }
+            catch (Exception)
+            {
+                activity5 = activitySource.StartActivity("Activity5");
+            }
+            finally
+            {
+                activity5.Dispose();
+            }
+
+            Assert.Equal(StatusCode.Error, activity1.GetStatus().StatusCode);
+            Assert.Null(activity1.GetTagValue("otel.exception_pointers"));
+            Assert.Equal(StatusCode.Error, activity2.GetStatus().StatusCode);
+            Assert.Null(activity2.GetTagValue("otel.exception_pointers"));
+            Assert.Equal(StatusCode.Unset, activity3.GetStatus().StatusCode);
+            Assert.Null(activity3.GetTagValue("otel.exception_pointers"));
+            Assert.Equal(StatusCode.Unset, activity4.GetStatus().StatusCode);
+            Assert.Null(activity4.GetTagValue("otel.exception_pointers"));
+            Assert.Equal(StatusCode.Unset, activity5.GetStatus().StatusCode);
+#if !NETFRAMEWORK
+            // In this rare case, the following Activity tag will not get cleaned up due to perf consideration.
+            Assert.NotNull(activity5.GetTagValue("otel.exception_pointers"));
+#endif
+        }
+
+        [Fact]
+        public void ActivityStatusNotSetWhenExceptionProcessorNotEnabled()
+        {
+            using var activitySource = new ActivitySource(ActivitySourceName);
+            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .AddSource(ActivitySourceName)
+                .SetSampler(new AlwaysOnSampler())
+                .Build();
+
+            Activity activity = null;
+
+            try
+            {
+                using (activity = activitySource.StartActivity("Activity"))
+                {
+                    throw new Exception("Oops!");
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            Assert.Equal(StatusCode.Unset, activity.GetStatus().StatusCode);
+        }
+    }
+}
