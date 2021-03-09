3c37c0b6e4d5fc6e14c593a1700936cda23dc20a
Cijo Thomas <cithomas@microsoft.com>
2021-03-09 14:35:03 -0800

    Update test package versions (#1888)
    * Update test package versions

--- build/process-codecoverage.ps1
+++ build/process-codecoverage.ps1
@@ -2,7 +2,7 @@
 Write-Host $env:USERPROFILE
 foreach ($file in $files)
 {
-    $command = $env:USERPROFILE+ '\.nuget\packages\microsoft.codecoverage\16.9.0-preview-20201020-06\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze /output:' + $file.DirectoryName + '\' + $file.Name + '.xml '+ $file.FullName
+    $command = $env:USERPROFILE+ '\.nuget\packages\microsoft.codecoverage\16.9.1\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze /output:' + $file.DirectoryName + '\' + $file.Name + '.xml '+ $file.FullName
     Write-Host $command
     Invoke-Expression $command
 }
