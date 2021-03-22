7210445c2baf9006b65108011843fab17665a11c
Eric J. Smith <eric@codesmithtools.com>
2021-03-22 11:03:46 -0500

    Fix file header

--- src/OpenTelemetry.Extensions.Hosting/Implementation/EventSourceEventFormatter.cs
+++ src/OpenTelemetry.Extensions.Hosting/Implementation/EventSourceEventFormatter.cs
@@ -1,4 +1,4 @@
-// <copyright file="EventSourceEventFormatting.cs" company="OpenTelemetry Authors">
+// <copyright file="EventSourceEventFormatter.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
