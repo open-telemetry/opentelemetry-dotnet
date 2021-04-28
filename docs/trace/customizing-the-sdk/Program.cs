085cb768c82b4e2a19d6b2dad73f16922d3221a4
Cijo Thomas <cithomas@microsoft.com>
2021-04-28 00:48:21 -0700

    AddSource section to customizer the sdk doc.

--- docs/trace/customizing-the-sdk/Program.cs
+++ docs/trace/customizing-the-sdk/Program.cs
@@ -14,9 +14,65 @@
 // limitations under the License.
 // </copyright>
 
+using System.Diagnostics;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+
 public class Program
 {
-    public static void Main(string[] args)
+    private static readonly ActivitySource MyLibraryActivitySource = new ActivitySource(
+        "MyCompany.MyProduct.MyLibrary");
+
+    private static readonly ActivitySource ComponentAActivitySource = new ActivitySource(
+        "ABCCompany.XYZProduct.ComponentA");
+
+    private static readonly ActivitySource ComponentBActivitySource = new ActivitySource(
+        "ABCCompany.XYZProduct.ComponentB");
+
+    private static readonly ActivitySource SomeOtherActivitySource = new ActivitySource(
+        "SomeCompany.SomeProduct.SomeComponent");
+
+    public static void Main()
     {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+
+            // The following subscribes to activities from Activity Source
+            // named "MyCompany.MyProduct.MyLibrary" only.
+            .AddSource("MyCompany.MyProduct.MyLibrary")
+
+            // The following subscribes to activities from all Activity Sources
+            // whose name starts with  "ABCCompany.XYZProduct.".
+            .AddSource("ABCCompany.XYZProduct.*")
+            .AddConsoleExporter()
+            .Build();
+
+        // This activity source is enabled.
+        using (var activity = MyLibraryActivitySource.StartActivity("SayHello"))
+        {
+            activity?.SetTag("foo", 1);
+            activity?.SetTag("bar", "Hello, World!");
+        }
+
+        // This activity source is enabled through wild card "ABCCompany.XYZProduct.*"
+        using (var activity = ComponentAActivitySource.StartActivity("SayHello"))
+        {
+            activity?.SetTag("foo", 1);
+            activity?.SetTag("bar", "Hello, World!");
+        }
+
+        // This activity source is enabled through wild card "ABCCompany.XYZProduct.*"
+        using (var activity = ComponentBActivitySource.StartActivity("SayHello"))
+        {
+            activity?.SetTag("foo", 1);
+            activity?.SetTag("bar", "Hello, World!");
+        }
+
+        // This activity source is not enabled, so activity will
+        // be null here.
+        using (var activity = SomeOtherActivitySource.StartActivity("SayHello"))
+        {
+            activity?.SetTag("foo", 1);
+            activity?.SetTag("bar", "Hello, World!");
+        }
     }
 }
