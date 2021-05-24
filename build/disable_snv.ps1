fea5a84a64146c47e4c6fca3f6ba5443f1d12b2d
Cijo Thomas <cithomas@microsoft.com>
2021-05-24 16:50:36 -0700

    locate sn.exe

--- build/disable_snv.ps1
+++ build/disable_snv.ps1
@@ -1,2 +1,2 @@
-﻿& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\x64\sn.exe" -Vr *,31bf3856ad364e35
-& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\sn.exe" -Vr *,31bf3856ad364e35
+﻿& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\sn.exe" -Vr *,31bf3856ad364e35
+& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -Vr *,31bf3856ad364e35
