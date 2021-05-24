022004da111a4b8d1e5794063913388d9fb015e2
Cijo Thomas <cithomas@microsoft.com>
2021-05-24 16:38:11 -0700

    try sn disable

---
+++ build/disable_snv.ps1
@@ -0,0 +1,2 @@
+﻿& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\x64\sn.exe" -Vr *,31bf3856ad364e35
+& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\sn.exe" -Vr *,31bf3856ad364e35
