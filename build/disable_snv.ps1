f2becd0165f8d7dd244ea551cb67762703d86660
Cijo Thomas <cithomas@microsoft.com>
2021-05-24 17:04:15 -0700

    add to sln

--- build/disable_snv.ps1
+++ build/disable_snv.ps1
@@ -1,3 +1,4 @@
-﻿& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\x64\sn.exe" -Vr *,b03f5f7f11d50a3a
-& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\sn.exe" -Vr *,b03f5f7f11d50a3a
+﻿& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\sn.exe" -Vr *,b03f5f7f11d50a3a
+& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -Vr *,b03f5f7f11d50a3a
+
 & where /R \ sn.exe
