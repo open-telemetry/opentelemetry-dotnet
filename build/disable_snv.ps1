2c563aa41efe5168637aa01bf7e6839234869277
Cijo Thomas <cithomas@microsoft.com>
2021-05-24 17:04:30 -0700

    remove where

--- build/disable_snv.ps1
+++ build/disable_snv.ps1
@@ -1,4 +1,3 @@
 ﻿& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\sn.exe" -Vr *,b03f5f7f11d50a3a
 & "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -Vr *,b03f5f7f11d50a3a
 
-& where /R \ sn.exe
