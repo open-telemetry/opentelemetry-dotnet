599a12c70d5e9cf17f3604bbfd6e8c3b8646a76a
Cijo Thomas <cithomas@microsoft.com>
2021-05-24 17:06:07 -0700

    Use Dotnet6 and sn fixes

---
+++ build/disable_snv.ps1
@@ -0,0 +1,3 @@
+﻿& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\x64\sn.exe" -Vr *,b03f5f7f11d50a3a
+& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.2 Tools\sn.exe" -Vr *,b03f5f7f11d50a3a
+& where /R \ sn.exe
