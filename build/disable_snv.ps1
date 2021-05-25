3e90b6b3d61abc797cc4d333ab115c233a7e3543
Cijo Thomas <cithomas@microsoft.com>
2021-05-24 17:21:36 -0700

    add both sn tokens

--- build/disable_snv.ps1
+++ build/disable_snv.ps1
@@ -1,3 +1,6 @@
 ﻿& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\sn.exe" -Vr *,b03f5f7f11d50a3a
 & "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -Vr *,b03f5f7f11d50a3a
+& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\sn.exe" -Vr *,cc7b13ffcd2ddd51
+& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -Vr *,cc7b13ffcd2ddd51
+
 
