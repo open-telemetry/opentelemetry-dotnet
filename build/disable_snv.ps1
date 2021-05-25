96d02aaa2f49991cbd8178476b683b874db4c99f
Cijo Thomas <cithomas@microsoft.com>
2021-05-24 17:32:19 -0700

    display settings

--- build/disable_snv.ps1
+++ build/disable_snv.ps1
@@ -1,6 +1,8 @@
-﻿& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\sn.exe" -Vr *,b03f5f7f11d50a3a
+﻿& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -Vl
+& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\sn.exe" -Vr *,b03f5f7f11d50a3a
 & "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -Vr *,b03f5f7f11d50a3a
 & "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\sn.exe" -Vr *,cc7b13ffcd2ddd51
 & "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -Vr *,cc7b13ffcd2ddd51
+& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\sn.exe" -Vl
 
 
