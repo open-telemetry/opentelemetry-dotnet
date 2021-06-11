f56aa83e41bbd04fb6bb22da7db6fa7a59e11979
Tim Hess <thess@vmware.com>
2021-06-11 15:28:40 -0500

    revert randomized script sleeping since it broke download

--- build/PreBuild.ps1
+++ build/PreBuild.ps1
@@ -13,16 +13,10 @@ if (Test-Path -Path "$workDir\$package.$version.zip")
 }
 else
 {
-    # because this script will be hit for each target framework in each project, there might be several copies running simultaneously
-    # wait a random couple of seconds before continuing, checking again for the zip file
-    Start-Sleep -Seconds (Get-Random -Minimum 0 -Maximum 3)
-    if (Test-Path -Path "$workDir\$package.$version.zip")
-    {
-        Write-Host "Retrieving $package @$version for compatibility check"
-        Invoke-WebRequest -Uri https://www.nuget.org/api/v2/package/$package/$version -Outfile "$workDir\$package.$version.zip"
-    }
+    Write-Host "Retrieving $package @$version for compatibility check"
+    Invoke-WebRequest -Uri https://www.nuget.org/api/v2/package/$package/$version -Outfile "$workDir\$package.$version.zip"
 }
-if (Test-Path -Path "$workDir\$package\$version")
+if (Test-Path -Path "$workDir\$package\$version\lib")
 {
     Write-Debug "Previous package version already extracted"
 }
