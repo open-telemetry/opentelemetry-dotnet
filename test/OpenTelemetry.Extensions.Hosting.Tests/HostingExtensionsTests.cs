2fecc22d3ce26422228a716ae5d8ed94b28851f3
Mikel Blanchard <mblanchard@macrosssoftware.com>
2021-06-11 20:28:15 -0700

    More tests, bug fixes, and simplification.

--- test/OpenTelemetry.Extensions.Hosting.Tests/HostingExtensionsTests.cs
+++ test/OpenTelemetry.Extensions.Hosting.Tests/HostingExtensionsTests.cs
@@ -15,6 +15,8 @@
 // </copyright>
 
 using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -140,6 +142,33 @@ namespace OpenTelemetry.Extensions.Hosting.Tests
             Assert.Equal(2, configureCalls);
         }
 
+        [Fact]
+        public void AddOpenTelemetryTracerProvider_ConfigureCallbacksUsingExtensions()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<TestInstrumentation>();
+            services.AddSingleton<TestProcessor>();
+            services.AddSingleton<TestSampler>();
+
+            services.AddOpenTelemetryTracing(builder => builder
+                .Configure((sp1, builder1) =>
+                {
+                    builder1
+                        .AddInstrumentation<TestInstrumentation>()
+                        .AddProcessor<TestProcessor>()
+                        .SetSampler<TestSampler>();
+                }));
+
+            using var serviceProvider = services.BuildServiceProvider();
+
+            var tracerProvider = (TracerProviderSdk)serviceProvider.GetRequiredService<TracerProvider>();
+
+            Assert.True(tracerProvider.Instrumentations.FirstOrDefault() is TestInstrumentation);
+            Assert.True(tracerProvider.Processor is TestProcessor);
+            Assert.True(tracerProvider.Sampler is TestSampler);
+        }
+
         [Fact(Skip = "Known limitation. See issue 1215.")]
         public void AddOpenTelemetryTracerProvider_Idempotent()
         {
@@ -177,6 +206,18 @@ namespace OpenTelemetry.Extensions.Hosting.Tests
             public void Dispose()
             {
                 this.Disposed = true;
+            }
+        }
+
+        internal class TestProcessor : BaseProcessor<Activity>
+        {
+        }
+
+        internal class TestSampler : Sampler
+        {
+            public override SamplingResult ShouldSample(in SamplingParameters samplingParameters)
+            {
+                return new SamplingResult(SamplingDecision.RecordAndSample);
             }
         }
     }
