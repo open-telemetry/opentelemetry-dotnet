53f7900d3db0c9d9a83d8c4b3ccee5126f793242
Cijo Thomas <cithomas@microsoft.com>
2021-06-16 10:19:31 -0700

    Metric console exporter modified to deal with all known types

--- src/OpenTelemetry/Metrics/Processors/MetricConsoleExporter.cs
+++ src/OpenTelemetry/Metrics/Processors/MetricConsoleExporter.cs
@@ -34,12 +34,45 @@ namespace OpenTelemetry.Metrics
             {
                 var tags = metric.Attributes.ToArray().Select(k => $"{k.Key}={k.Value?.ToString()}");
 
+                string valueDisplay = string.Empty;
+                if (metric is ISumMetric sumMetric)
+                {
+                    if (sumMetric.Sum is double doubleSum)
+                    {
+                        valueDisplay = ((double)doubleSum).ToString();
+                    }
+                    else if (sumMetric.Sum is long longSum)
+                    {
+                        valueDisplay = ((long)longSum).ToString();
+                    }
+                }
+                else if (metric is IGaugeMetric gaugeMetric)
+                {
+                    if (gaugeMetric.LastValue.Value is double doubleValue)
+                    {
+                        valueDisplay = ((double)doubleValue).ToString();
+                    }
+                    else if (gaugeMetric.LastValue.Value is long longValue)
+                    {
+                        valueDisplay = ((long)longValue).ToString();
+                    }
+
+                    // Qn: tags again ? gaugeMetric.LastValue.Tags
+                }
+                else if (metric is ISummaryMetric summaryMetric)
+                {
+                    valueDisplay = string.Format("Sum: {0} Count: {1}", summaryMetric.PopulationSum, summaryMetric.PopulationCount);
+                }
+                else if (metric is IHistogramMetric histogramMetric)
+                {
+                    valueDisplay = string.Format("Sum: {0} Count: {1}", histogramMetric.PopulationSum, histogramMetric.PopulationCount);
+                }
+
                 var kind = metric.GetType().Name;
-                var value = metric.ToDisplayString();
 
                 string time = $"{metric.StartTimeExclusive.ToLocalTime().ToString("HH:mm:ss.fff")} {metric.EndTimeInclusive.ToLocalTime().ToString("HH:mm:ss.fff")}";
 
-                var msg = $"Export[{this.name}] {time} {metric.Name} [{string.Join(";", tags)}] {kind} {value}";
+                var msg = $"Export[{this.name}] {time} {metric.Name} [{string.Join(";", tags)}] {kind} Value: {valueDisplay}";
                 Console.WriteLine(msg);
             }
         }
