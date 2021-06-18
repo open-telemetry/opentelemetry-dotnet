3ae78295826d37290a7fdcfdaddc2697d432ebc2
Utkarsh Umesan Pillai <utpilla@microsoft.com>
2021-06-18 16:28:14 -0700

    Update code coverage script

--- build/process-codecoverage.ps1
+++ build/process-codecoverage.ps1
@@ -1,8 +1,11 @@
-﻿$files = Get-ChildItem "TestResults" -Filter "*.coverage" -Recurse
+﻿$commonProps = Get-Content -Path 'Common.props' -Raw
+$commonProps -match '<MicrosoftCodeCoveragePkgVer>\[(.+)\]</MicrosoftCodeCoveragePkgVer>'
+$microsoftCodeCoveragePkgVer = $Matches[1]
+$files = Get-ChildItem "TestResults" -Filter "*.coverage" -Recurse
 Write-Host $env:USERPROFILE
 foreach ($file in $files)
 {
-    $command = $env:USERPROFILE+ '\.nuget\packages\microsoft.codecoverage\16.10.0\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze /output:' + $file.DirectoryName + '\' + $file.Name + '.xml '+ $file.FullName
+    $command = $env:USERPROFILE+ '\.nuget\packages\microsoft.codecoverage\' + $microsoftCodeCoveragePkgVer + '\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze /output:' + $file.DirectoryName + '\' + $file.Name + '.xml '+ $file.FullName
     Write-Host $command
     Invoke-Expression $command
 }
