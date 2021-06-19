8cf8f07abee55b24d604d15d09ab71443a33b204
Utkarsh Umesan Pillai <utpilla@microsoft.com>
2021-06-18 17:11:08 -0700

    Update code coverage script

--- build/process-codecoverage.ps1
+++ build/process-codecoverage.ps1
@@ -1,6 +1,7 @@
-﻿$commonProps = Get-Content -Path 'Common.props' -Raw
-$commonProps -match '<MicrosoftCodeCoveragePkgVer>\[(.+)\]</MicrosoftCodeCoveragePkgVer>'
-$microsoftCodeCoveragePkgVer = $Matches[1]
+﻿[xml]$commonProps = Get-Content -Path $PSScriptRoot\Common.props
+$microsoftCodeCoveragePkgVer = [string]$commonProps.Project.PropertyGroup.MicrosoftCodeCoveragePkgVer # This is collected in the format: "[16.10.0]"
+$microsoftCodeCoveragePkgVer = $microsoftCodeCoveragePkgVer.Trim();
+$microsoftCodeCoveragePkgVer = $microsoftCodeCoveragePkgVer.SubString(1, $microsoftCodeCoveragePkgVer.Length - 2) # Removing square brackets
 $files = Get-ChildItem "TestResults" -Filter "*.coverage" -Recurse
 Write-Host $env:USERPROFILE
 foreach ($file in $files)
