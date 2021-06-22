786bea6b0f1cc1fad8e65bcf4b5f115b5fc853eb
Utkarsh Umesan Pillai <66651184+utpilla@users.noreply.github.com>
2021-06-22 11:53:28 -0700

    Update code coverage script to fetch the MicrosoftCodeCoverage package version from Common.props (#2094)
    Co-authored-by: Cijo Thomas <cithomas@microsoft.com>

--- build/process-codecoverage.ps1
+++ build/process-codecoverage.ps1
@@ -1,8 +1,12 @@
-﻿$files = Get-ChildItem "TestResults" -Filter "*.coverage" -Recurse
+﻿[xml]$commonProps = Get-Content -Path $PSScriptRoot\Common.props
+$microsoftCodeCoveragePkgVer = [string]$commonProps.Project.PropertyGroup.MicrosoftCodeCoveragePkgVer # This is collected in the format: "[16.10.0]"
+$microsoftCodeCoveragePkgVer = $microsoftCodeCoveragePkgVer.Trim();
+$microsoftCodeCoveragePkgVer = $microsoftCodeCoveragePkgVer.SubString(1, $microsoftCodeCoveragePkgVer.Length - 2) # Removing square brackets
+$files = Get-ChildItem "TestResults" -Filter "*.coverage" -Recurse
 Write-Host $env:USERPROFILE
 foreach ($file in $files)
 {
-    $command = $env:USERPROFILE+ '\.nuget\packages\microsoft.codecoverage\16.10.0\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze /output:' + $file.DirectoryName + '\' + $file.Name + '.xml '+ $file.FullName
+    $command = $env:USERPROFILE+ '\.nuget\packages\microsoft.codecoverage\' + $microsoftCodeCoveragePkgVer + '\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze /output:' + $file.DirectoryName + '\' + $file.Name + '.xml '+ $file.FullName
     Write-Host $command
     Invoke-Expression $command
 }
