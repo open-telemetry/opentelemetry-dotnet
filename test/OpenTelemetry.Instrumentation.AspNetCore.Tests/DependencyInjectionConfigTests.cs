d41f85081cde1be788a786e273063260a42914b3
Cijo Thomas <cithomas@microsoft.com>
2021-06-23 18:18:30 -0700

    Fix AspNetCore configuration for .Net Core 3 and above

---
+++ test/OpenTelemetry.Instrumentation.AspNetCore.Tests/DependencyInjectionConfigTests.cs
@@ -0,0 +1,92 @@
+// <copyright file="DependencyInjectionConfigTests.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Newtonsoft.Json;
+using OpenTelemetry.Context.Propagation;
+using OpenTelemetry.Instrumentation.AspNetCore.Implementation;
+using OpenTelemetry.Tests;
+using OpenTelemetry.Trace;
+#if NETCOREAPP2_1
+using TestApp.AspNetCore._2._1;
+#elif NETCOREAPP3_1
+using TestApp.AspNetCore._3._1;
+#else
+using TestApp.AspNetCore._5._0;
+#endif
+using Xunit;
+
+namespace OpenTelemetry.Instrumentation.AspNetCore.Tests
+{
+    public class DependencyInjectionConfigTests
+        : IClassFixture<WebApplicationFactory<Startup>>, IDisposable
+    {
+        private readonly WebApplicationFactory<Startup> factory;
+        private TracerProvider openTelemetrySdk = null;
+
+        public DependencyInjectionConfigTests(WebApplicationFactory<Startup> factory)
+        {
+            this.factory = factory;
+        }
+
+        [Fact]
+        public async Task TestDiConfig()
+        {
+            bool optionsPickedFromDI = false;
+            void ConfigureTestServices(IServiceCollection services)
+            {
+                services.AddOpenTelemetryTracing(
+                    builder =>
+                     {
+                         builder.AddAspNetCoreInstrumentation();
+                     });
+
+                services.Configure<AspNetCoreInstrumentationOptions>(options =>
+                {
+                    optionsPickedFromDI = true;
+                });
+            }
+
+            // Arrange
+            using (var client = this.factory
+                .WithWebHostBuilder(builder =>
+                    builder.ConfigureTestServices(ConfigureTestServices))
+                .CreateClient())
+            {
+                // Act
+                var response = await client.GetAsync("/api/values");
+            }
+
+            Assert.True(optionsPickedFromDI);
+        }
+
+        public void Dispose()
+        {
+            this.openTelemetrySdk?.Dispose();
+        }
+    }
+}
