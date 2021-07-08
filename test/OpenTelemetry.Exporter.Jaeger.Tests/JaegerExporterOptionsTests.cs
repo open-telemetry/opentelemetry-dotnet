84f77668f0fb65514cde437c7e38cfac4d24a5bc
Robert Pająk <pellared@hotmail.com>
2021-07-08 10:03:58 +0200

    Lint

--- test/OpenTelemetry.Exporter.Jaeger.Tests/JaegerExporterOptionsTests.cs
+++ test/OpenTelemetry.Exporter.Jaeger.Tests/JaegerExporterOptionsTests.cs
@@ -1,4 +1,4 @@
-// <copyright file="JaegerExporterTests.cs" company="OpenTelemetry Authors">
+// <copyright file="JaegerExporterOptionsTests.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -69,6 +69,5 @@ namespace OpenTelemetry.Exporter.Jaeger.Tests
             Environment.SetEnvironmentVariable("OTEL_EXPORTER_JAEGER_AGENT_HOST", null);
             Environment.SetEnvironmentVariable("OTEL_EXPORTER_JAEGER_AGENT_PORT", null);
         }
-
     }
 }
