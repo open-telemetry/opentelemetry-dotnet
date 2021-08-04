f954e64e6028ff65df0bc9a18f2bd974a09edb57
Reiley Yang <reyang@microsoft.com>
2021-08-03 23:22:20 -0700

    update copyright info (#2225)

--- src/Microsoft.AspNet.TelemetryCorrelation/Internal/HttpHeaderParser.cs
+++ src/Microsoft.AspNet.TelemetryCorrelation/Internal/HttpHeaderParser.cs
@@ -1,6 +1,18 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
-
+﻿// <copyright file="HttpHeaderParser.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
 namespace Microsoft.AspNet.TelemetryCorrelation
 {
     // Adoptation of code from https://github.com/aspnet/HttpAbstractions/blob/07d115400e4f8c7a66ba239f230805f03a14ee3d/src/Microsoft.Net.Http.Headers/HttpHeaderParser.cs
