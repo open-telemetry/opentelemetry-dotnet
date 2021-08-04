261abaea28a7b9d499e026cc5a166cdd0b86e546
Reiley Yang <reyang@microsoft.com>
2021-08-03 17:23:54 -0700

    initial cleanup

---
+++ test/Microsoft.AspNet.TelemetryCorrelation.Tests/PropertyExtensions.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="PropertyExtensions.cs" company="Microsoft">
+// Copyright (c) .NET Foundation. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// </copyright>
+
+namespace Microsoft.AspNet.TelemetryCorrelation.Tests
+{
+    using System.Reflection;
+
+    internal static class PropertyExtensions
+    {
+        public static object GetProperty(this object obj, string propertyName)
+        {
+            return obj.GetType().GetTypeInfo().GetDeclaredProperty(propertyName)?.GetValue(obj);
+        }
+    }
+}
