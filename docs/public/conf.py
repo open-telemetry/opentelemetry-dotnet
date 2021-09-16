77c3f93e635803a4f55fd8c5a871839842df5c31
Michael Maxwell <micmax@microsoft.com>
2021-09-16 17:40:16 -0400

    Add markdown support

--- docs/public/conf.py
+++ docs/public/conf.py
@@ -30,8 +30,7 @@ release = '1.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['myst_parser']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
