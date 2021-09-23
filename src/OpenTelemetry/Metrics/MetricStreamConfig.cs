485c04ebec8c6c6edf0cf5a1f18fb775f03739f9
Cijo Thomas <cithomas@microsoft.com>
2021-09-23 13:02:26 -0700

    rename

--- src/OpenTelemetry/Metrics/MetricStreamConfig.cs
+++ src/OpenTelemetry/Metrics/MetricStreamConfig.cs
@@ -1,4 +1,4 @@
-// <copyright file="MetricSteamConfig.cs" company="OpenTelemetry Authors">
+// <copyright file="MetricStreamConfig.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
