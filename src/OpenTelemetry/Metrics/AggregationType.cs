e6f1486127ef49008d11c031fdc43111baf4d285
Cijo Thomas <cithomas@microsoft.com>
2021-09-30 11:38:17 -0700

    fixcomment

--- src/OpenTelemetry/Metrics/AggregationType.cs
+++ src/OpenTelemetry/Metrics/AggregationType.cs
@@ -59,7 +59,7 @@ namespace OpenTelemetry.Metrics
         Histogram = 6,
 
         /// <summary>
-        /// Histogram with no sum,count only.
+        /// Histogram with sum,count only.
         /// </summary>
         HistogramSumCount = 7,
     }
