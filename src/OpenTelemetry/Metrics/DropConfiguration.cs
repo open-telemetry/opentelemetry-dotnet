71b566ce485f4b7f36f9d18bb8e8bab6ea78e9c2
Cijo Thomas <cithomas@microsoft.com>
2021-10-05 14:25:31 -0700

    file name fix

--- src/OpenTelemetry/Metrics/DropConfiguration.cs
+++ src/OpenTelemetry/Metrics/DropConfiguration.cs
@@ -1,4 +1,4 @@
-// <copyright file="HistogramConfiguration.cs" company="OpenTelemetry Authors">
+// <copyright file="DropConfiguration.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
