23cf66ef3603a0d6f907eda8aafaced69d56ff6a
Yun-Ting Lin <yunl@microsoft.com>
2021-10-12 15:37:29 -0700

    one more test fix

--- test/OpenTelemetry.Instrumentation.AspNetCore.Tests/MetricTests.cs
+++ test/OpenTelemetry.Instrumentation.AspNetCore.Tests/MetricTests.cs
@@ -69,6 +69,7 @@ namespace OpenTelemetry.Instrumentation.AspNetCore.Tests
             };
             this.meterProvider = Sdk.CreateMeterProviderBuilder()
                 .AddAspNetCoreInstrumentation()
+                .AddMeter("SomeCompany.SomeProject.SomeComponent")
                 .AddReader(metricReader)
                 .Build();
 
