46673db7ea37c923d7fd9c65380a969bcddacd60
Cijo Thomas <cithomas@microsoft.com>
2021-11-22 16:41:08 -0800

    remove unnused using import

--- src/OpenTelemetry/Metrics/ExplicitBucketHistogramConfiguration.cs
+++ src/OpenTelemetry/Metrics/ExplicitBucketHistogramConfiguration.cs
@@ -14,8 +14,6 @@
 // limitations under the License.
 // </copyright>
 
-using System;
-
 namespace OpenTelemetry.Metrics
 {
     public class ExplicitBucketHistogramConfiguration : MetricStreamConfiguration
