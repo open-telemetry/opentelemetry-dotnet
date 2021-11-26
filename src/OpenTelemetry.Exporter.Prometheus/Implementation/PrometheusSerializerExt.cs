500a7e24c56fcd13874704187a2aef244294edc5
Cijo Thomas <cithomas@microsoft.com>
2021-11-26 12:58:19 -0800

    MetricType modification to allow future additions to histogram

--- src/OpenTelemetry.Exporter.Prometheus/Implementation/PrometheusSerializerExt.cs
+++ src/OpenTelemetry.Exporter.Prometheus/Implementation/PrometheusSerializerExt.cs
@@ -23,7 +23,7 @@ namespace OpenTelemetry.Exporter.Prometheus
     /// </summary>
     internal static partial class PrometheusSerializer
     {
-        private static readonly string[] MetricTypes = new string[] { "untyped", "counter", "gauge", "histogram", "summary" };
+        private static readonly string[] MetricTypes = new string[] { "untyped", "counter", "gauge", "untyped", "histogram", "histogram", "histogram", "histogram", "summary" };
 
         public static int WriteMetric(byte[] buffer, int cursor, Metric metric)
         {
@@ -35,7 +35,7 @@ namespace OpenTelemetry.Exporter.Prometheus
             int metricType = (int)metric.MetricType >> 4;
             cursor = WriteTypeInfo(buffer, cursor, metric.Name, metric.Unit, MetricTypes[metricType]);
 
-            if (metric.MetricType != MetricType.Histogram)
+            if (!metric.MetricType.IsHistogram())
             {
                 foreach (ref var metricPoint in metric.GetMetricPoints())
                 {
