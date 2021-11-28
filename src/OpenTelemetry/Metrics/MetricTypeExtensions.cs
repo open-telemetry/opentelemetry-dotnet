c0ed3df843e8c33ba26e85cbac09107263165147
Cijo Thomas <cithomas@microsoft.com>
2021-11-28 11:18:30 -0800

    changelog and pr comment

--- src/OpenTelemetry/Metrics/MetricTypeExtensions.cs
+++ src/OpenTelemetry/Metrics/MetricTypeExtensions.cs
@@ -26,8 +26,8 @@ namespace OpenTelemetry.Metrics
 
         internal const MetricType METRIC_TYPE_SUM = (MetricType)0x10;
         internal const MetricType METRIC_TYPE_GAUGE = (MetricType)0x20;
-        internal const MetricType METRIC_TYPE_HISTOGRAM = (MetricType)0x40;
         /* internal const byte METRIC_TYPE_SUMMARY = 0x30; // not used */
+        internal const MetricType METRIC_TYPE_HISTOGRAM = (MetricType)0x40;
 
         internal const MetricType POINT_KIND_MASK = (MetricType)0x0f;
 
