d00100ec81f13151b7d6e2122d9f512fc7f37a47
Tom Kerkhove <kerkhove.tom@gmail.com>
2021-12-17 01:57:05 +0100

    feat: Fail fast when insecure channel is not configured for .NET Core 3.1 with gRPC (#2691)
    * feat: Fail fast when unsecure channel is not configured for .NET Core 3.1
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Update changelog
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * NotSupportedException -> InvalidOperationException
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Code quality
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Use static class, instead of base class
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Revert change
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Add missing file header
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Change build directive
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Code linting
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Remove redundant check
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Update changelog
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Fix build errors after rebase
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Use previous build directives
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Update changelog with latest RC
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Check for version, instead of build directive
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Align tests
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Change error message
    
    Co-authored-by: Alan West <3676547+alanwest@users.noreply.github.com>
    
    * Change comment
    
    Co-authored-by: Alan West <3676547+alanwest@users.noreply.github.com>
    
    * Change test name
    
    Co-authored-by: Alan West <3676547+alanwest@users.noreply.github.com>
    
    * Change test name
    
    Co-authored-by: Alan West <3676547+alanwest@users.noreply.github.com>
    
    * fix: Build integration test image with SDK of specified .NET version
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * revert: Build in 6.0 due to language version
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Use version check instead of build directive
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Fix formatting
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Fix check on version
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Remove flaky test
    
    * Incorporate @pellared's feedback
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Change code style
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Code review remarks
    
    Signed-off-by: Tom Kerkhove <kerkhove.tom@gmail.com>
    
    * Update changelog
    
    Co-authored-by: Cijo Thomas <cithomas@microsoft.com>
    
    * Update changelog
    
    Co-authored-by: Cijo Thomas <cithomas@microsoft.com>
    
    Co-authored-by: Alan West <3676547+alanwest@users.noreply.github.com>
    Co-authored-by: Cijo Thomas <cithomas@microsoft.com>

--- test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/IntegrationTests.cs
+++ test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/IntegrationTests.cs
@@ -68,5 +68,32 @@ namespace OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests
             Assert.Single(delegatingExporter.ExportResults);
             Assert.Equal(ExportResult.Success, delegatingExporter.ExportResults[0]);
         }
+
+        [Trait("CategoryName", "CollectorIntegrationTests")]
+        [SkipUnlessEnvVarFoundFact(CollectorHostnameEnvVarName)]
+        public void ConstructingGrpcExporterFailsWhenHttp2UnencryptedSupportIsDisabledForNetcoreapp31()
+        {
+            // Adding the OtlpExporter creates a GrpcChannel.
+            // This switch must be set before creating a GrpcChannel/HttpClient when calling an insecure gRPC service.
+            // We want to fail fast so we are disabling it
+            // See: https://docs.microsoft.com/aspnet/core/grpc/troubleshoot#call-insecure-grpc-services-with-net-core-client
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", false);
+
+            var exporterOptions = new OtlpExporterOptions
+            {
+                Endpoint = new Uri($"http://{CollectorHostname}:4317"),
+            };
+
+            var exception = Record.Exception(() => new OtlpTraceExporter(exporterOptions));
+
+            if (Environment.Version.Major == 3)
+            {
+                Assert.NotNull(exception);
+            }
+            else
+            {
+                Assert.Null(exception);
+            }
+        }
     }
 }
