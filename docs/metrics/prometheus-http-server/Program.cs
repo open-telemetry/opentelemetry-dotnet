7db8456724ac26e5b702317211d3956022a61dd5
Yun-Ting Lin <yunl@microsoft.com>
2022-01-13 10:13:00 -0800

    examples

---
+++ docs/metrics/prometheus-http-server/Program.cs
@@ -0,0 +1,51 @@
+// <copyright file="Program.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System;
+using System.Diagnostics.Metrics;
+using System.Threading;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+
+public class Program
+{
+    private static readonly Meter MyMeter = new Meter("MyCompany.MyProduct.MyLibrary", "1.0");
+    private static readonly Counter<long> MyFruitCounter = MyMeter.CreateCounter<long>("MyFruitCounter");
+
+    public static void Main(string[] args)
+    {
+        using var meterProvider = Sdk.CreateMeterProviderBuilder()
+            .AddMeter("MyCompany.MyProduct.MyLibrary")
+            .AddPrometheusExporter(opt =>
+            {
+                opt.StartHttpListener = true;
+                opt.HttpListenerPrefixes = new string[] { $"http://localhost:9184/" };
+            })
+            .Build();
+
+        Console.WriteLine("Press any key to exit");
+        while (!Console.KeyAvailable)
+        {
+            Thread.Sleep(1000);
+            MyFruitCounter.Add(1, new("name", "apple"), new("color", "red"));
+            MyFruitCounter.Add(2, new("name", "lemon"), new("color", "yellow"));
+            MyFruitCounter.Add(1, new("name", "lemon"), new("color", "yellow"));
+            MyFruitCounter.Add(2, new("name", "apple"), new("color", "green"));
+            MyFruitCounter.Add(5, new("name", "apple"), new("color", "red"));
+            MyFruitCounter.Add(4, new("name", "lemon"), new("color", "yellow"));
+        }
+    }
+}
