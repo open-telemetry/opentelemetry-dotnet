4842673c8c4a3a11cd2e5dd803a6ea7106e297ee
Cijo Thomas <cithomas@microsoft.com>
2022-01-19 12:55:11 -0800

    sample outout

--- test/Benchmarks/Logs/LogBenchmarks.cs
+++ test/Benchmarks/Logs/LogBenchmarks.cs
@@ -19,6 +19,25 @@ using Microsoft.Extensions.Logging;
 using OpenTelemetry;
 using OpenTelemetry.Logs;
 
+/*
+// * Summary *
+
+BenchmarkDotNet = v0.13.1, OS = Windows 10.0.22000
+Intel Core i7-8650U CPU 1.90GHz (Kaby Lake R), 1 CPU, 8 logical and 4 physical cores
+.NET SDK=6.0.101
+  [Host]     : .NET 6.0.1(6.0.121.56705), X64 RyuJIT
+  DefaultJob : .NET 6.0.1(6.0.121.56705), X64 RyuJIT
+
+
+|                                 Method |     Mean |    Error |   StdDev |   Median |  Gen 0 | Allocated |
+|--------------------------------------- |---------:|---------:|---------:|---------:|-------:|----------:|
+|                             NoListener | 135.6 ns | 10.90 ns | 32.15 ns | 125.7 ns | 0.0153 |      64 B |
+|                           OneProcessor | 251.9 ns | 16.29 ns | 46.73 ns | 241.9 ns | 0.0553 |     232 B |
+| OneProcessorWithLoggerMessageGenerator | 216.6 ns |  9.48 ns | 27.65 ns | 213.6 ns | 0.0401 |     168 B |
+|                          TwoProcessors | 319.6 ns | 24.42 ns | 70.86 ns | 314.4 ns | 0.0553 |     232 B |
+|                        ThreeProcessors | 289.2 ns | 21.75 ns | 62.76 ns | 274.4 ns | 0.0553 |     232 B |
+*/
+
 namespace Benchmarks.Logs
 {
     [MemoryDiagnoser]
