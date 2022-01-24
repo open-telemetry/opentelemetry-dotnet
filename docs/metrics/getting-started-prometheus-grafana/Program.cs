8eda0afa7792a0457c8c115087823d6010d25b56
Yun-Ting Lin <yunl@microsoft.com>
2022-01-24 09:56:54 -0800

    fix library and update project file

--- docs/metrics/getting-started-prometheus-grafana/Program.cs
+++ docs/metrics/getting-started-prometheus-grafana/Program.cs
@@ -14,7 +14,9 @@
 // limitations under the License.
 // </copyright>
 
+using System;
 using System.Diagnostics.Metrics;
+using System.Threading;
 using OpenTelemetry;
 using OpenTelemetry.Metrics;
 
