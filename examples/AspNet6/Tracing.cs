4c8c0bf9c746ef0023982ed0f31cb12560a9de2e
Xavier <xavierjohn@hotmail.com>
2022-02-06 14:00:53 -0800

    Match header to filename.

--- examples/AspNet6/Tracing.cs
+++ examples/AspNet6/Tracing.cs
@@ -1,4 +1,4 @@
-// <copyright file="TracingConstants.cs" company="OpenTelemetry Authors">
+// <copyright file="Tracing.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
