058230b099ae98f1323248c30ae9702fe77fb92e
Cijo Thomas <cithomas@microsoft.com>
2022-02-15 19:09:40 -0800

    Minor test namespace adjustment (#2903)

--- test/OpenTelemetry.Tests/Internal/GuardTest.cs
+++ test/OpenTelemetry.Tests/Internal/GuardTest.cs
@@ -16,10 +16,9 @@
 
 using System;
 using System.Threading;
-using OpenTelemetry.Internal;
 using Xunit;
 
-namespace OpenTelemetry.Tests.Internal
+namespace OpenTelemetry.Internal.Tests
 {
 #pragma warning disable SA1402 // File may only contain a single type
 #pragma warning disable SA1649 // File name should match first type name
