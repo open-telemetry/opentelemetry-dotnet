065aadbc2370a968780d58e2f2bba315a4d5c5a7
Yun-Ting Lin <yunl@microsoft.com>
2022-02-25 16:50:08 -0800

    revert

--- src/OpenTelemetry/Resources/Resource.cs
+++ src/OpenTelemetry/Resources/Resource.cs
@@ -35,7 +35,7 @@ namespace OpenTelemetry.Resources
         /// Initializes a new instance of the <see cref="Resource"/> class.
         /// </summary>
         /// <param name="attributes">An <see cref="IEnumerable{T}"/> of attributes that describe the resource.</param>
-        public Resource(IEnumerable<KeyValuePair<string, object>> attributes)
+        internal Resource(IEnumerable<KeyValuePair<string, object>> attributes)
         {
             if (attributes == null)
             {
