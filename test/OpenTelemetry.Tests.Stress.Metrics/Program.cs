2e5f6c229423676e5e3368073839347d0d1170eb
Utkarsh Umesan Pillai <utpilla@microsoft.com>
2022-03-01 18:09:32 -0800

    Address PR comments

--- test/OpenTelemetry.Tests.Stress.Metrics/Program.cs
+++ test/OpenTelemetry.Tests.Stress.Metrics/Program.cs
@@ -26,6 +26,7 @@ public partial class Program
 {
     private const int ArraySize = 10;
 
+    // Note: Uncomment the below line if you want to run Histogram stress test
     // private const int MaxHistogramMeasurement = 1000;
 
     private static readonly Meter TestMeter = new Meter(Utils.GetCurrentMethodName());
@@ -33,6 +34,7 @@ public partial class Program
     private static readonly string[] DimensionValues = new string[ArraySize];
     private static readonly ThreadLocal<Random> ThreadLocalRandom = new ThreadLocal<Random>(() => new Random());
 
+    // Note: Uncomment the below line if you want to run Histogram stress test
     // private static readonly Histogram<long> TestHistogram = TestMeter.CreateHistogram<long>("TestHistogram");
 
     public static void Main()
@@ -66,6 +68,7 @@ public partial class Program
             new("DimName3", DimensionValues[random.Next(0, ArraySize)]));
     }
 
+    // Note: Uncomment the below lines if you want to run Histogram stress test
     // [MethodImpl(MethodImplOptions.AggressiveInlining)]
     // protected static void Run()
     // {
