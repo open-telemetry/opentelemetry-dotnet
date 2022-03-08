49903035ecb5f99dd9df8072139e6cc9c1998d0e
Alan West <3676547+alanwest@users.noreply.github.com>
2022-03-07 16:46:26 -0800

    Set Http2UnencryptedSupport switch for netcoreapp3.1

--- test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/OtlpMetricsExporterTests.cs
+++ test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/OtlpMetricsExporterTests.cs
@@ -35,6 +35,14 @@ namespace OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests
         [Fact]
         public void TestAddOtlpExporter_SetsCorrectMetricReaderDefaults()
         {
+            if (Environment.Version.Major == 3)
+            {
+                // Adding the OtlpExporter creates a GrpcChannel.
+                // This switch must be set before creating a GrpcChannel when calling an insecure HTTP/2 endpoint.
+                // See: https://docs.microsoft.com/aspnet/core/grpc/troubleshoot#call-insecure-grpc-services-with-net-core-client
+                AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+            }
+
             var meterProvider = Sdk.CreateMeterProviderBuilder()
                 .AddOtlpExporter()
                 .Build();
