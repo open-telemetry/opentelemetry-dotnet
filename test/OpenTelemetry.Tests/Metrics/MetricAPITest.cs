acfcab42bf2384ee32bb88459341dbf47f44ac53
Alan West <3676547+alanwest@users.noreply.github.com>
2022-03-10 16:32:41 -0800

    Unused using

--- test/OpenTelemetry.Tests/Metrics/MetricAPITest.cs
+++ test/OpenTelemetry.Tests/Metrics/MetricAPITest.cs
@@ -18,7 +18,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.Metrics;
-using System.Linq;
 using System.Threading;
 using OpenTelemetry.Tests;
 using Xunit;
