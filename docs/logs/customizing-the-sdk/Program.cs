752f6fd09da02d7d9c21c8178667549c02e2a46f
TimothyMothra <tilee@microsoft.com>
2022-03-14 11:06:23 -0700

    includescopes

--- docs/logs/customizing-the-sdk/Program.cs
+++ docs/logs/customizing-the-sdk/Program.cs
@@ -26,6 +26,7 @@ public class Program
         {
             builder.AddOpenTelemetry(options =>
             {
+                options.IncludeScopes = true;
                 options.AddConsoleExporter();
             });
         });
@@ -35,5 +36,12 @@ public class Program
         logger.LogInformation("Hello Information");
         logger.LogWarning("Hello Warning");
         logger.LogError("Hello Error");
+
+        // TESTING options.IncludeScopes
+        using (logger.BeginScope("My Scope 1"))
+        using (logger.BeginScope("My Scope 2"))
+        {
+            logger.LogInformation("Hello Information within scope");
+        }
     }
 }
