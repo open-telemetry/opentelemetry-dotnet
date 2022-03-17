ec74b888348320a6f2a2527cb6c3653a3098e1bf
Yun-Ting Lin <yunl@microsoft.com>
2022-03-17 14:20:37 -0700

    update

--- src/OpenTelemetry.Exporter.Console/ConsoleActivityExporter.cs
+++ src/OpenTelemetry.Exporter.Console/ConsoleActivityExporter.cs
@@ -86,7 +86,7 @@ namespace OpenTelemetry.Exporter
                     this.WriteLine($"StatusCode : {activity.Status}");
                     if (!string.IsNullOrEmpty(activity.StatusDescription))
                     {
-                        this.WriteLine($"Status Description : {activity.StatusDescription}");
+                        this.WriteLine($"Activity.StatusDescription : {activity.StatusDescription}");
                     }
                 }
                 else if (!string.IsNullOrEmpty(statusCode))
@@ -94,7 +94,7 @@ namespace OpenTelemetry.Exporter
                     this.WriteLine($"   StatusCode : {statusCode}");
                     if (!string.IsNullOrEmpty(statusDesc))
                     {
-                        this.WriteLine($"   Status Description : {statusDesc}");
+                        this.WriteLine($"   Activity.StatusDescription : {statusDesc}");
                     }
                 }
 
