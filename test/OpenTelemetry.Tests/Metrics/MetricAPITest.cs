866535e2c4318188d3cd8698beb218205bef5a1f
Alan West <3676547+alanwest@users.noreply.github.com>
2022-03-23 10:46:48 -0700

    Two instruments with view matching only one of them

--- test/OpenTelemetry.Tests/Metrics/MetricAPITest.cs
+++ test/OpenTelemetry.Tests/Metrics/MetricAPITest.cs
@@ -652,6 +652,58 @@ namespace OpenTelemetry.Metrics.Tests
         }
 
         [Fact]
+        public void DuplicateInstrumentRegistration_WithViews_TwoInstruments_OneView_ResultsInConflict()
+        {
+            var exportedItems = new List<Metric>();
+
+            using var meter = new Meter($"{Utils.GetCurrentMethodName()}");
+            var meterProviderBuilder = Sdk.CreateMeterProviderBuilder()
+                .AddMeter(meter.Name)
+                .AddView((instrument) =>
+                {
+                    if (instrument.Name == "name")
+                    {
+                        return new MetricStreamConfiguration { Name = "othername", TagKeys = new[] { "key1" } };
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                })
+                .AddInMemoryExporter(exportedItems);
+
+            using var meterProvider = meterProviderBuilder.Build();
+
+            var instrument1 = meter.CreateCounter<long>("name");
+            var instrument2 = meter.CreateCounter<long>("othername");
+
+            var tags = new KeyValuePair<string, object>[]
+            {
+                new("key1", "value"),
+                new("key2", "value"),
+            };
+
+            instrument1.Add(10, tags);
+            instrument2.Add(10, tags);
+
+            meterProvider.ForceFlush(MaxTimeToAllowForFlush);
+
+            // The view only matches one instrument but renames it to the name
+            // of the other instrument, so only one stream is produced
+            Assert.Equal(1, exportedItems.Count);
+            var metric1 = new List<Metric>() { exportedItems[0] };
+
+            var tag1 = new List<KeyValuePair<string, object>> { tags[0] };
+
+            Assert.Equal("name", exportedItems[0].Name);
+
+            // Both instruments record, tags get filtered even though second instrument
+            // did not match view
+            Assert.Equal(20, GetLongSum(metric1));
+            CheckTagsForNthMetricPoint(metric1, tag1, 1);
+        }
+
+        [Fact]
         public void DuplicateInstrumentNamesFromDifferentMetersWithSameNameDifferentVersion()
         {
             var exportedItems = new List<Metric>();
