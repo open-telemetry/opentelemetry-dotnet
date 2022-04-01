d8efbce00443532e8c24df1fc7beedbeee57f704
Alan West <3676547+alanwest@users.noreply.github.com>
2022-04-01 16:11:55 -0700

    Reintroduce message in correct spot

--- src/OpenTelemetry/Metrics/MetricReaderExt.cs
+++ src/OpenTelemetry/Metrics/MetricReaderExt.cs
@@ -156,6 +156,15 @@ namespace OpenTelemetry.Metrics
                         continue;
                     }
 
+                    if (this.metricStreamNames.Contains(metricStreamName))
+                    {
+                        OpenTelemetrySdkEventSource.Log.DuplicateMetricInstrument(
+                            metricName,
+                            meterName,
+                            "Metric instrument has the same name as an existing one but differs by description, unit, or instrument type. Measurements from this instrument will still be exported but may result in conflicts.",
+                            "Either change the name of the instrument or use MeterProviderBuilder.AddView to resolve the conflict.");
+                    }
+
                     var index = ++this.metricIndex;
                     if (index >= this.maxMetricStreams)
                     {
