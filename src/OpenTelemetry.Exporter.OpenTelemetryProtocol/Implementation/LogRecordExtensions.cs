1bc4adf2ecaa3977f0666a2b2c8e813ab522a579
Cijo Thomas <cithomas@microsoft.com>
2022-04-21 22:02:21 -0700

    comment

--- src/OpenTelemetry.Exporter.OpenTelemetryProtocol/Implementation/LogRecordExtensions.cs
+++ src/OpenTelemetry.Exporter.OpenTelemetryProtocol/Implementation/LogRecordExtensions.cs
@@ -77,6 +77,11 @@ namespace OpenTelemetry.Exporter.OpenTelemetryProtocol.Implementation
 
                 if (!string.IsNullOrEmpty(logRecord.CategoryName))
                 {
+                    // TODO:
+                    // 1. Track the following issue, and map CategoryName to Name
+                    // if it makes it to log data model.
+                    // https://github.com/open-telemetry/opentelemetry-specification/issues/2398
+                    // 2. Confirm if this name for attribute is good.
                     otlpLogRecord.Attributes.AddStringAttribute("dotnet.ilogger.category", logRecord.CategoryName);
                 }
 
--- test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/OtlpLogExporterTests.cs
+++ test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/OtlpLogExporterTests.cs
@@ -17,7 +17,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
