4b4fcbe9525d94839fce665616376d565d3c3be5
Vishwesh Bankwar <vibankwa@microsoft.com>
2022-05-06 14:06:41 -0700

    fix test

--- test/OpenTelemetry.Instrumentation.AspNetCore.Tests/MetricTests.cs
+++ test/OpenTelemetry.Instrumentation.AspNetCore.Tests/MetricTests.cs
@@ -116,7 +116,7 @@ namespace OpenTelemetry.Instrumentation.AspNetCore.Tests
 
             var method = new KeyValuePair<string, object>(SemanticConventions.AttributeHttpMethod, "GET");
             var scheme = new KeyValuePair<string, object>(SemanticConventions.AttributeHttpScheme, "http");
-            var statusCode = new KeyValuePair<string, object>(SemanticConventions.AttributeHttpStatusCode, 200);
+            var statusCode = new KeyValuePair<string, object>(SemanticConventions.AttributeHttpStatusCode, "200");
             var flavor = new KeyValuePair<string, object>(SemanticConventions.AttributeHttpFlavor, "HTTP/1.1");
             var host = new KeyValuePair<string, object>(SemanticConventions.AttributeHttpHost, "localhost");
             var target = new KeyValuePair<string, object>(SemanticConventions.AttributeHttpTarget, "api/Values");
