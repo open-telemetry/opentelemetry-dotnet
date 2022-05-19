957380e3568b868f9e6b8de415958adbc6644d60
Mikel Blanchard <mblanchard@macrosssoftware.com>
2022-05-19 10:51:22 -0700

    Benchmark tweaks and fixes.

--- test/Benchmarks/Context/PropagationBenchmarks.cs
+++ test/Benchmarks/Context/PropagationBenchmarks.cs
@@ -29,42 +29,42 @@ namespace Benchmarks.Context
         private static readonly Func<HttpRequest, string, IEnumerable<string>> CarrierGetter = (request, name) => request.Headers[name];
         private static readonly Action<object, string, string> CarrierSetter = (request, name, value) => { };
         private readonly TraceContextPropagator traceContextPropagator = new();
-        private readonly TextMapPropagator compositePropagator = new CompositeTextMapPropagator(new TextMapPropagator[] { new TraceContextPropagator(), new BaggagePropagator(), new B3Propagator() });
+        private readonly B3Propagator b3Propagator = new();
         private readonly object injectRequest = new();
+        private readonly HttpRequest emptyExtractRequest;
+        private readonly HttpRequest w3cExtractRequest;
+        private readonly HttpRequest b3ExtractRequest;
+
+        private readonly TextMapPropagator compositePropagator = new CompositeTextMapPropagator(
+            new TextMapPropagator[] { new TraceContextPropagator(), new BaggagePropagator(), new B3Propagator() });
+
         private readonly PropagationContext injectContext = new(
             new ActivityContext(ActivityTraceId.CreateRandom(), ActivitySpanId.CreateRandom(), ActivityTraceFlags.Recorded),
             default);
 
-        private HttpRequest extractRequest;
-
-        [Params("Empty", "W3C", "B3")]
-        public string Mode { get; set; }
-
-        [GlobalSetup]
-        public void GlobalSetup()
+        public PropagationBenchmarks()
         {
-            HttpContext context = new DefaultHttpContext();
+            HttpContext emptyContext = new DefaultHttpContext();
+            this.emptyExtractRequest = emptyContext.Request;
 
-            this.extractRequest = context.Request;
+            HttpContext w3cContext = new DefaultHttpContext();
+            this.w3cExtractRequest = w3cContext.Request;
+            this.w3cExtractRequest.Headers.Add(TraceContextPropagator.TraceParent, "00-0af7651916cd43dd8448eb211c80319c-00f067aa0ba902b7-00");
 
-            switch (this.Mode)
-            {
-                case "W3C":
-                    this.extractRequest.Headers.Add(TraceContextPropagator.TraceParent, "00-0af7651916cd43dd8448eb211c80319c-00f067aa0ba902b7-00");
-                    break;
-                case "B3":
-                    this.extractRequest.Headers.Add(B3Propagator.XB3TraceId, "ff000000000000000000000000000041");
-                    this.extractRequest.Headers.Add(B3Propagator.XB3SpanId, "ff00000000000041");
-                    this.extractRequest.Headers.Add(B3Propagator.XB3Sampled, "1");
-                    break;
-            }
+            HttpContext b3Context = new DefaultHttpContext();
+            this.b3ExtractRequest = b3Context.Request;
+            this.b3ExtractRequest.Headers.Add(B3Propagator.XB3TraceId, "ff000000000000000000000000000041");
+            this.b3ExtractRequest.Headers.Add(B3Propagator.XB3SpanId, "ff00000000000041");
+            this.b3ExtractRequest.Headers.Add(B3Propagator.XB3Sampled, "1");
         }
 
         [Benchmark]
-        public void TraceContextPropagator_ExtractBenchmark()
+        [Arguments("Empty")]
+        [Arguments("W3C")]
+        public void TraceContextPropagator_ExtractBenchmark(string mode)
         {
             PropagationContext context = default;
-            this.traceContextPropagator.Extract(ref context, this.extractRequest, CarrierGetter);
+            this.traceContextPropagator.Extract(ref context, mode == "Empty" ? this.emptyExtractRequest : this.w3cExtractRequest, CarrierGetter);
         }
 
         [Benchmark]
@@ -74,16 +74,41 @@ namespace Benchmarks.Context
         }
 
         [Benchmark]
-        public void CompositeTextMapPropagator_ExtractBenchmark()
+        [Arguments("Empty")]
+        [Arguments("B3")]
+        public void B3Propagator_ExtractBenchmark(string mode)
         {
             PropagationContext context = default;
-            this.compositePropagator.Extract(ref context, this.extractRequest, CarrierGetter);
+            this.b3Propagator.Extract(ref context, mode == "Empty" ? this.emptyExtractRequest : this.b3ExtractRequest, CarrierGetter);
+        }
+
+        [Benchmark]
+        public void B3Propagator_InjectBenchmark()
+        {
+            this.b3Propagator.Inject(in this.injectContext, this.injectRequest, CarrierSetter);
+        }
+
+        [Benchmark]
+        [Arguments("Empty")]
+        [Arguments("W3C")]
+        [Arguments("B3")]
+        public void CompositeTextMapPropagator_ExtractBenchmark(string mode)
+        {
+            var request = mode switch
+            {
+                "W3C" => this.w3cExtractRequest,
+                "B3" => this.b3ExtractRequest,
+                _ => this.emptyExtractRequest,
+            };
+
+            PropagationContext context = default;
+            this.compositePropagator.Extract(ref context, request, CarrierGetter);
         }
 
         [Benchmark]
         public void CompositeTextMapPropagator_InjectBenchmark()
         {
-            this.traceContextPropagator.Inject(in this.injectContext, this.injectRequest, CarrierSetter);
+            this.compositePropagator.Inject(in this.injectContext, this.injectRequest, CarrierSetter);
         }
     }
 }
