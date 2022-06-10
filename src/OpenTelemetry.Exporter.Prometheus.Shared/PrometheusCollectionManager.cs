a166d41c04079055986e9b5469b5093a4d0135e9
Yun-Ting Lin <yunl@microsoft.com>
2022-06-10 15:44:22 -0700

    header

--- src/OpenTelemetry.Exporter.Prometheus.Shared/PrometheusCollectionManager.cs
+++ src/OpenTelemetry.Exporter.Prometheus.Shared/PrometheusCollectionManager.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using OpenTelemetry.Exporter.Prometheus.HTTP.Server;
