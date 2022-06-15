cd3071be5d67cfd6cdeb593850e40b7e90a2f121
Vishwesh Bankwar <vibankwa@microsoft.com>
2022-06-15 11:31:43 -0700

    add net7.0 target for aspnetcore

--- test/OpenTelemetry.Instrumentation.AspNetCore.Tests/BasicTests.cs
+++ test/OpenTelemetry.Instrumentation.AspNetCore.Tests/BasicTests.cs
@@ -38,6 +38,9 @@ using TestApp.AspNetCore._3._1;
 #if NET6_0
 using TestApp.AspNetCore._6._0;
 #endif
+#if NET7_0
+using TestApp.AspNetCore._7._0;
+#endif
 using Xunit;
 
 namespace OpenTelemetry.Instrumentation.AspNetCore.Tests
