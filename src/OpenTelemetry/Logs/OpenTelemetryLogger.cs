2cd2d1ab96a9d7aa7ce030e3c86dd37bb9e25da6
Mikel Blanchard <mblanchard@macrosssoftware.com>
2022-06-18 10:44:10 -0700

    Tweak.

--- src/OpenTelemetry/Logs/OpenTelemetryLogger.cs
+++ src/OpenTelemetry/Logs/OpenTelemetryLogger.cs
@@ -59,7 +59,7 @@ namespace OpenTelemetry.Logs
 
                 record.ScopeProvider = provider.IncludeScopes ? this.ScopeProvider : null;
                 record.State = provider.ParseStateValues ? null : state;
-                record.StateValues = provider.ParseStateValues ? this.ParseState(record, state) : null;
+                record.StateValues = provider.ParseStateValues ? ParseState(record, state) : null;
 
                 ref LogRecordData data = ref record.Data;
 
@@ -90,7 +90,7 @@ namespace OpenTelemetry.Logs
 
         public IDisposable BeginScope<TState>(TState state) => this.ScopeProvider?.Push(state) ?? NullScope.Instance;
 
-        private IReadOnlyList<KeyValuePair<string, object?>> ParseState<TState>(LogRecord logRecord, TState state)
+        private static IReadOnlyList<KeyValuePair<string, object?>> ParseState<TState>(LogRecord logRecord, TState state)
         {
             if (state is IReadOnlyList<KeyValuePair<string, object?>> stateList)
             {
