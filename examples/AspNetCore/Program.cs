7da684d271151d8c510d0cc08da9f030691bb078
Cijo Thomas <cithomas@microsoft.com>
2022-06-24 19:24:08 -0700

    Add runtime metrics to example aspnet core app

--- examples/AspNetCore/Program.cs
+++ examples/AspNetCore/Program.cs
@@ -112,13 +112,16 @@ builder.Services.Configure<OpenTelemetryLoggerOptions>(opt =>
 });
 
 // Metrics
+
+var metricsExporter = builder.Configuration.GetValue<string>("UseMetricsExporter").ToLowerInvariant();
+
 builder.Services.AddOpenTelemetryMetrics(options =>
 {
     options.SetResourceBuilder(resourceBuilder)
+        .AddRuntimeMetrics()
         .AddHttpClientInstrumentation()
         .AddAspNetCoreInstrumentation();
 
-    var metricsExporter = builder.Configuration.GetValue<string>("UseMetricsExporter").ToLowerInvariant();
     switch (metricsExporter)
     {
         case "prometheus":
@@ -159,9 +162,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-var metricsExporter = builder.Configuration.GetValue<string>("UseMetricsExporter").ToLowerInvariant();
-
-if (metricsExporter == "prometheus")
+if (metricsExporter.Equals("prometheus", StringComparison.OrdinalIgnoreCase))
 {
     app.UseOpenTelemetryPrometheusScrapingEndpoint();
 }
