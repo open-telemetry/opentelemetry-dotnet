09582555c569bd076b03657a1d8e7a963f2d88b4
Rasmus Kuusmann <rkuusmann@splunk.com>
2022-06-27 14:06:22 +0300

    fix test accessibility

--- src/OpenTelemetry.Api/AssemblyInfo.cs
+++ src/OpenTelemetry.Api/AssemblyInfo.cs
@@ -18,6 +18,7 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("OpenTelemetry" + AssemblyInfo.PublicKey)]
 [assembly: InternalsVisibleTo("OpenTelemetry.Tests" + AssemblyInfo.PublicKey)]
+[assembly: InternalsVisibleTo("OpenTelemetry.Extensions.Hosting.Tests" + AssemblyInfo.PublicKey)]
 [assembly: InternalsVisibleTo("OpenTelemetry.Shims.OpenTracing.Tests" + AssemblyInfo.PublicKey)]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2" + AssemblyInfo.MoqPublicKey)]
 
