c9f3a3d151aab4e5963165cbeb92f44b9788eb72
Michael Maxwell <micmax@microsoft.com>
2022-07-05 14:38:31 -0700

    remove stress test change

--- test/OpenTelemetry.Tests.Stress.Metrics/Program.cs
+++ test/OpenTelemetry.Tests.Stress.Metrics/Program.cs
@@ -24,28 +24,24 @@ namespace OpenTelemetry.Tests.Stress;
 
 public partial class Program
 {
-    private const int ArraySize = 49;
-    private const int MaxValue = 1000;
-    private const int MaxValue = int.MaxValue;
+    private const int ArraySize = 10;
+
+    // Note: Uncomment the below line if you want to run Histogram stress test
+    // private const int MaxHistogramMeasurement = 1000;
 
     private static readonly Meter TestMeter = new(Utils.GetCurrentMethodName());
+    private static readonly Counter<long> TestCounter = TestMeter.CreateCounter<long>("TestCounter");
     private static readonly string[] DimensionValues = new string[ArraySize];
     private static readonly ThreadLocal<Random> ThreadLocalRandom = new(() => new Random());
 
-    private static readonly Counter<long> TestCounter = TestMeter.CreateCounter<long>("TestCounter");
-    private static readonly Histogram<long> TestHistogram = TestMeter.CreateHistogram<long>("TestHistogram");
+    // Note: Uncomment the below line if you want to run Histogram stress test
+    // private static readonly Histogram<long> TestHistogram = TestMeter.CreateHistogram<long>("TestHistogram");
 
     public static void Main()
     {
         for (int i = 0; i < ArraySize; i++)
         {
             DimensionValues[i] = $"DimValue{i}";
-        }
-
-        var bounds = new double[ArraySize];
-        for (int i = 0; i < bounds.Length; i++)
-        {
-            bounds[i] = i * (MaxValue / bounds.Length);
         }
 
         using var meterProvider = Sdk.CreateMeterProviderBuilder()
@@ -56,7 +52,6 @@ public partial class Program
                 options.HttpListenerPrefixes = new string[] { $"http://localhost:9185/" };
                 options.ScrapeResponseCacheDurationMilliseconds = 0;
             })
-            .AddView(TestHistogram.Name, new ExplicitBucketHistogramConfiguration() { Boundaries = bounds })
             .Build();
 
         Stress(prometheusPort: 9184);
@@ -66,12 +61,22 @@ public partial class Program
     protected static void Run()
     {
         var random = ThreadLocalRandom.Value;
-        /*TestCounter.Add(
+        TestCounter.Add(
             100,
             new("DimName1", DimensionValues[random.Next(0, ArraySize)]),
             new("DimName2", DimensionValues[random.Next(0, ArraySize)]),
-            new("DimName3", DimensionValues[random.Next(0, ArraySize)]));*/
-
-        TestHistogram.Record(random.Next(MaxValue));
+            new("DimName3", DimensionValues[random.Next(0, ArraySize)]));
     }
+
+    // Note: Uncomment the below lines if you want to run Histogram stress test
+    // [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    // protected static void Run()
+    // {
+    //    var random = ThreadLocalRandom.Value;
+    //    TestHistogram.Record(
+    //        random.Next(MaxHistogramMeasurement),
+    //        new("DimName1", DimensionValues[random.Next(0, ArraySize)]),
+    //        new("DimName2", DimensionValues[random.Next(0, ArraySize)]),
+    //        new("DimName3", DimensionValues[random.Next(0, ArraySize)]));
+    // }
 }
