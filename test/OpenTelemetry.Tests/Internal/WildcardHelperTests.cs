11c65d0918dead343bc765c9f9751aace7b4dbec
glucaci <gabriel.lucaci.dev@gmail.com>
2022-07-14 09:18:16 +0200

    Fix file header

--- test/OpenTelemetry.Tests/Internal/WildcardHelperTests.cs
+++ test/OpenTelemetry.Tests/Internal/WildcardHelperTests.cs
@@ -1,4 +1,4 @@
-// <copyright file="WildcardTests.cs" company="OpenTelemetry Authors">
+// <copyright file="WildcardHelperTests.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
