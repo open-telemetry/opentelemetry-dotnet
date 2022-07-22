7446ec09b58f106d40f86e26d61a48233886be6e
Mikel Blanchard <mblanchard@macrosssoftware.com>
2022-07-21 17:16:37 -0700

    Test improvements.

--- test/OpenTelemetry.Extensions.EventSource.Tests/OpenTelemetryEventSourceLogEmitterTests.cs
+++ test/OpenTelemetry.Extensions.EventSource.Tests/OpenTelemetryEventSourceLogEmitterTests.cs
@@ -16,7 +16,10 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Diagnostics.Tracing;
+using System.Globalization;
+using Microsoft.Extensions.Logging;
 using OpenTelemetry.Logs;
 using Xunit;
 
@@ -56,9 +59,10 @@ namespace OpenTelemetry.Extensions.EventSource.Tests
         }
 
         [Theory]
-        [InlineData("OpenTelemetry.Extensions.EventSource.Tests", true)]
-        [InlineData("_invalid_", false)]
-        public void OpenTelemetryEventSourceLogEmitterFilterTests(string sourceName, bool shouldCaptureMessages)
+        [InlineData("OpenTelemetry.Extensions.EventSource.Tests", EventLevel.LogAlways, 2)]
+        [InlineData("OpenTelemetry.Extensions.EventSource.Tests", EventLevel.Warning, 1)]
+        [InlineData("_invalid_", EventLevel.LogAlways, 0)]
+        public void OpenTelemetryEventSourceLogEmitterFilterTests(string sourceName, EventLevel? eventLevel, int expectedNumberOfLogRecords)
         {
             List<LogRecord> exportedItems = new();
 
@@ -71,19 +75,13 @@ namespace OpenTelemetry.Extensions.EventSource.Tests
 
             using (var openTelemetryEventSourceLogEmitter = new OpenTelemetryEventSourceLogEmitter(
                 openTelemetryLoggerProvider,
-                (name) => name == sourceName ? EventLevel.LogAlways : null))
+                (name) => name == sourceName ? eventLevel : null))
             {
                 TestEventSource.Log.SimpleEvent();
+                TestEventSource.Log.ComplexEvent("Test_Message", 18);
             }
 
-            if (shouldCaptureMessages)
-            {
-                Assert.Single(exportedItems);
-            }
-            else
-            {
-                Assert.Empty(exportedItems);
-            }
+            Assert.Equal(expectedNumberOfLogRecords, exportedItems.Count);
         }
 
         [Fact]
@@ -108,6 +106,134 @@ namespace OpenTelemetry.Extensions.EventSource.Tests
             }
 
             Assert.Single(exportedItems);
+        }
+
+        [Fact]
+        public void OpenTelemetryEventSourceLogEmitterSimpleEventTest()
+        {
+            List<LogRecord> exportedItems = new();
+
+#pragma warning disable CA2000 // Dispose objects before losing scope
+            var openTelemetryLoggerProvider = new WrappedOpenTelemetryLoggerProvider(options =>
+            {
+                options.AddInMemoryExporter(exportedItems);
+            });
+#pragma warning restore CA2000 // Dispose objects before losing scope
+
+            using (var openTelemetryEventSourceLogEmitter = new OpenTelemetryEventSourceLogEmitter(
+                openTelemetryLoggerProvider,
+                (name) => name == "OpenTelemetry.Extensions.EventSource.Tests" ? EventLevel.LogAlways : null))
+            {
+                TestEventSource.Log.SimpleEvent();
+            }
+
+            Assert.Single(exportedItems);
+
+            var logRecord = exportedItems[0];
+
+            Assert.NotEqual(DateTime.MinValue, logRecord.Timestamp);
+            Assert.Equal(TestEventSource.SimpleEventMessage, logRecord.FormattedMessage);
+            Assert.Equal(TestEventSource.SimpleEventId, logRecord.EventId.Id);
+            Assert.Equal(nameof(TestEventSource.SimpleEvent), logRecord.EventId.Name);
+            Assert.Equal(LogLevel.Warning, logRecord.LogLevel);
+            Assert.Null(logRecord.CategoryName);
+            Assert.Null(logRecord.Exception);
+
+            Assert.Equal(default, logRecord.TraceId);
+            Assert.Equal(default, logRecord.SpanId);
+            Assert.Null(logRecord.TraceState);
+            Assert.Equal(ActivityTraceFlags.None, logRecord.TraceFlags);
+
+            Assert.NotNull(logRecord.StateValues);
+            Assert.Contains(logRecord.StateValues, kvp => kvp.Key == "event_source.name" && (string?)kvp.Value == "OpenTelemetry.Extensions.EventSource.Tests");
+        }
+
+        [Fact]
+        public void OpenTelemetryEventSourceLogEmitterSimpleEventWithActivityTest()
+        {
+            using var activity = new Activity("Test");
+            activity.Start();
+
+            List<LogRecord> exportedItems = new();
+
+#pragma warning disable CA2000 // Dispose objects before losing scope
+            var openTelemetryLoggerProvider = new WrappedOpenTelemetryLoggerProvider(options =>
+            {
+                options.AddInMemoryExporter(exportedItems);
+            });
+#pragma warning restore CA2000 // Dispose objects before losing scope
+
+            using (var openTelemetryEventSourceLogEmitter = new OpenTelemetryEventSourceLogEmitter(
+                openTelemetryLoggerProvider,
+                (name) => name == "OpenTelemetry.Extensions.EventSource.Tests" ? EventLevel.LogAlways : null))
+            {
+                TestEventSource.Log.SimpleEvent();
+            }
+
+            Assert.Single(exportedItems);
+
+            var logRecord = exportedItems[0];
+
+            Assert.NotEqual(default, logRecord.TraceId);
+
+            Assert.Equal(activity.TraceId, logRecord.TraceId);
+            Assert.Equal(activity.SpanId, logRecord.SpanId);
+            Assert.Equal(activity.TraceStateString, logRecord.TraceState);
+            Assert.Equal(activity.ActivityTraceFlags, logRecord.TraceFlags);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void OpenTelemetryEventSourceLogEmitterComplexEventTest(bool formatMessage)
+        {
+            List<LogRecord> exportedItems = new();
+
+#pragma warning disable CA2000 // Dispose objects before losing scope
+            var openTelemetryLoggerProvider = new WrappedOpenTelemetryLoggerProvider(options =>
+            {
+                options.IncludeFormattedMessage = formatMessage;
+                options.AddInMemoryExporter(exportedItems);
+            });
+#pragma warning restore CA2000 // Dispose objects before losing scope
+
+            using (var openTelemetryEventSourceLogEmitter = new OpenTelemetryEventSourceLogEmitter(
+                openTelemetryLoggerProvider,
+                (name) => name == "OpenTelemetry.Extensions.EventSource.Tests" ? EventLevel.LogAlways : null))
+            {
+                TestEventSource.Log.ComplexEvent("Test_Message", 18);
+            }
+
+            Assert.Single(exportedItems);
+
+            var logRecord = exportedItems[0];
+
+            Assert.NotEqual(DateTime.MinValue, logRecord.Timestamp);
+            if (!formatMessage)
+            {
+                Assert.Equal(TestEventSource.ComplexEventMessageStructured, logRecord.FormattedMessage);
+            }
+            else
+            {
+                string expectedMessage = string.Format(CultureInfo.InvariantCulture, TestEventSource.ComplexEventMessage, "Test_Message", 18);
+                Assert.Equal(expectedMessage, logRecord.FormattedMessage);
+            }
+
+            Assert.Equal(TestEventSource.ComplexEventId, logRecord.EventId.Id);
+            Assert.Equal(nameof(TestEventSource.ComplexEvent), logRecord.EventId.Name);
+            Assert.Equal(LogLevel.Information, logRecord.LogLevel);
+            Assert.Null(logRecord.CategoryName);
+            Assert.Null(logRecord.Exception);
+
+            Assert.Equal(default, logRecord.TraceId);
+            Assert.Equal(default, logRecord.SpanId);
+            Assert.Null(logRecord.TraceState);
+            Assert.Equal(ActivityTraceFlags.None, logRecord.TraceFlags);
+
+            Assert.NotNull(logRecord.StateValues);
+            Assert.Contains(logRecord.StateValues, kvp => kvp.Key == "event_source.name" && (string?)kvp.Value == "OpenTelemetry.Extensions.EventSource.Tests");
+            Assert.Contains(logRecord.StateValues, kvp => kvp.Key == "arg1" && (string?)kvp.Value == "Test_Message");
+            Assert.Contains(logRecord.StateValues, kvp => kvp.Key == "arg2" && (int?)kvp.Value == 18);
         }
 
         private sealed class WrappedOpenTelemetryLoggerProvider : OpenTelemetryLoggerProvider
@@ -138,12 +264,22 @@ namespace OpenTelemetry.Extensions.EventSource.Tests
             public const int SimpleEventId = 1;
             public const string SimpleEventMessage = "Warning event with no arguments.";
 
+            public const int ComplexEventId = 2;
+            public const string ComplexEventMessage = "Information event with two arguments: '{0}' & '{1}'.";
+            public const string ComplexEventMessageStructured = "Information event with two arguments: '{arg1}' & '{arg2}'.";
+
             public static TestEventSource Log { get; } = new();
 
             [Event(SimpleEventId, Message = SimpleEventMessage, Level = EventLevel.Warning)]
             public void SimpleEvent()
             {
                 this.WriteEvent(SimpleEventId);
+            }
+
+            [Event(ComplexEventId, Message = ComplexEventMessage, Level = EventLevel.Informational)]
+            public void ComplexEvent(string arg1, int arg2)
+            {
+                this.WriteEvent(ComplexEventId, arg1, arg2);
             }
         }
     }
