8c3b455fc015bda0e328e906bf2e4ec653d15543
Reiley Yang <reyang@microsoft.com>
2022-07-26 15:41:30 -0700

    clean up

--- src/OpenTelemetry/Metrics/ExponentialBucketHistogram.cs
+++ src/OpenTelemetry/Metrics/ExponentialBucketHistogram.cs
@@ -28,7 +28,7 @@ namespace OpenTelemetry.Metrics;
 /// identified by <c>Bucket[index] = ( base ^ index, base ^ (index + 1) ]</c>,
 /// where <c>index</c> is an integer.
 /// </summary>
-public class ExponentialBucketHistogram
+internal class ExponentialBucketHistogram
 {
     private static readonly double Log2E = Math.Log2(Math.E); // 1 / Math.Log(2)
 
