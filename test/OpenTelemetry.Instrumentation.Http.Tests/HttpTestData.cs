8ae12a582bc2675da5daed96b15efe0199a05667
liangli <liang.li.ness@gmail.com>
2022-07-28 20:47:17 +0800

    modify test case to remove custom json converter

--- test/OpenTelemetry.Instrumentation.Http.Tests/HttpTestData.cs
+++ test/OpenTelemetry.Instrumentation.Http.Tests/HttpTestData.cs
@@ -13,11 +13,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // </copyright>
-using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace OpenTelemetry.Instrumentation.Http.Tests
 {
@@ -73,60 +71,7 @@ namespace OpenTelemetry.Instrumentation.Http.Tests
 
             public bool? SpanStatusHasDescription { get; set; }
 
-            [JsonConverter(typeof(DictionaryStringStringConverter))]
             public Dictionary<string, string> SpanAttributes { get; set; }
-        }
-
-        /// <summary>
-        /// Convert string-number to string-string pair.
-        /// </summary>
-        private class DictionaryStringStringConverter : JsonConverter<Dictionary<string, string>>
-        {
-            public override Dictionary<string, string> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-            {
-                if (reader.TokenType != JsonTokenType.StartObject)
-                {
-                    throw new JsonException("The token type should be StartObject.");
-                }
-
-                var value = new Dictionary<string, string>();
-                while (reader.Read())
-                {
-                    if (reader.TokenType == JsonTokenType.EndObject)
-                    {
-                        return value;
-                    }
-
-                    var key = reader.GetString();
-
-                    reader.Read();
-                    string valueAsString;
-                    if (reader.TokenType == JsonTokenType.Number)
-                    {
-                        valueAsString = reader.GetInt32().ToString();
-                    }
-                    else
-                    {
-                        valueAsString = reader.GetString();
-                    }
-
-                    value.Add(key, valueAsString);
-                }
-
-                throw new JsonException("Json Token is not valid.");
-            }
-
-            public override void Write(Utf8JsonWriter writer, Dictionary<string, string> value, JsonSerializerOptions options)
-            {
-                writer.WriteStartObject();
-
-                foreach (var item in value)
-                {
-                    writer.WriteString(item.Key, item.Value);
-                }
-
-                writer.WriteEndObject();
-            }
         }
     }
 }
