e061e816e5e731befbb2eeb63c1c9938d9d0a99c
Vishwesh Bankwar <vibankwa@microsoft.com>
2022-08-04 17:35:03 -0700

    fix file header

--- test/OpenTelemetry.Instrumentation.Http.Tests/HttpClientTests.Basic.cs
+++ test/OpenTelemetry.Instrumentation.Http.Tests/HttpClientTests.Basic.cs
@@ -1,4 +1,4 @@
-// <copyright file="HttpClientTests.Basic.netcore31.cs" company="OpenTelemetry Authors">
+// <copyright file="HttpClientTests.Basic.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
