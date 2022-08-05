4c99b46c5f0603aa0361d211ae38473d3111da4f
Vishwesh Bankwar <vibankwa@microsoft.com>
2022-08-04 17:23:08 -0700

    file rename

--- test/OpenTelemetry.Instrumentation.Http.Tests/HttpClientTests.cs
+++ test/OpenTelemetry.Instrumentation.Http.Tests/HttpClientTests.cs
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // </copyright>
-#if NET6_0_OR_GREATER
+#if !NETFRAMEWORK
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
