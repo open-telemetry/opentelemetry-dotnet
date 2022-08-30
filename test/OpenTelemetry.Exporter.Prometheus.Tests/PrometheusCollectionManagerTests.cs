b26f5e26814710ea864a1abba33989ba306e486f
Alan West <3676547+alanwest@users.noreply.github.com>
2022-08-30 14:45:01 -0700

    Skip test for net462

--- test/OpenTelemetry.Exporter.Prometheus.Tests/PrometheusCollectionManagerTests.cs
+++ test/OpenTelemetry.Exporter.Prometheus.Tests/PrometheusCollectionManagerTests.cs
@@ -29,7 +29,11 @@ namespace OpenTelemetry.Exporter.Prometheus.Tests
 {
     public sealed class PrometheusCollectionManagerTests
     {
+#if NETFRAMEWORK
+        [Fact(Skip = "Might be flaky. Might be a bug. See: https://github.com/open-telemetry/opentelemetry-dotnet/pull/3618#issuecomment-1232200946")]
+#else
         [Fact]
+#endif
         public async Task EnterExitCollectTest()
         {
             using var meter = new Meter(Utils.GetCurrentMethodName());
