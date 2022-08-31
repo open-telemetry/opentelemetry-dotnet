cf9d50cc4e835de88bb99fb9bc761350b211e8f3
Alan West <3676547+alanwest@users.noreply.github.com>
2022-08-31 11:09:09 -0700

    Not suspicious I guess

--- test/OpenTelemetry.Tests/Metrics/MetricAPITest.cs
+++ test/OpenTelemetry.Tests/Metrics/MetricAPITest.cs
@@ -899,6 +899,10 @@ namespace OpenTelemetry.Metrics.Tests
 
             exportedItems.Clear();
 
+#if NETFRAMEWORK
+            Thread.Sleep(5000); // Compensates for low resolution timing in netfx.
+#endif
+
             counterLong.Add(10);
             counterLong.Add(-5);
             meterProvider.ForceFlush(MaxTimeToAllowForFlush);
