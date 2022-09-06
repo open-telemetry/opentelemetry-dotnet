2d250b007cc6abb2e94a78f2ef7e6fae17ea81eb
Vishwesh Bankwar <vibankwa@microsoft.com>
2022-09-06 13:37:15 -0700

    draft

---
+++ test/Benchmarks/Instrumentation/AspNetCoreInstrumentationBenchmark.cs
@@ -0,0 +1,80 @@
+// <copyright file="AspNetCoreInstrumentationBenchmark.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+#if !NETFRAMEWORK
+using System.Net.Http;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+
+namespace Benchmarks.Instrumentation
+{
+    [InProcess]
+    public class AspNetCoreInstrumentationBenchmark
+    {
+        private HttpClient httpClient;
+
+        [GlobalSetup(Target = nameof(UninstrumentedAspNetCoreApp))]
+        public void UninstrumentedAspNetCoreAppGlobalSetup()
+        {
+            var builder = WebApplication.CreateBuilder();
+            builder.Logging.ClearProviders();
+            var app = builder.Build();
+            app.MapGet("/", () => "Hello World!");
+            Task.Run(() => app.Run());
+            this.httpClient = new HttpClient();
+        }
+
+        [GlobalSetup(Target = nameof(InstrumentedAspNetCoreAppWithDefaultOptions))]
+        public void InstrumentedAspNetCoreAppWithDefaultOptionsGlobalSetup()
+        {
+            var builder = WebApplication.CreateBuilder();
+            builder.Logging.ClearProviders();
+            var app = builder.Build();
+            app.MapGet("/", () => "Hello World!");
+            Task.Run(() => app.Run());
+            this.httpClient = new HttpClient();
+
+            Sdk.CreateTracerProviderBuilder()
+                .AddAspNetCoreInstrumentation()
+                .Build();
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            this.httpClient.Dispose();
+        }
+
+        [Benchmark]
+        public async Task UninstrumentedAspNetCoreApp()
+        {
+            var httpResponse = await this.httpClient.GetAsync("http://localhost:5000/");
+            httpResponse.EnsureSuccessStatusCode();
+        }
+
+        [Benchmark]
+        public async Task InstrumentedAspNetCoreAppWithDefaultOptions()
+        {
+            var httpResponse = await this.httpClient.GetAsync("http://localhost:5000/");
+            httpResponse.EnsureSuccessStatusCode();
+        }
+    }
+}
+#endif
