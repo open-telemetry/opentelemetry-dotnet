261aa7f098ca6651175be0eda8e06b0dd3b0f606
Vishwesh Bankwar <vibankwa@microsoft.com>
2022-09-13 08:53:45 -0700

    remove unused

--- test/Benchmarks/Helper/ValuesController.cs
+++ test/Benchmarks/Helper/ValuesController.cs
@@ -29,31 +29,6 @@ namespace Benchmark.Helper
         {
             return new string[] { "value1", "value2" };
         }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
 #endif
