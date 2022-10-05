fc0fccbf731de45df0ed1d995f653541fd69aa2d
TimothyMothra <tilee@microsoft.com>
2022-10-05 13:29:46 -0700

    fix file header

--- test/OpenTelemetry.Tests/E2ETracesTest.cs
+++ test/OpenTelemetry.Tests/E2ETracesTest.cs
@@ -1,4 +1,4 @@
-// <copyright file="BaseProcessorTest.cs" company="OpenTelemetry Authors">
+// <copyright file="E2ETracesTest.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
