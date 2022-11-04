3bb9bd322eee038f99faaac74b5284f128e4d037
Alan West <3676547+alanwest@users.noreply.github.com>
2022-11-04 14:43:53 -0700

    Space: The final frontier.

--- test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/MockCollectorIntegrationTests.cs
+++ test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/MockCollectorIntegrationTests.cs
@@ -66,10 +66,10 @@ public sealed class MockCollectorIntegrationTests
 
                        endpoints.MapGrpcService<MockTraceService>();
                    });
-                }))
+               }))
            .StartAsync();
 
-        var httpClient = new HttpClient() { BaseAddress = new System.Uri("http://localhost:5050") }; // host.GetTestClient();
+        var httpClient = new HttpClient() { BaseAddress = new System.Uri("http://localhost:5050") };
 
         var codes = new[] { Grpc.Core.StatusCode.Unimplemented, Grpc.Core.StatusCode.OK };
         await httpClient.GetAsync($"/MockCollector/SetResponseCodes/{string.Join(",", codes.Select(x => (int)x))}");
