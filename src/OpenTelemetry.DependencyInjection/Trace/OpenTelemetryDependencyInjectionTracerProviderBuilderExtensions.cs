7dfdb13eae74e0572ccd5b7baf6cd775525f8978
Mikel Blanchard <mblanchard@macrosssoftware.com>
2022-12-02 12:44:28 -0800

    Cleanup.

--- src/OpenTelemetry.DependencyInjection/Trace/OpenTelemetryDependencyInjectionTracerProviderBuilderExtensions.cs
+++ src/OpenTelemetry.DependencyInjection/Trace/OpenTelemetryDependencyInjectionTracerProviderBuilderExtensions.cs
@@ -14,7 +14,6 @@
 // limitations under the License.
 // </copyright>
 
-using System;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using OpenTelemetry.Internal;
