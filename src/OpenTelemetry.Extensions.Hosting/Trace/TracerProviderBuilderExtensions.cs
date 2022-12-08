e29f8443e2b2ffc79d8142d36ac4db914e505a05
Timothy Mothra <tilee@microsoft.com>
2022-12-08 10:51:34 -0800

    remove `ImplicitUsings disable` from projects (part 5) (#3989)
    * remove ImplicitUsings disable from projects.
    ZPages
    
    * Hosting
    
    * Propagators

--- src/OpenTelemetry.Extensions.Hosting/Trace/TracerProviderBuilderExtensions.cs
+++ src/OpenTelemetry.Extensions.Hosting/Trace/TracerProviderBuilderExtensions.cs
@@ -14,7 +14,6 @@
 // limitations under the License.
 // </copyright>
 
-using System;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace OpenTelemetry.Trace
--- src/OpenTelemetry.Extensions.Propagators/B3Propagator.cs
+++ src/OpenTelemetry.Extensions.Propagators/B3Propagator.cs
@@ -14,10 +14,7 @@
 // limitations under the License.
 // </copyright>
 
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Text;
 using OpenTelemetry.Context.Propagation;
 using OpenTelemetry.Internal;
