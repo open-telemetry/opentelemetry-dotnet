6cfba0c01c2e8e03f67cd52af4841ce06848ceb2
TimothyMothra <tilee@microsoft.com>
2022-12-08 10:04:35 -0800

    Shims

--- src/OpenTelemetry.Shims.OpenTracing/SpanBuilderShim.cs
+++ src/OpenTelemetry.Shims.OpenTracing/SpanBuilderShim.cs
@@ -14,8 +14,6 @@
 // limitations under the License.
 // </copyright>
 
-using System;
-using System.Collections.Generic;
 using OpenTelemetry.Internal;
 using OpenTelemetry.Trace;
 using OpenTracing;
