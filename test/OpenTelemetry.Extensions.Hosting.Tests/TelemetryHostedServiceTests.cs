da4019344c9777c93b8ed73cc36c5a291c4b7173
Mikel Blanchard <mblanchard@macrosssoftware.com>
2022-12-14 13:00:46 -0800

    Warning cleanup.

--- test/OpenTelemetry.Extensions.Hosting.Tests/TelemetryHostedServiceTests.cs
+++ test/OpenTelemetry.Extensions.Hosting.Tests/TelemetryHostedServiceTests.cs
@@ -1,4 +1,4 @@
-// <copyright file="HostingMeterExtensionTests.cs" company="OpenTelemetry Authors">
+// <copyright file="TelemetryHostedServiceTests.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
