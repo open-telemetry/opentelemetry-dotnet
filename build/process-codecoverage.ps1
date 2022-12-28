4214d0466119f7b1a2fd9ad62667b3be32f44c97
Utkarsh Umesan Pillai <utpilla@microsoft.com>
2022-12-27 16:13:46 -0800

    Update CodeCoverage script

--- build/process-codecoverage.ps1
+++ build/process-codecoverage.ps1
@@ -1,4 +1,4 @@
-﻿[xml]$commonProps = Get-Content -Path $PSScriptRoot\Common.props
+[xml]$commonProps = Get-Content -Path $PSScriptRoot\Common.props
 $microsoftCodeCoveragePkgVer = [string]$commonProps.Project.PropertyGroup.MicrosoftCodeCoveragePkgVer # This is collected in the format: "[16.10.0]"
 $microsoftCodeCoveragePkgVer = $microsoftCodeCoveragePkgVer.Trim();
 $microsoftCodeCoveragePkgVer = $microsoftCodeCoveragePkgVer.SubString(1, $microsoftCodeCoveragePkgVer.Length - 2) # Removing square brackets
@@ -6,7 +6,7 @@ $files = Get-ChildItem "TestResults" -Filter "*.coverage" -Recurse
 Write-Host $env:USERPROFILE
 foreach ($file in $files)
 {
-    $command = $env:USERPROFILE+ '\.nuget\packages\microsoft.codecoverage\' + $microsoftCodeCoveragePkgVer + '\build\netstandard1.0\CodeCoverage\CodeCoverage.exe analyze /output:' + $file.DirectoryName + '\' + $file.Name + '.xml '+ $file.FullName
+    $command = $env:USERPROFILE+ '\.nuget\packages\microsoft.codecoverage\' + $microsoftCodeCoveragePkgVer + '\build\netstandard2.0\CodeCoverage\CodeCoverage.exe analyze /output:' + $file.DirectoryName + '\' + $file.Name + '.xml '+ $file.FullName
     Write-Host $command
     Invoke-Expression $command
 }
