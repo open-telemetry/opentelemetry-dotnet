37b0d7d35f7c0ebd44d5b40a976aaa9dfb035eb6
TimothyMothra <tilee@microsoft.com>
2023-01-20 16:39:41 -0800

    remove FormatException and temp disable tests

--- src/OpenTelemetry/Internal/ConfigurationExtensions.cs
+++ src/OpenTelemetry/Internal/ConfigurationExtensions.cs
@@ -66,7 +66,9 @@ internal static class ConfigurationExtensions
 
         if (!Uri.TryCreate(stringValue, UriKind.Absolute, out value))
         {
-            throw new FormatException($"{key} environment variable has an invalid value: '{stringValue}'");
+            OpenTelemetrySdkEventSource.Log.InvalidEnvironmentVariable(key, stringValue);
+            value = default;
+            return false;
         }
 
         return true;
@@ -85,7 +87,9 @@ internal static class ConfigurationExtensions
 
         if (!int.TryParse(stringValue, NumberStyles.None, CultureInfo.InvariantCulture, out value))
         {
-            throw new FormatException($"{key} environment variable has an invalid value: '{stringValue}'");
+            OpenTelemetrySdkEventSource.Log.InvalidEnvironmentVariable(key, stringValue);
+            value = default;
+            return false;
         }
 
         return true;
@@ -108,7 +112,9 @@ internal static class ConfigurationExtensions
 
         if (!tryParseFunc(stringValue!, out value))
         {
-            throw new FormatException($"{key} environment variable has an invalid value: '{stringValue}'");
+            OpenTelemetrySdkEventSource.Log.InvalidEnvironmentVariable(key, stringValue);
+            value = default;
+            return false;
         }
 
         return true;
