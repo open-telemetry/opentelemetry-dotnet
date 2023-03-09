7136cb86b60e9beb53a79b1c2a5ae70e6d2cb943
Vishwesh Bankwar <vibankwa@microsoft.com>
2023-03-09 08:38:39 -0800

    rename

---
+++ src/OpenTelemetry/Internal/SdkInternalState.cs
@@ -0,0 +1,51 @@
+// <copyright file="SdkInternalState.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using System.Diagnostics.Metrics;
+
+namespace OpenTelemetry.Internal
+{
+    internal sealed class SdkInternalState : IDisposable
+    {
+        internal static SdkInternalState Instance = new();
+        private const string SdkInternalStateMeterName = "SdkInternalState";
+        private readonly Meter SdkInternalStateMeter;
+
+        private SdkInternalState()
+        {
+            this.SdkInternalStateMeter = new Meter(SdkInternalStateMeterName, "1.0");
+            AppDomain.CurrentDomain.ProcessExit += (sender, eventArgs) =>
+            {
+                Instance.Dispose();
+            };
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.SdkInternalStateMeter.Dispose();
+            }
+        }
+    }
+}
