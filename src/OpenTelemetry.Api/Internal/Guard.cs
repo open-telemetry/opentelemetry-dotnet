09b0b44c5a90212d543971e5fc6ea154b44cec37
TimothyMothra <tilee@microsoft.com>
2023-03-10 15:07:33 -0800

    move attributes to new file

--- src/OpenTelemetry.Api/Internal/Guard.cs
+++ src/OpenTelemetry.Api/Internal/Guard.cs
@@ -42,19 +42,6 @@ namespace System.Runtime.CompilerServices
 }
 #endif
 
-#if !NETCOREAPP3_0_OR_GREATER && !NETSTANDARD2_1_OR_GREATER
-namespace System.Diagnostics.CodeAnalysis
-{
-    /// <summary>Specifies that an output is not <see langword="null"/> even if
-    /// the corresponding type allows it. Specifies that an input argument was
-    /// not <see langword="null"/> when the call returns.</summary>
-    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.ReturnValue, Inherited = false)]
-    internal sealed class NotNullAttribute : Attribute
-    {
-    }
-}
-#endif
-
 namespace OpenTelemetry.Internal
 {
     /// <summary>
---
+++ src/OpenTelemetry.Api/Internal/NullableAttributes.cs
@@ -0,0 +1,37 @@
+// <copyright file="NullableAttributes.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+// The following code was borrowed from https://github.com/dotnet/runtime/blob/main/src/libraries/System.Private.CoreLib/src/System/Diagnostics/CodeAnalysis/NullableAttributes.cs
+// These classes are available in .NET versions after NetStandard2.0
+// For more information see: https://learn.microsoft.com/dotnet/csharp/language-reference/attributes/nullable-analysis
+
+#nullable enable
+
+#pragma warning disable SA1402 // File may only contain a single type
+#pragma warning disable SA1649 // File name should match first type name
+
+#if !NETCOREAPP3_0_OR_GREATER && !NETSTANDARD2_1_OR_GREATER
+namespace System.Diagnostics.CodeAnalysis
+{
+    /// <summary>Specifies that an output is not <see langword="null"/> even if
+    /// the corresponding type allows it. Specifies that an input argument was
+    /// not <see langword="null"/> when the call returns.</summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.ReturnValue, Inherited = false)]
+    internal sealed class NotNullAttribute : Attribute
+    {
+    }
+}
+#endif
