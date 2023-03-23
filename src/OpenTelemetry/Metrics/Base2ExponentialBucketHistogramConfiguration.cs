86f046d07b5a1b9de7864b74ac96ca80ee3c657c
Alan West <3676547+alanwest@users.noreply.github.com>
2023-03-22 22:47:20 -0700

    Add exponential histogram  MaxScale configuration

--- src/OpenTelemetry/Metrics/Base2ExponentialBucketHistogramConfiguration.cs
+++ src/OpenTelemetry/Metrics/Base2ExponentialBucketHistogramConfiguration.cs
@@ -21,7 +21,8 @@ namespace OpenTelemetry.Metrics;
 /// </summary>
 internal sealed class Base2ExponentialBucketHistogramConfiguration : HistogramConfiguration
 {
-    private int maxSize = 160;
+    private int maxSize = Metric.DefaultExponentialHistogramMaxBuckets;
+    private int maxScale = Metric.DefaultExponentialHistogramMaxScale;
 
     /// <summary>
     /// Gets or sets the maximum number of buckets in each of the positive and negative ranges, not counting the special zero bucket.
@@ -44,6 +45,31 @@ internal sealed class Base2ExponentialBucketHistogramConfiguration : HistogramCo
             }
 
             this.maxSize = value;
+        }
+    }
+
+    /// <summary>
+    /// Gets or sets the maximum scale factor used to determine the resolution of bucket boundaries.
+    /// The higher the scale the higher the resolution.
+    /// </summary>
+    /// <remarks>
+    /// The default value is 20. The minimum size is -11. The maximum size is 20.
+    /// </remarks>
+    public int MaxScale
+    {
+        get
+        {
+            return this.maxScale;
+        }
+
+        set
+        {
+            if (value < -11 || value > 20)
+            {
+                throw new ArgumentException($"Histogram max scale is invalid. Max scale must be in the range [-11, 20].", nameof(value));
+            }
+
+            this.maxScale = value;
         }
     }
 }
