e5099dc2dc7005056813e5b304ea4bf50738fb62
TimothyMothra <tilee@microsoft.com>
2023-03-27 20:08:16 -0700

    cleanup

--- src/OpenTelemetry.Api/Internal/NullableAttributes.cs
+++ src/OpenTelemetry.Api/Internal/NullableAttributes.cs
@@ -20,8 +20,8 @@
 
 #nullable enable
 
-#pragma warning disable SA1402 // File may only contain a single type
 #pragma warning disable SA1649 // File name should match first type name
+#pragma warning disable SA1402 // File may only contain a single type
 
 #if NETFRAMEWORK || NETSTANDARD2_0
 namespace System.Diagnostics.CodeAnalysis
