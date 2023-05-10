f6fc73e02f727f7f8d901ff218da33ad8eaebf9d
Mikel Blanchard <mblanchard@macrosssoftware.com>
2023-05-10 14:31:54 -0700

    [sdk-logs] [nits] Update source files to use file scoped namespaces (#4476)

--- src/OpenTelemetry/Logs/Pool/LogRecordPoolHelper.cs
+++ src/OpenTelemetry/Logs/Pool/LogRecordPoolHelper.cs
@@ -16,45 +16,44 @@
 
 #nullable enable
 
-namespace OpenTelemetry.Logs
+namespace OpenTelemetry.Logs;
+
+internal static class LogRecordPoolHelper
 {
-    internal static class LogRecordPoolHelper
+    public const int DefaultMaxNumberOfAttributes = 64;
+    public const int DefaultMaxNumberOfScopes = 16;
+
+    public static void Clear(LogRecord logRecord)
     {
-        public const int DefaultMaxNumberOfAttributes = 64;
-        public const int DefaultMaxNumberOfScopes = 16;
-
-        public static void Clear(LogRecord logRecord)
+        var attributeStorage = logRecord.AttributeStorage;
+        if (attributeStorage != null)
         {
-            var attributeStorage = logRecord.AttributeStorage;
-            if (attributeStorage != null)
+            if (attributeStorage.Count > DefaultMaxNumberOfAttributes)
             {
-                if (attributeStorage.Count > DefaultMaxNumberOfAttributes)
-                {
-                    // Don't allow the pool to grow unconstained.
-                    logRecord.AttributeStorage = null;
-                }
-                else
-                {
-                    /* List<T>.Clear sets the count/size to 0 but it maintains the
-                    underlying array (capacity). */
-                    attributeStorage.Clear();
-                }
+                // Don't allow the pool to grow unconstained.
+                logRecord.AttributeStorage = null;
             }
-
-            var scopeStorage = logRecord.ScopeStorage;
-            if (scopeStorage != null)
+            else
             {
-                if (scopeStorage.Count > DefaultMaxNumberOfScopes)
-                {
-                    // Don't allow the pool to grow unconstained.
-                    logRecord.ScopeStorage = null;
-                }
-                else
-                {
-                    /* List<T>.Clear sets the count/size to 0 but it maintains the
-                    underlying array (capacity). */
-                    scopeStorage.Clear();
-                }
+                /* List<T>.Clear sets the count/size to 0 but it maintains the
+                underlying array (capacity). */
+                attributeStorage.Clear();
+            }
+        }
+
+        var scopeStorage = logRecord.ScopeStorage;
+        if (scopeStorage != null)
+        {
+            if (scopeStorage.Count > DefaultMaxNumberOfScopes)
+            {
+                // Don't allow the pool to grow unconstained.
+                logRecord.ScopeStorage = null;
+            }
+            else
+            {
+                /* List<T>.Clear sets the count/size to 0 but it maintains the
+                underlying array (capacity). */
+                scopeStorage.Clear();
             }
         }
     }
--- src/OpenTelemetry/Logs/Pool/LogRecordSharedPool.cs
+++ src/OpenTelemetry/Logs/Pool/LogRecordSharedPool.cs
@@ -19,130 +19,129 @@
 using System.Diagnostics.CodeAnalysis;
 using OpenTelemetry.Internal;
 
-namespace OpenTelemetry.Logs
+namespace OpenTelemetry.Logs;
+
+internal sealed class LogRecordSharedPool : ILogRecordPool
 {
-    internal sealed class LogRecordSharedPool : ILogRecordPool
+    public const int DefaultMaxPoolSize = 2048;
+
+    public static LogRecordSharedPool Current = new(DefaultMaxPoolSize);
+
+    public readonly int Capacity;
+    private readonly LogRecord?[] pool;
+    private long rentIndex;
+    private long returnIndex;
+
+    public LogRecordSharedPool(int capacity)
     {
-        public const int DefaultMaxPoolSize = 2048;
+        this.Capacity = capacity;
+        this.pool = new LogRecord?[capacity];
+    }
 
-        public static LogRecordSharedPool Current = new(DefaultMaxPoolSize);
+    public int Count => (int)(Volatile.Read(ref this.returnIndex) - Volatile.Read(ref this.rentIndex));
 
-        public readonly int Capacity;
-        private readonly LogRecord?[] pool;
-        private long rentIndex;
-        private long returnIndex;
+    // Note: It might make sense to expose this (somehow) in the future.
+    // Ideal config is shared pool capacity == max batch size.
+    public static void Resize(int capacity)
+    {
+        Guard.ThrowIfOutOfRange(capacity, min: 1);
 
-        public LogRecordSharedPool(int capacity)
+        Current = new(capacity);
+    }
+
+    public LogRecord Rent()
+    {
+        while (true)
         {
-            this.Capacity = capacity;
-            this.pool = new LogRecord?[capacity];
-        }
+            var rentSnapshot = Volatile.Read(ref this.rentIndex);
+            var returnSnapshot = Volatile.Read(ref this.returnIndex);
 
-        public int Count => (int)(Volatile.Read(ref this.returnIndex) - Volatile.Read(ref this.rentIndex));
-
-        // Note: It might make sense to expose this (somehow) in the future.
-        // Ideal config is shared pool capacity == max batch size.
-        public static void Resize(int capacity)
-        {
-            Guard.ThrowIfOutOfRange(capacity, min: 1);
-
-            Current = new(capacity);
-        }
-
-        public LogRecord Rent()
-        {
-            while (true)
+            if (rentSnapshot >= returnSnapshot)
             {
-                var rentSnapshot = Volatile.Read(ref this.rentIndex);
-                var returnSnapshot = Volatile.Read(ref this.returnIndex);
-
-                if (rentSnapshot >= returnSnapshot)
-                {
-                    break; // buffer is empty
-                }
-
-                if (Interlocked.CompareExchange(ref this.rentIndex, rentSnapshot + 1, rentSnapshot) == rentSnapshot)
-                {
-                    var logRecord = Interlocked.Exchange(ref this.pool[rentSnapshot % this.Capacity], null);
-                    if (logRecord == null && !this.TryRentCoreRare(rentSnapshot, out logRecord))
-                    {
-                        continue;
-                    }
-
-                    logRecord.ResetReferenceCount();
-                    return logRecord;
-                }
+                break; // buffer is empty
             }
 
-            var newLogRecord = new LogRecord();
-            newLogRecord.ResetReferenceCount();
-            return newLogRecord;
+            if (Interlocked.CompareExchange(ref this.rentIndex, rentSnapshot + 1, rentSnapshot) == rentSnapshot)
+            {
+                var logRecord = Interlocked.Exchange(ref this.pool[rentSnapshot % this.Capacity], null);
+                if (logRecord == null && !this.TryRentCoreRare(rentSnapshot, out logRecord))
+                {
+                    continue;
+                }
+
+                logRecord.ResetReferenceCount();
+                return logRecord;
+            }
         }
 
-        public void Return(LogRecord logRecord)
+        var newLogRecord = new LogRecord();
+        newLogRecord.ResetReferenceCount();
+        return newLogRecord;
+    }
+
+    public void Return(LogRecord logRecord)
+    {
+        if (logRecord.RemoveReference() != 0)
         {
-            if (logRecord.RemoveReference() != 0)
+            return;
+        }
+
+        LogRecordPoolHelper.Clear(logRecord);
+
+        while (true)
+        {
+            var rentSnapshot = Volatile.Read(ref this.rentIndex);
+            var returnSnapshot = Volatile.Read(ref this.returnIndex);
+
+            if (returnSnapshot - rentSnapshot >= this.Capacity)
             {
+                return; // buffer is full
+            }
+
+            if (Interlocked.CompareExchange(ref this.returnIndex, returnSnapshot + 1, returnSnapshot) == returnSnapshot)
+            {
+                // If many threads are hammering rent/return it is possible
+                // for two threads to write to the same index. In that case
+                // only one of the logRecords will make it back into the
+                // pool. Anything lost in the race will collected by the GC
+                // and the pool will issue new instances as needed. This
+                // could be abated by an Interlocked.CompareExchange here
+                // but for the general use case of an exporter returning
+                // records one-by-one, better to keep this fast and not pay
+                // for Interlocked.CompareExchange. The race is more
+                // theoretical.
+                this.pool[returnSnapshot % this.Capacity] = logRecord;
                 return;
             }
-
-            LogRecordPoolHelper.Clear(logRecord);
-
-            while (true)
-            {
-                var rentSnapshot = Volatile.Read(ref this.rentIndex);
-                var returnSnapshot = Volatile.Read(ref this.returnIndex);
-
-                if (returnSnapshot - rentSnapshot >= this.Capacity)
-                {
-                    return; // buffer is full
-                }
-
-                if (Interlocked.CompareExchange(ref this.returnIndex, returnSnapshot + 1, returnSnapshot) == returnSnapshot)
-                {
-                    // If many threads are hammering rent/return it is possible
-                    // for two threads to write to the same index. In that case
-                    // only one of the logRecords will make it back into the
-                    // pool. Anything lost in the race will collected by the GC
-                    // and the pool will issue new instances as needed. This
-                    // could be abated by an Interlocked.CompareExchange here
-                    // but for the general use case of an exporter returning
-                    // records one-by-one, better to keep this fast and not pay
-                    // for Interlocked.CompareExchange. The race is more
-                    // theoretical.
-                    this.pool[returnSnapshot % this.Capacity] = logRecord;
-                    return;
-                }
-            }
         }
+    }
 
-        private bool TryRentCoreRare(long rentSnapshot, [NotNullWhen(true)] out LogRecord? logRecord)
+    private bool TryRentCoreRare(long rentSnapshot, [NotNullWhen(true)] out LogRecord? logRecord)
+    {
+        SpinWait wait = default;
+        while (true)
         {
-            SpinWait wait = default;
-            while (true)
+            if (wait.NextSpinWillYield)
             {
-                if (wait.NextSpinWillYield)
-                {
-                    // Super rare case. If many threads are hammering
-                    // rent/return it is possible a read was issued an index and
-                    // then yielded while other threads caused the pointers to
-                    // wrap around. When the yielded thread wakes up its read
-                    // index could have been stolen by another thread. To
-                    // prevent deadlock, bail out of read after spinning. This
-                    // will cause either a successful rent from another index,
-                    // or a new record to be created
-                    logRecord = null;
-                    return false;
-                }
+                // Super rare case. If many threads are hammering
+                // rent/return it is possible a read was issued an index and
+                // then yielded while other threads caused the pointers to
+                // wrap around. When the yielded thread wakes up its read
+                // index could have been stolen by another thread. To
+                // prevent deadlock, bail out of read after spinning. This
+                // will cause either a successful rent from another index,
+                // or a new record to be created
+                logRecord = null;
+                return false;
+            }
 
-                wait.SpinOnce();
+            wait.SpinOnce();
 
-                logRecord = Interlocked.Exchange(ref this.pool[rentSnapshot % this.Capacity], null);
-                if (logRecord != null)
-                {
-                    // Rare case where the write was still working when the read came in
-                    return true;
-                }
+            logRecord = Interlocked.Exchange(ref this.pool[rentSnapshot % this.Capacity], null);
+            if (logRecord != null)
+            {
+                // Rare case where the write was still working when the read came in
+                return true;
             }
         }
     }
