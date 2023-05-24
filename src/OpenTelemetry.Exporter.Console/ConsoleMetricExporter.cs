eb12f9b386f0f3231bd9f41b34b65cc927831e1c
Alan West <3676547+alanwest@users.noreply.github.com>
2023-05-24 15:27:41 -0700

    Console exporter: Display exponential histogram boundaries (#4507)

--- src/OpenTelemetry.Exporter.Console/ConsoleMetricExporter.cs
+++ src/OpenTelemetry.Exporter.Console/ConsoleMetricExporter.cs
@@ -146,8 +146,21 @@ namespace OpenTelemetry.Exporter
                         }
                         else
                         {
-                            // TODO: Consider how/if to display buckets for exponential histograms.
-                            bucketsBuilder.AppendLine("Buckets are not displayed for exponential histograms.");
+                            var exponentialHistogramData = metricPoint.GetExponentialHistogramData();
+                            var scale = exponentialHistogramData.Scale;
+
+                            if (exponentialHistogramData.ZeroCount != 0)
+                            {
+                                bucketsBuilder.AppendLine($"Zero Bucket:{exponentialHistogramData.ZeroCount}");
+                            }
+
+                            var offset = exponentialHistogramData.PositiveBuckets.Offset;
+                            foreach (var bucketCount in exponentialHistogramData.PositiveBuckets)
+                            {
+                                var lowerBound = Base2ExponentialBucketHistogram.LowerBoundary(offset, scale).ToString(CultureInfo.InvariantCulture);
+                                var upperBound = Base2ExponentialBucketHistogram.LowerBoundary(++offset, scale).ToString(CultureInfo.InvariantCulture);
+                                bucketsBuilder.AppendLine($"({lowerBound}, {upperBound}]:{bucketCount}");
+                            }
                         }
 
                         valueDisplay = bucketsBuilder.ToString();
