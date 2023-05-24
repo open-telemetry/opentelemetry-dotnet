4ca40630206f27987b8955c743543dbb4ace4326
TimothyMothra <tilee@microsoft.com>
2023-05-23 18:02:59 -0700

    file-scoped namespace

--- src/OpenTelemetry.Api/Internal/HttpSemanticConventionHelper.cs
+++ src/OpenTelemetry.Api/Internal/HttpSemanticConventionHelper.cs
@@ -14,54 +14,53 @@
 // limitations under the License.
 // </copyright>
 
-namespace OpenTelemetry.Internal
+namespace OpenTelemetry.Internal;
+
+/// <summary>
+/// Helper class for Http Semantic Conventions.
+/// </summary>
+/// <remarks>
+/// Due to a breaking change in the semantic convention, affected instrumentation libraries
+/// must inspect an environment variable to determine which attributes to emit.
+/// This is expected to be removed when the instrumentation libraries reach Stable.
+/// <see href="https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/http.md"/>.
+/// </remarks>
+internal static class HttpSemanticConventionHelper
 {
-    /// <summary>
-    /// Helper class for Http Semantic Conventions.
-    /// </summary>
-    /// <remarks>
-    /// Due to a breaking change in the semantic convention, affected instrumentation libraries
-    /// must inspect an environment variable to determine which attributes to emit.
-    /// This is expected to be removed when the instrumentation libraries reach Stable.
-    /// <see href="https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/http.md"/>.
-    /// </remarks>
-    internal static class HttpSemanticConventionHelper
+    [Flags]
+    internal enum HttpSemanticConvention
     {
-        [Flags]
-        internal enum HttpSemanticConvention
+        /// <summary>
+        /// Instructs an instrumentation library to emit the old experimental HTTP attributes.
+        /// </summary>
+        Old = 0x1,
+
+        /// <summary>
+        /// Instructs an instrumentation library to emit the new, stable Http attributes.
+        /// </summary>
+        New = 0x2,
+
+        /// <summary>
+        /// Instructs an instrumentation library to emit both the old and new attributes.
+        /// </summary>
+        Dupe = Old | New,
+    }
+
+    public static HttpSemanticConvention GetSemanticConventionOptIn()
+    {
+        try
         {
-            /// <summary>
-            /// Instructs an instrumentation library to emit the old experimental HTTP attributes.
-            /// </summary>
-            Old = 0x1,
-
-            /// <summary>
-            /// Instructs an instrumentation library to emit the new, stable Http attributes.
-            /// </summary>
-            New = 0x2,
-
-            /// <summary>
-            /// Instructs an instrumentation library to emit both the old and new attributes.
-            /// </summary>
-            Dupe = Old | New,
+            var envVarValue = Environment.GetEnvironmentVariable("OTEL_SEMCONV_STABILITY_OPT_IN");
+            return envVarValue?.ToLowerInvariant() switch
+            {
+                "http" => HttpSemanticConvention.New,
+                "http/dup" => HttpSemanticConvention.Dupe,
+                _ => HttpSemanticConvention.Old,
+            };
         }
-
-        public static HttpSemanticConvention GetSemanticConventionOptIn()
+        catch
         {
-            try
-            {
-                var envVarValue = Environment.GetEnvironmentVariable("OTEL_SEMCONV_STABILITY_OPT_IN");
-                return envVarValue?.ToLowerInvariant() switch
-                {
-                    "http" => HttpSemanticConvention.New,
-                    "http/dup" => HttpSemanticConvention.Dupe,
-                    _ => HttpSemanticConvention.Old,
-                };
-            }
-            catch
-            {
-                return HttpSemanticConvention.Old;
-            }
+            return HttpSemanticConvention.Old;
         }
     }
 }
--- test/OpenTelemetry.Api.Tests/Internal/HttpSemanticConventionHelperTest.cs
+++ test/OpenTelemetry.Api.Tests/Internal/HttpSemanticConventionHelperTest.cs
@@ -17,52 +17,51 @@
 using Xunit;
 using static OpenTelemetry.Internal.HttpSemanticConventionHelper;
 
-namespace OpenTelemetry.Api.Tests.Internal
+namespace OpenTelemetry.Api.Tests.Internal;
+
+public class HttpSemanticConventionHelperTest
 {
-    public class HttpSemanticConventionHelperTest
+    [Fact]
+    public void VerifyFlags()
     {
-        [Fact]
-        public void VerifyFlags()
+        var testValue = HttpSemanticConvention.Dupe;
+        Assert.True(testValue.HasFlag(HttpSemanticConvention.Old));
+        Assert.True(testValue.HasFlag(HttpSemanticConvention.New));
+
+        testValue = HttpSemanticConvention.Old;
+        Assert.True(testValue.HasFlag(HttpSemanticConvention.Old));
+        Assert.False(testValue.HasFlag(HttpSemanticConvention.New));
+
+        testValue = HttpSemanticConvention.New;
+        Assert.False(testValue.HasFlag(HttpSemanticConvention.Old));
+        Assert.True(testValue.HasFlag(HttpSemanticConvention.New));
+    }
+
+    [Fact]
+    public void VerifyGetSemanticConventionOptIn()
+    {
+        this.RunTestWithEnvironmentVariable(null, HttpSemanticConvention.Old);
+        this.RunTestWithEnvironmentVariable(string.Empty, HttpSemanticConvention.Old);
+        this.RunTestWithEnvironmentVariable("junk", HttpSemanticConvention.Old);
+        this.RunTestWithEnvironmentVariable("none", HttpSemanticConvention.Old);
+        this.RunTestWithEnvironmentVariable("NONE", HttpSemanticConvention.Old);
+        this.RunTestWithEnvironmentVariable("http", HttpSemanticConvention.New);
+        this.RunTestWithEnvironmentVariable("HTTP", HttpSemanticConvention.New);
+        this.RunTestWithEnvironmentVariable("http/dup", HttpSemanticConvention.Dupe);
+        this.RunTestWithEnvironmentVariable("HTTP/DUP", HttpSemanticConvention.Dupe);
+    }
+
+    private void RunTestWithEnvironmentVariable(string value, HttpSemanticConvention expected)
+    {
+        try
         {
-            var testValue = HttpSemanticConvention.Dupe;
-            Assert.True(testValue.HasFlag(HttpSemanticConvention.Old));
-            Assert.True(testValue.HasFlag(HttpSemanticConvention.New));
+            Environment.SetEnvironmentVariable("OTEL_SEMCONV_STABILITY_OPT_IN", value);
 
-            testValue = HttpSemanticConvention.Old;
-            Assert.True(testValue.HasFlag(HttpSemanticConvention.Old));
-            Assert.False(testValue.HasFlag(HttpSemanticConvention.New));
-
-            testValue = HttpSemanticConvention.New;
-            Assert.False(testValue.HasFlag(HttpSemanticConvention.Old));
-            Assert.True(testValue.HasFlag(HttpSemanticConvention.New));
+            Assert.Equal(expected, GetSemanticConventionOptIn());
         }
-
-        [Fact]
-        public void VerifyGetSemanticConventionOptIn()
+        finally
         {
-            this.RunTestWithEnvironmentVariable(null, HttpSemanticConvention.Old);
-            this.RunTestWithEnvironmentVariable(string.Empty, HttpSemanticConvention.Old);
-            this.RunTestWithEnvironmentVariable("junk", HttpSemanticConvention.Old);
-            this.RunTestWithEnvironmentVariable("none", HttpSemanticConvention.Old);
-            this.RunTestWithEnvironmentVariable("NONE", HttpSemanticConvention.Old);
-            this.RunTestWithEnvironmentVariable("http", HttpSemanticConvention.New);
-            this.RunTestWithEnvironmentVariable("HTTP", HttpSemanticConvention.New);
-            this.RunTestWithEnvironmentVariable("http/dup", HttpSemanticConvention.Dupe);
-            this.RunTestWithEnvironmentVariable("HTTP/DUP", HttpSemanticConvention.Dupe);
-        }
-
-        private void RunTestWithEnvironmentVariable(string value, HttpSemanticConvention expected)
-        {
-            try
-            {
-                Environment.SetEnvironmentVariable("OTEL_SEMCONV_STABILITY_OPT_IN", value);
-
-                Assert.Equal(expected, GetSemanticConventionOptIn());
-            }
-            finally
-            {
-                Environment.SetEnvironmentVariable("OTEL_SEMCONV_STABILITY_OPT_IN", null);
-            }
+            Environment.SetEnvironmentVariable("OTEL_SEMCONV_STABILITY_OPT_IN", null);
         }
     }
 }
