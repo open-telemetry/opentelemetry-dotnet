f39a7b197ba0fc93e3b7be62ce84cb0a91cebd7a
Mikel Blanchard <mblanchard@macrosssoftware.com>
2023-05-25 10:33:16 -0700

    Expose a detached MeterProviderBuilder extension on IServiceCollection which may modify services.

--- src/OpenTelemetry.Api.ProviderBuilderExtensions/Metrics/OpenTelemetryDependencyInjectionMetricsServiceCollectionExtensions.cs
+++ src/OpenTelemetry.Api.ProviderBuilderExtensions/Metrics/OpenTelemetryDependencyInjectionMetricsServiceCollectionExtensions.cs
@@ -26,9 +26,7 @@ public static class OpenTelemetryDependencyInjectionMetricsServiceCollectionExte
 {
     /// <summary>
     /// Registers an action used to configure the OpenTelemetry <see
-    /// cref="MeterProviderBuilder"/> used to create the <see
-    /// cref="MeterProvider"/> for the <see cref="IServiceCollection"/> being
-    /// configured.
+    /// cref="MeterProviderBuilder"/>.
     /// </summary>
     /// <remarks>
     /// Notes:
@@ -36,14 +34,53 @@ public static class OpenTelemetryDependencyInjectionMetricsServiceCollectionExte
     /// <item>This is safe to be called multiple times and by library authors.
     /// Each registered configuration action will be applied
     /// sequentially.</item>
-    /// <item>A <see cref="MeterProvider"/> will not be created automatically
+    /// <item>A <see cref="MeterProviderBuilder"/> will NOT be created automatically
     /// using this method. To begin collecting metrics use the
     /// <c>IServiceCollection.AddOpenTelemetry</c> extension in the
     /// <c>OpenTelemetry.Extensions.Hosting</c> package.</item>
     /// </list>
     /// </remarks>
-    /// <param name="services">The <see cref="IServiceCollection" /> to add
-    /// services to.</param>
+    /// <param name="services"><see cref="IServiceCollection" />.</param>
+    /// <param name="configure">Callback action to configure the <see
+    /// cref="MeterProviderBuilder"/>.</param>
+    /// <returns>The <see cref="IServiceCollection"/> so that additional calls
+    /// can be chained.</returns>
+    public static IServiceCollection ConfigureOpenTelemetryMeterProvider(
+        this IServiceCollection services,
+        Action<MeterProviderBuilder> configure)
+    {
+        Guard.ThrowIfNull(services);
+        Guard.ThrowIfNull(configure);
+
+        configure(new MeterProviderServiceCollectionBuilder(services));
+
+        return services;
+    }
+
+    /// <summary>
+    /// Registers an action used to configure the OpenTelemetry <see
+    /// cref="MeterProviderBuilder"/> once the <see cref="IServiceProvider"/>
+    /// is available.
+    /// </summary>
+    /// <remarks>
+    /// Notes:
+    /// <list type="bullet">
+    /// <item>This is safe to be called multiple times and by library authors.
+    /// Each registered configuration action will be applied
+    /// sequentially.</item>
+    /// <item>A <see cref="MeterProvider"/> will NOT be created automatically
+    /// using this method. To begin collecting metrics use the
+    /// <c>IServiceCollection.AddOpenTelemetry</c> extension in the
+    /// <c>OpenTelemetry.Extensions.Hosting</c> package.</item>
+    /// <item>The supplied configuration delegate is called once the <see
+    /// cref="IServiceProvider"/> is available. Services may NOT be added to a
+    /// <see cref="MeterProviderBuilder"/> once the <see
+    /// cref="IServiceProvider"/> has been created. Many helper extensions
+    /// register services and may throw if invoked inside the configuration
+    /// delegate.</item>
+    /// </list>
+    /// </remarks>
+    /// <param name="services"><see cref="IServiceCollection" />.</param>
     /// <param name="configure">Callback action to configure the <see
     /// cref="MeterProviderBuilder"/>.</param>
     /// <returns>The <see cref="IServiceCollection"/> so that additional calls
@@ -52,18 +89,13 @@ public static class OpenTelemetryDependencyInjectionMetricsServiceCollectionExte
         this IServiceCollection services,
         Action<IServiceProvider, MeterProviderBuilder> configure)
     {
-        RegisterBuildAction(services, configure);
-
-        return services;
-    }
-
-    private static void RegisterBuildAction(IServiceCollection services, Action<IServiceProvider, MeterProviderBuilder> configure)
-    {
         Guard.ThrowIfNull(services);
         Guard.ThrowIfNull(configure);
 
         services.AddSingleton<IConfigureMeterProviderBuilder>(
             new ConfigureMeterProviderBuilderCallbackWrapper(configure));
+
+        return services;
     }
 
     private sealed class ConfigureMeterProviderBuilderCallbackWrapper : IConfigureMeterProviderBuilder
