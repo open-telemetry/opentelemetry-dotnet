6befb4108e60f71fddd5e708f9f78d91ed897a1b
Alan West <3676547+alanwest@users.noreply.github.com>
2023-05-31 13:09:40 -0700

    File-scoped namespaces

--- test/Benchmarks/Metrics/ExponentialHistogramBenchmarks.cs
+++ test/Benchmarks/Metrics/ExponentialHistogramBenchmarks.cs
@@ -35,57 +35,56 @@ Apple M1 Max, 1 CPU, 10 logical and 10 physical cores
 | HistogramHotPath |    20 | 32.08 ns | 0.076 ns | 0.063 ns |         - |
 */
 
-namespace Benchmarks.Metrics
+namespace Benchmarks.Metrics;
+
+public class ExponentialHistogramBenchmarks
 {
-    public class ExponentialHistogramBenchmarks
+    private const int MaxValue = 10000;
+    private readonly Random random = new();
+    private Histogram<long> histogram;
+    private MeterProvider provider;
+    private Meter meter;
+
+    // This is a simple benchmark that records values in the range [0, 10000].
+    // The reason the following scales are benchmarked are as follows:
+    //
+    // -11: Non-positive scales should perform better than positive scales.
+    //      The algorithm to map values to buckets for non-positive scales is more efficient.
+    //   3: The benchmark records values in the range [0, 10000] and uses the default max number of buckets (160).
+    //      Scale 3 is the maximum scale that will fit this range of values given the number of buckets.
+    //      That is, no scale down will occur.
+    //  20: Scale 20 should perform the same as scale 3. During warmup the histogram should scale down to 3.
+    [Params(-11, 3, 20)]
+    public int Scale { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
     {
-        private const int MaxValue = 10000;
-        private readonly Random random = new();
-        private Histogram<long> histogram;
-        private MeterProvider provider;
-        private Meter meter;
+        this.meter = new Meter(Utils.GetCurrentMethodName());
+        this.histogram = this.meter.CreateHistogram<long>("histogram");
 
-        // This is a simple benchmark that records values in the range [0, 10000].
-        // The reason the following scales are benchmarked are as follows:
-        //
-        // -11: Non-positive scales should perform better than positive scales.
-        //      The algorithm to map values to buckets for non-positive scales is more efficient.
-        //   3: The benchmark records values in the range [0, 10000] and uses the default max number of buckets (160).
-        //      Scale 3 is the maximum scale that will fit this range of values given the number of buckets.
-        //      That is, no scale down will occur.
-        //  20: Scale 20 should perform the same as scale 3. During warmup the histogram should scale down to 3.
-        [Params(-11, 3, 20)]
-        public int Scale { get; set; }
+        var exportedItems = new List<Metric>();
 
-        [GlobalSetup]
-        public void Setup()
-        {
-            this.meter = new Meter(Utils.GetCurrentMethodName());
-            this.histogram = this.meter.CreateHistogram<long>("histogram");
+        this.provider = Sdk.CreateMeterProviderBuilder()
+            .AddMeter(this.meter.Name)
+            .AddInMemoryExporter(exportedItems, metricReaderOptions =>
+            {
+                metricReaderOptions.PeriodicExportingMetricReaderOptions.ExportIntervalMilliseconds = 1000;
+            })
+            .AddView("histogram", new Base2ExponentialBucketHistogramConfiguration() { MaxScale = this.Scale, MaxSize = Metric.DefaultExponentialHistogramMaxBuckets })
+            .Build();
+    }
 
-            var exportedItems = new List<Metric>();
+    [GlobalCleanup]
+    public void Cleanup()
+    {
+        this.meter?.Dispose();
+        this.provider?.Dispose();
+    }
 
-            this.provider = Sdk.CreateMeterProviderBuilder()
-                .AddMeter(this.meter.Name)
-                .AddInMemoryExporter(exportedItems, metricReaderOptions =>
-                {
-                    metricReaderOptions.PeriodicExportingMetricReaderOptions.ExportIntervalMilliseconds = 1000;
-                })
-                .AddView("histogram", new Base2ExponentialBucketHistogramConfiguration() { MaxScale = this.Scale, MaxSize = Metric.DefaultExponentialHistogramMaxBuckets })
-                .Build();
-        }
-
-        [GlobalCleanup]
-        public void Cleanup()
-        {
-            this.meter?.Dispose();
-            this.provider?.Dispose();
-        }
-
-        [Benchmark]
-        public void HistogramHotPath()
-        {
-            this.histogram.Record(this.random.Next(MaxValue));
-        }
+    [Benchmark]
+    public void HistogramHotPath()
+    {
+        this.histogram.Record(this.random.Next(MaxValue));
     }
 }
--- test/Benchmarks/Metrics/ExponentialHistogramMapToIndexBenchmarks.cs
+++ test/Benchmarks/Metrics/ExponentialHistogramMapToIndexBenchmarks.cs
@@ -32,27 +32,26 @@ Apple M1 Max, 1 CPU, 10 logical and 10 physical cores
 | MapToIndex |    20 | 14.40 ns | 0.026 ns | 0.024 ns |         - |
 */
 
-namespace Benchmarks.Metrics
+namespace Benchmarks.Metrics;
+
+public class ExponentialHistogramMapToIndexBenchmarks
 {
-    public class ExponentialHistogramMapToIndexBenchmarks
+    private const int MaxValue = 10000;
+    private readonly Random random = new();
+    private Base2ExponentialBucketHistogram exponentialHistogram;
+
+    [Params(-11, 3, 20)]
+    public int Scale { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
     {
-        private const int MaxValue = 10000;
-        private readonly Random random = new();
-        private Base2ExponentialBucketHistogram exponentialHistogram;
+        this.exponentialHistogram = new Base2ExponentialBucketHistogram(scale: this.Scale);
+    }
 
-        [Params(-11, 3, 20)]
-        public int Scale { get; set; }
-
-        [GlobalSetup]
-        public void Setup()
-        {
-            this.exponentialHistogram = new Base2ExponentialBucketHistogram(scale: this.Scale);
-        }
-
-        [Benchmark]
-        public void MapToIndex()
-        {
-            this.exponentialHistogram.MapToIndex(this.random.Next(MaxValue));
-        }
+    [Benchmark]
+    public void MapToIndex()
+    {
+        this.exponentialHistogram.MapToIndex(this.random.Next(MaxValue));
     }
 }
