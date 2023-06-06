977df327e0cbc3efd8b870b037cfd73b64fd4a3f
Mikel Blanchard <mblanchard@macrosssoftware.com>
2023-06-06 16:40:51 -0700

    Expose logging artifacts in API as public.

--- src/OpenTelemetry.Extensions.Hosting/AssemblyInfo.cs
+++ src/OpenTelemetry.Extensions.Hosting/AssemblyInfo.cs
@@ -19,7 +19,6 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("OpenTelemetry.Extensions.Hosting.Tests" + AssemblyInfo.PublicKey)]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2" + AssemblyInfo.MoqPublicKey)]
 
-/* Note: OpenTelemetry.Extensions.Hosting temporarily sees OpenTelemetry.Api internals for LoggerProvider
 #if SIGNED
 internal static class AssemblyInfo
 {
@@ -33,4 +32,3 @@ internal static class AssemblyInfo
     public const string MoqPublicKey = "";
 }
 #endif
-*/
