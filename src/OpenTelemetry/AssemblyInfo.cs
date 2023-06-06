e74e0ed5dd2ae2fd5f99ecdf04c1b28341674121
Mikel Blanchard <mblanchard@macrosssoftware.com>
2023-06-06 13:58:09 -0700

    [console-exporter] Add extension on LoggerProviderBuilder (#4549)

--- src/OpenTelemetry/AssemblyInfo.cs
+++ src/OpenTelemetry/AssemblyInfo.cs
@@ -17,6 +17,7 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("OpenTelemetry.Tests" + AssemblyInfo.PublicKey)]
+[assembly: InternalsVisibleTo("OpenTelemetry.Exporter.Console" + AssemblyInfo.PublicKey)]
 [assembly: InternalsVisibleTo("OpenTelemetry.Exporter.InMemory" + AssemblyInfo.PublicKey)]
 [assembly: InternalsVisibleTo("OpenTelemetry.Exporter.Prometheus.AspNetCore" + AssemblyInfo.PublicKey)]
 [assembly: InternalsVisibleTo("OpenTelemetry.Exporter.Prometheus.AspNetCore.Tests" + AssemblyInfo.PublicKey)]
