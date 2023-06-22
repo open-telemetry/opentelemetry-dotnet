be51a687cc61d51f89ddbcc9c9ab149b84940a63
Timothy Mothra <tilee@microsoft.com>
2023-06-21 17:43:58 -0700

    update HttpSemanticConventions for Instrumentation.Http (#4538)

--- src/OpenTelemetry.Api/Internal/SemanticConventions.cs
+++ src/OpenTelemetry.Api/Internal/SemanticConventions.cs
@@ -117,6 +117,7 @@ namespace OpenTelemetry.Trace
         public const string AttributeNetworkProtocolVersion = "network.protocol.version"; // replaces: "http.flavor" (AttributeHttpFlavor)
         public const string AttributeServerAddress = "server.address"; // replaces: "net.host.name" (AttributeNetHostName)
         public const string AttributeServerPort = "server.port"; // replaces: "net.host.port" (AttributeNetHostPort) and "net.peer.port" (AttributeNetPeerPort)
+        public const string AttributeUrlFull = "url.full"; // replaces: "http.url" (AttributeHttpUrl)
         public const string AttributeUrlPath = "url.path"; // replaces: "http.target" (AttributeHttpTarget)
         public const string AttributeUrlScheme = "url.scheme"; // replaces: "http.scheme" (AttributeHttpScheme)
         public const string AttributeUrlQuery = "url.query";
