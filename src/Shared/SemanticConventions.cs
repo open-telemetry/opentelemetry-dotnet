b75d92283b8819a420476ecd9f38a7f63ecc4b51
Timothy Mothra Lee <tilee@microsoft.com>
2023-07-14 15:38:52 -0700

    update grpc attributes in aspnetcore

--- src/Shared/SemanticConventions.cs
+++ src/Shared/SemanticConventions.cs
@@ -114,6 +114,9 @@ namespace OpenTelemetry.Trace
         // v1.21.0 (unreleased as of this commit)
         // https://github.com/open-telemetry/semantic-conventions/blob/v1.21.0/docs/http/http-spans.md
         // https://github.com/open-telemetry/semantic-conventions/blob/v1.21.0/docs/database/database-spans.md
+        // https://github.com/open-telemetry/semantic-conventions/blob/v1.21.0/docs/rpc/rpc-spans.md
+        public const string AttributeClientAddress = "client.address";
+        public const string AttributeClientPort = "client.port";
         public const string AttributeHttpRequestMethod = "http.request.method"; // replaces: "http.method" (AttributeHttpMethod)
         public const string AttributeHttpResponseStatusCode = "http.response.status_code"; // replaces: "http.status_code" (AttributeHttpStatusCode)
         public const string AttributeNetworkProtocolVersion = "network.protocol.version"; // replaces: "http.flavor" (AttributeHttpFlavor)
