b5c6f8784b762f8bf5848bb497cea2a79f5cdcd0
Kyle Tully <kjt@cisco.com>
2023-07-18 11:42:57 -0500

    updated OpenTelemetry.Api files to use file scoped namespaces

--- src/OpenTelemetry.Api/Context/Propagation/PropagationContext.cs
+++ src/OpenTelemetry.Api/Context/Propagation/PropagationContext.cs
@@ -16,69 +16,68 @@
 
 using System.Diagnostics;
 
-namespace OpenTelemetry.Context.Propagation
+namespace OpenTelemetry.Context.Propagation;
+
+/// <summary>
+/// Stores propagation data.
+/// </summary>
+public readonly struct PropagationContext : IEquatable<PropagationContext>
 {
     /// <summary>
-    /// Stores propagation data.
+    /// Initializes a new instance of the <see cref="PropagationContext"/> struct.
     /// </summary>
-    public readonly struct PropagationContext : IEquatable<PropagationContext>
+    /// <param name="activityContext"><see cref="System.Diagnostics.ActivityContext"/>.</param>
+    /// <param name="baggage"><see cref="Baggage"/>.</param>
+    public PropagationContext(ActivityContext activityContext, Baggage baggage)
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="PropagationContext"/> struct.
-        /// </summary>
-        /// <param name="activityContext"><see cref="System.Diagnostics.ActivityContext"/>.</param>
-        /// <param name="baggage"><see cref="Baggage"/>.</param>
-        public PropagationContext(ActivityContext activityContext, Baggage baggage)
+        this.ActivityContext = activityContext;
+        this.Baggage = baggage;
+    }
+
+    /// <summary>
+    /// Gets <see cref="System.Diagnostics.ActivityContext"/>.
+    /// </summary>
+    public ActivityContext ActivityContext { get; }
+
+    /// <summary>
+    /// Gets <see cref="Baggage"/>.
+    /// </summary>
+    public Baggage Baggage { get; }
+
+    /// <summary>
+    /// Compare two entries of <see cref="PropagationContext"/> for equality.
+    /// </summary>
+    /// <param name="left">First Entry to compare.</param>
+    /// <param name="right">Second Entry to compare.</param>
+    public static bool operator ==(PropagationContext left, PropagationContext right) => left.Equals(right);
+
+    /// <summary>
+    /// Compare two entries of <see cref="PropagationContext"/> for not equality.
+    /// </summary>
+    /// <param name="left">First Entry to compare.</param>
+    /// <param name="right">Second Entry to compare.</param>
+    public static bool operator !=(PropagationContext left, PropagationContext right) => !(left == right);
+
+    /// <inheritdoc/>
+    public bool Equals(PropagationContext value)
+    {
+        return this.ActivityContext == value.ActivityContext
+            && this.Baggage == value.Baggage;
+    }
+
+    /// <inheritdoc/>
+    public override bool Equals(object obj) => (obj is PropagationContext context) && this.Equals(context);
+
+    /// <inheritdoc/>
+    public override int GetHashCode()
+    {
+        var hash = 323591981;
+        unchecked
         {
-            this.ActivityContext = activityContext;
-            this.Baggage = baggage;
+            hash = (hash * -1521134295) + this.ActivityContext.GetHashCode();
+            hash = (hash * -1521134295) + this.Baggage.GetHashCode();
         }
 
-        /// <summary>
-        /// Gets <see cref="System.Diagnostics.ActivityContext"/>.
-        /// </summary>
-        public ActivityContext ActivityContext { get; }
-
-        /// <summary>
-        /// Gets <see cref="Baggage"/>.
-        /// </summary>
-        public Baggage Baggage { get; }
-
-        /// <summary>
-        /// Compare two entries of <see cref="PropagationContext"/> for equality.
-        /// </summary>
-        /// <param name="left">First Entry to compare.</param>
-        /// <param name="right">Second Entry to compare.</param>
-        public static bool operator ==(PropagationContext left, PropagationContext right) => left.Equals(right);
-
-        /// <summary>
-        /// Compare two entries of <see cref="PropagationContext"/> for not equality.
-        /// </summary>
-        /// <param name="left">First Entry to compare.</param>
-        /// <param name="right">Second Entry to compare.</param>
-        public static bool operator !=(PropagationContext left, PropagationContext right) => !(left == right);
-
-        /// <inheritdoc/>
-        public bool Equals(PropagationContext value)
-        {
-            return this.ActivityContext == value.ActivityContext
-                && this.Baggage == value.Baggage;
-        }
-
-        /// <inheritdoc/>
-        public override bool Equals(object obj) => (obj is PropagationContext context) && this.Equals(context);
-
-        /// <inheritdoc/>
-        public override int GetHashCode()
-        {
-            var hash = 323591981;
-            unchecked
-            {
-                hash = (hash * -1521134295) + this.ActivityContext.GetHashCode();
-                hash = (hash * -1521134295) + this.Baggage.GetHashCode();
-            }
-
-            return hash;
-        }
+        return hash;
     }
 }
--- test/OpenTelemetry.Api.Tests/Trace/TracerTest.cs
+++ test/OpenTelemetry.Api.Tests/Trace/TracerTest.cs
@@ -17,281 +17,280 @@
 using System.Diagnostics;
 using Xunit;
 
-namespace OpenTelemetry.Trace.Tests
+namespace OpenTelemetry.Trace.Tests;
+
+public class TracerTest : IDisposable
 {
-    public class TracerTest : IDisposable
+    // TODO: This is only a basic test. This must cover the entire shim API scenarios.
+    private readonly Tracer tracer;
+
+    public TracerTest()
     {
-        // TODO: This is only a basic test. This must cover the entire shim API scenarios.
-        private readonly Tracer tracer;
+        this.tracer = TracerProvider.Default.GetTracer("tracername", "tracerversion");
+    }
 
-        public TracerTest()
+    [Fact]
+    public void CurrentSpanNullByDefault()
+    {
+        var current = Tracer.CurrentSpan;
+        Assert.True(IsNoopSpan(current));
+        Assert.False(current.Context.IsValid);
+    }
+
+    [Fact]
+    public void TracerStartWithSpan()
+    {
+        Tracer.WithSpan(TelemetrySpan.NoopInstance);
+        var current = Tracer.CurrentSpan;
+        Assert.Same(current, TelemetrySpan.NoopInstance);
+    }
+
+    [Fact]
+    public void TracerStartReturnsNoopSpanWhenNoSdk()
+    {
+        var span = this.tracer.StartSpan("name");
+        Assert.True(IsNoopSpan(span));
+        Assert.False(span.Context.IsValid);
+        Assert.False(span.IsRecording);
+    }
+
+    [Fact]
+    public void Tracer_StartRootSpan_BadArgs_NullSpanName()
+    {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddSource("tracername")
+            .Build();
+
+        var span1 = this.tracer.StartRootSpan(null);
+        Assert.Null(span1.Activity.DisplayName);
+
+        var span2 = this.tracer.StartRootSpan(null, SpanKind.Client);
+        Assert.Null(span2.Activity.DisplayName);
+
+        var span3 = this.tracer.StartRootSpan(null, SpanKind.Client, default);
+        Assert.Null(span3.Activity.DisplayName);
+    }
+
+    [Fact(Skip = "See https://github.com/open-telemetry/opentelemetry-dotnet/issues/2803")]
+    public async Task Tracer_StartRootSpan_StartsNewTrace()
+    {
+        var exportedItems = new List<Activity>();
+
+        using var tracer = Sdk.CreateTracerProviderBuilder()
+            .AddSource("tracername")
+            .AddInMemoryExporter(exportedItems)
+            .Build();
+
+        async Task DoSomeAsyncWork()
         {
-            this.tracer = TracerProvider.Default.GetTracer("tracername", "tracerversion");
-        }
-
-        [Fact]
-        public void CurrentSpanNullByDefault()
-        {
-            var current = Tracer.CurrentSpan;
-            Assert.True(IsNoopSpan(current));
-            Assert.False(current.Context.IsValid);
-        }
-
-        [Fact]
-        public void TracerStartWithSpan()
-        {
-            Tracer.WithSpan(TelemetrySpan.NoopInstance);
-            var current = Tracer.CurrentSpan;
-            Assert.Same(current, TelemetrySpan.NoopInstance);
-        }
-
-        [Fact]
-        public void TracerStartReturnsNoopSpanWhenNoSdk()
-        {
-            var span = this.tracer.StartSpan("name");
-            Assert.True(IsNoopSpan(span));
-            Assert.False(span.Context.IsValid);
-            Assert.False(span.IsRecording);
-        }
-
-        [Fact]
-        public void Tracer_StartRootSpan_BadArgs_NullSpanName()
-        {
-            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-                .AddSource("tracername")
-                .Build();
-
-            var span1 = this.tracer.StartRootSpan(null);
-            Assert.Null(span1.Activity.DisplayName);
-
-            var span2 = this.tracer.StartRootSpan(null, SpanKind.Client);
-            Assert.Null(span2.Activity.DisplayName);
-
-            var span3 = this.tracer.StartRootSpan(null, SpanKind.Client, default);
-            Assert.Null(span3.Activity.DisplayName);
-        }
-
-        [Fact(Skip = "See https://github.com/open-telemetry/opentelemetry-dotnet/issues/2803")]
-        public async Task Tracer_StartRootSpan_StartsNewTrace()
-        {
-            var exportedItems = new List<Activity>();
-
-            using var tracer = Sdk.CreateTracerProviderBuilder()
-                .AddSource("tracername")
-                .AddInMemoryExporter(exportedItems)
-                .Build();
-
-            async Task DoSomeAsyncWork()
+            await Task.Delay(10).ConfigureAwait(false);
+            using (tracer.GetTracer("tracername").StartRootSpan("RootSpan2"))
             {
                 await Task.Delay(10).ConfigureAwait(false);
-                using (tracer.GetTracer("tracername").StartRootSpan("RootSpan2"))
-                {
-                    await Task.Delay(10).ConfigureAwait(false);
-                }
             }
-
-            using (tracer.GetTracer("tracername").StartActiveSpan("RootSpan1"))
-            {
-                await DoSomeAsyncWork().ConfigureAwait(false);
-            }
-
-            Assert.Equal(2, exportedItems.Count);
-
-            var rootSpan2 = exportedItems[0];
-            var rootSpan1 = exportedItems[1];
-            Assert.Equal("RootSpan2", rootSpan2.DisplayName);
-            Assert.Equal("RootSpan1", rootSpan1.DisplayName);
-            Assert.Equal(default, rootSpan1.ParentSpanId);
-
-            // This is where this test currently fails
-            // rootSpan2 should be a root span of a new trace and not a child of rootSpan1
-            Assert.Equal(default, rootSpan2.ParentSpanId);
-            Assert.NotEqual(rootSpan2.TraceId, rootSpan1.TraceId);
-            Assert.NotEqual(rootSpan2.ParentSpanId, rootSpan1.SpanId);
         }
 
-        [Fact]
-        public void Tracer_StartSpan_BadArgs_NullSpanName()
+        using (tracer.GetTracer("tracername").StartActiveSpan("RootSpan1"))
         {
-            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-                .AddSource("tracername")
-                .Build();
-
-            var span1 = this.tracer.StartSpan(null);
-            Assert.Null(span1.Activity.DisplayName);
-
-            var span2 = this.tracer.StartSpan(null, SpanKind.Client);
-            Assert.Null(span2.Activity.DisplayName);
-
-            var span3 = this.tracer.StartSpan(null, SpanKind.Client, null);
-            Assert.Null(span3.Activity.DisplayName);
+            await DoSomeAsyncWork().ConfigureAwait(false);
         }
 
-        [Fact]
-        public void Tracer_StartActiveSpan_BadArgs_NullSpanName()
-        {
-            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-                .AddSource("tracername")
-                .Build();
+        Assert.Equal(2, exportedItems.Count);
 
-            var span1 = this.tracer.StartActiveSpan(null);
-            Assert.Null(span1.Activity.DisplayName);
+        var rootSpan2 = exportedItems[0];
+        var rootSpan1 = exportedItems[1];
+        Assert.Equal("RootSpan2", rootSpan2.DisplayName);
+        Assert.Equal("RootSpan1", rootSpan1.DisplayName);
+        Assert.Equal(default, rootSpan1.ParentSpanId);
 
-            var span2 = this.tracer.StartActiveSpan(null, SpanKind.Client);
-            Assert.Null(span2.Activity.DisplayName);
+        // This is where this test currently fails
+        // rootSpan2 should be a root span of a new trace and not a child of rootSpan1
+        Assert.Equal(default, rootSpan2.ParentSpanId);
+        Assert.NotEqual(rootSpan2.TraceId, rootSpan1.TraceId);
+        Assert.NotEqual(rootSpan2.ParentSpanId, rootSpan1.SpanId);
+    }
 
-            var span3 = this.tracer.StartActiveSpan(null, SpanKind.Client, null);
-            Assert.Null(span3.Activity.DisplayName);
-        }
+    [Fact]
+    public void Tracer_StartSpan_BadArgs_NullSpanName()
+    {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddSource("tracername")
+            .Build();
 
-        [Fact]
-        public void Tracer_StartSpan_FromParent_BadArgs_NullSpanName()
-        {
-            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-                .AddSource("tracername")
-                .Build();
+        var span1 = this.tracer.StartSpan(null);
+        Assert.Null(span1.Activity.DisplayName);
 
-            var span1 = this.tracer.StartSpan(null, SpanKind.Client, TelemetrySpan.NoopInstance);
-            Assert.Null(span1.Activity.DisplayName);
+        var span2 = this.tracer.StartSpan(null, SpanKind.Client);
+        Assert.Null(span2.Activity.DisplayName);
 
-            var span2 = this.tracer.StartSpan(null, SpanKind.Client, TelemetrySpan.NoopInstance, default);
-            Assert.Null(span2.Activity.DisplayName);
-        }
+        var span3 = this.tracer.StartSpan(null, SpanKind.Client, null);
+        Assert.Null(span3.Activity.DisplayName);
+    }
 
-        [Fact]
-        public void Tracer_StartSpan_FromParentContext_BadArgs_NullSpanName()
-        {
-            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-                .AddSource("tracername")
-                .Build();
+    [Fact]
+    public void Tracer_StartActiveSpan_BadArgs_NullSpanName()
+    {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddSource("tracername")
+            .Build();
 
-            var blankContext = default(SpanContext);
+        var span1 = this.tracer.StartActiveSpan(null);
+        Assert.Null(span1.Activity.DisplayName);
 
-            var span1 = this.tracer.StartSpan(null, SpanKind.Client, blankContext);
-            Assert.Null(span1.Activity.DisplayName);
+        var span2 = this.tracer.StartActiveSpan(null, SpanKind.Client);
+        Assert.Null(span2.Activity.DisplayName);
 
-            var span2 = this.tracer.StartSpan(null, SpanKind.Client, blankContext, default);
-            Assert.Null(span2.Activity.DisplayName);
-        }
+        var span3 = this.tracer.StartActiveSpan(null, SpanKind.Client, null);
+        Assert.Null(span3.Activity.DisplayName);
+    }
 
-        [Fact]
-        public void Tracer_StartActiveSpan_FromParent_BadArgs_NullSpanName()
-        {
-            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-                .AddSource("tracername")
-                .Build();
+    [Fact]
+    public void Tracer_StartSpan_FromParent_BadArgs_NullSpanName()
+    {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddSource("tracername")
+            .Build();
 
-            var span1 = this.tracer.StartActiveSpan(null, SpanKind.Client, TelemetrySpan.NoopInstance);
-            Assert.Null(span1.Activity.DisplayName);
+        var span1 = this.tracer.StartSpan(null, SpanKind.Client, TelemetrySpan.NoopInstance);
+        Assert.Null(span1.Activity.DisplayName);
 
-            var span2 = this.tracer.StartActiveSpan(null, SpanKind.Client, TelemetrySpan.NoopInstance, default);
-            Assert.Null(span2.Activity.DisplayName);
-        }
+        var span2 = this.tracer.StartSpan(null, SpanKind.Client, TelemetrySpan.NoopInstance, default);
+        Assert.Null(span2.Activity.DisplayName);
+    }
 
-        [Fact]
-        public void Tracer_StartActiveSpan_FromParentContext_BadArgs_NullSpanName()
-        {
-            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-                .AddSource("tracername")
-                .Build();
+    [Fact]
+    public void Tracer_StartSpan_FromParentContext_BadArgs_NullSpanName()
+    {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddSource("tracername")
+            .Build();
 
-            var blankContext = default(SpanContext);
+        var blankContext = default(SpanContext);
 
-            var span1 = this.tracer.StartActiveSpan(null, SpanKind.Client, blankContext);
-            Assert.Null(span1.Activity.DisplayName);
+        var span1 = this.tracer.StartSpan(null, SpanKind.Client, blankContext);
+        Assert.Null(span1.Activity.DisplayName);
 
-            var span2 = this.tracer.StartActiveSpan(null, SpanKind.Client, blankContext, default);
-            Assert.Null(span2.Activity.DisplayName);
-        }
+        var span2 = this.tracer.StartSpan(null, SpanKind.Client, blankContext, default);
+        Assert.Null(span2.Activity.DisplayName);
+    }
 
-        [Fact]
-        public void Tracer_StartActiveSpan_CreatesActiveSpan()
-        {
-            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-                .AddSource("tracername")
-                .Build();
+    [Fact]
+    public void Tracer_StartActiveSpan_FromParent_BadArgs_NullSpanName()
+    {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddSource("tracername")
+            .Build();
 
-            var span1 = this.tracer.StartActiveSpan("Test");
-            Assert.Equal(span1.Activity.SpanId, Tracer.CurrentSpan.Context.SpanId);
+        var span1 = this.tracer.StartActiveSpan(null, SpanKind.Client, TelemetrySpan.NoopInstance);
+        Assert.Null(span1.Activity.DisplayName);
 
-            var span2 = this.tracer.StartActiveSpan("Test", SpanKind.Client);
-            Assert.Equal(span2.Activity.SpanId, Tracer.CurrentSpan.Context.SpanId);
+        var span2 = this.tracer.StartActiveSpan(null, SpanKind.Client, TelemetrySpan.NoopInstance, default);
+        Assert.Null(span2.Activity.DisplayName);
+    }
 
-            var span = this.tracer.StartSpan("foo");
-            Tracer.WithSpan(span);
+    [Fact]
+    public void Tracer_StartActiveSpan_FromParentContext_BadArgs_NullSpanName()
+    {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddSource("tracername")
+            .Build();
 
-            var span3 = this.tracer.StartActiveSpan("Test", SpanKind.Client, span);
-            Assert.Equal(span3.Activity.SpanId, Tracer.CurrentSpan.Context.SpanId);
+        var blankContext = default(SpanContext);
 
-            var spanContext = new SpanContext(ActivityTraceId.CreateRandom(), ActivitySpanId.CreateRandom(), ActivityTraceFlags.Recorded);
-            var span4 = this.tracer.StartActiveSpan("Test", SpanKind.Client, spanContext);
-            Assert.Equal(span4.Activity.SpanId, Tracer.CurrentSpan.Context.SpanId);
-        }
+        var span1 = this.tracer.StartActiveSpan(null, SpanKind.Client, blankContext);
+        Assert.Null(span1.Activity.DisplayName);
 
-        [Fact]
-        public void GetCurrentSpanBlank()
-        {
-            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-                .AddSource("tracername")
-                .Build();
-            Assert.False(Tracer.CurrentSpan.Context.IsValid);
-        }
+        var span2 = this.tracer.StartActiveSpan(null, SpanKind.Client, blankContext, default);
+        Assert.Null(span2.Activity.DisplayName);
+    }
 
-        [Fact]
-        public void GetCurrentSpanBlankWontThrowOnEnd()
-        {
-            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-                .AddSource("tracername")
-                .Build();
-            var current = Tracer.CurrentSpan;
-            current.End();
-        }
+    [Fact]
+    public void Tracer_StartActiveSpan_CreatesActiveSpan()
+    {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddSource("tracername")
+            .Build();
 
-        [Fact]
-        public void GetCurrentSpan()
-        {
-            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-                .AddSource("tracername")
-                .Build();
+        var span1 = this.tracer.StartActiveSpan("Test");
+        Assert.Equal(span1.Activity.SpanId, Tracer.CurrentSpan.Context.SpanId);
 
-            var span = this.tracer.StartSpan("foo");
-            Tracer.WithSpan(span);
+        var span2 = this.tracer.StartActiveSpan("Test", SpanKind.Client);
+        Assert.Equal(span2.Activity.SpanId, Tracer.CurrentSpan.Context.SpanId);
 
-            Assert.Equal(span.Context.SpanId, Tracer.CurrentSpan.Context.SpanId);
-            Assert.True(Tracer.CurrentSpan.Context.IsValid);
-        }
+        var span = this.tracer.StartSpan("foo");
+        Tracer.WithSpan(span);
 
-        [Fact]
-        public void CreateSpan_Sampled()
-        {
-            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-                .AddSource("tracername")
-                .Build();
-            var span = this.tracer.StartSpan("foo");
-            Assert.True(span.IsRecording);
-        }
+        var span3 = this.tracer.StartActiveSpan("Test", SpanKind.Client, span);
+        Assert.Equal(span3.Activity.SpanId, Tracer.CurrentSpan.Context.SpanId);
 
-        [Fact]
-        public void CreateSpan_NotSampled()
-        {
-            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-                .AddSource("tracername")
-                .SetSampler(new AlwaysOffSampler())
-                .Build();
+        var spanContext = new SpanContext(ActivityTraceId.CreateRandom(), ActivitySpanId.CreateRandom(), ActivityTraceFlags.Recorded);
+        var span4 = this.tracer.StartActiveSpan("Test", SpanKind.Client, spanContext);
+        Assert.Equal(span4.Activity.SpanId, Tracer.CurrentSpan.Context.SpanId);
+    }
 
-            var span = this.tracer.StartSpan("foo");
-            Assert.False(span.IsRecording);
-        }
+    [Fact]
+    public void GetCurrentSpanBlank()
+    {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddSource("tracername")
+            .Build();
+        Assert.False(Tracer.CurrentSpan.Context.IsValid);
+    }
 
-        public void Dispose()
-        {
-            Activity.Current = null;
-            GC.SuppressFinalize(this);
-        }
+    [Fact]
+    public void GetCurrentSpanBlankWontThrowOnEnd()
+    {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddSource("tracername")
+            .Build();
+        var current = Tracer.CurrentSpan;
+        current.End();
+    }
 
-        private static bool IsNoopSpan(TelemetrySpan span)
-        {
-            return span.Activity == null;
-        }
+    [Fact]
+    public void GetCurrentSpan()
+    {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddSource("tracername")
+            .Build();
+
+        var span = this.tracer.StartSpan("foo");
+        Tracer.WithSpan(span);
+
+        Assert.Equal(span.Context.SpanId, Tracer.CurrentSpan.Context.SpanId);
+        Assert.True(Tracer.CurrentSpan.Context.IsValid);
+    }
+
+    [Fact]
+    public void CreateSpan_Sampled()
+    {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddSource("tracername")
+            .Build();
+        var span = this.tracer.StartSpan("foo");
+        Assert.True(span.IsRecording);
+    }
+
+    [Fact]
+    public void CreateSpan_NotSampled()
+    {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddSource("tracername")
+            .SetSampler(new AlwaysOffSampler())
+            .Build();
+
+        var span = this.tracer.StartSpan("foo");
+        Assert.False(span.IsRecording);
+    }
+
+    public void Dispose()
+    {
+        Activity.Current = null;
+        GC.SuppressFinalize(this);
+    }
+
+    private static bool IsNoopSpan(TelemetrySpan span)
+    {
+        return span.Activity == null;
     }
 }
