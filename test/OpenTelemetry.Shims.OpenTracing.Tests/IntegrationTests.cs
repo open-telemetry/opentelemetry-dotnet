b3273ea4f7382b1bed4a19729f56a05f96331492
Jarosal <azethis@gmail.com>
2023-07-24 16:01:07 +0200

    Formatted OpenTelemetry.Shims.OpenTracing

--- test/OpenTelemetry.Shims.OpenTracing.Tests/IntegrationTests.cs
+++ test/OpenTelemetry.Shims.OpenTracing.Tests/IntegrationTests.cs
@@ -20,130 +20,129 @@ using OpenTelemetry.Trace;
 using OpenTracing;
 using Xunit;
 
-namespace OpenTelemetry.Shims.OpenTracing.Tests
+namespace OpenTelemetry.Shims.OpenTracing.Tests;
+
+public class IntegrationTests
 {
-    public class IntegrationTests
+    private const string ChildActivitySource = "ChildActivitySource";
+    private const string ParentActivitySource = "ParentActivitySource";
+    private const string ShimTracerName = "OpenTracing.Shim";
+
+    [Theory]
+    [InlineData(SamplingDecision.Drop, SamplingDecision.Drop, SamplingDecision.Drop)]
+    [InlineData(SamplingDecision.Drop, SamplingDecision.RecordAndSample, SamplingDecision.Drop)]
+    [InlineData(SamplingDecision.Drop, SamplingDecision.RecordOnly, SamplingDecision.Drop)]
+    [InlineData(SamplingDecision.RecordOnly, SamplingDecision.RecordAndSample, SamplingDecision.RecordOnly)]
+    [InlineData(SamplingDecision.RecordAndSample, SamplingDecision.RecordOnly, SamplingDecision.RecordAndSample)]
+    [InlineData(SamplingDecision.RecordAndSample, SamplingDecision.Drop, SamplingDecision.RecordAndSample)]
+    public void WithActivities(
+        SamplingDecision parentActivitySamplingDecision,
+        SamplingDecision shimSamplingDecision,
+        SamplingDecision childActivitySamplingDecision)
     {
-        private const string ChildActivitySource = "ChildActivitySource";
-        private const string ParentActivitySource = "ParentActivitySource";
-        private const string ShimTracerName = "OpenTracing.Shim";
+        var exportedSpans = new List<Activity>();
 
-        [Theory]
-        [InlineData(SamplingDecision.Drop, SamplingDecision.Drop, SamplingDecision.Drop)]
-        [InlineData(SamplingDecision.Drop, SamplingDecision.RecordAndSample, SamplingDecision.Drop)]
-        [InlineData(SamplingDecision.Drop, SamplingDecision.RecordOnly, SamplingDecision.Drop)]
-        [InlineData(SamplingDecision.RecordOnly, SamplingDecision.RecordAndSample, SamplingDecision.RecordOnly)]
-        [InlineData(SamplingDecision.RecordAndSample, SamplingDecision.RecordOnly, SamplingDecision.RecordAndSample)]
-        [InlineData(SamplingDecision.RecordAndSample, SamplingDecision.Drop, SamplingDecision.RecordAndSample)]
-        public void WithActivities(
-            SamplingDecision parentActivitySamplingDecision,
-            SamplingDecision shimSamplingDecision,
-            SamplingDecision childActivitySamplingDecision)
+        const string ParentActivityName = "ParentActivity";
+        const string ShimActivityName = "ShimActivity";
+        const string ChildActivityName = "ChildActivity";
+
+        var testSampler = new TestSampler((samplingParameters) =>
+            samplingParameters.Name switch
+            {
+                ParentActivityName => parentActivitySamplingDecision,
+                ShimActivityName => shimSamplingDecision,
+                ChildActivityName => childActivitySamplingDecision,
+                _ => SamplingDecision.Drop,
+            });
+
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddInMemoryExporter(exportedSpans)
+            .SetSampler(testSampler)
+            .When(
+                parentActivitySamplingDecision == SamplingDecision.RecordAndSample,
+                b => b.AddSource(ParentActivitySource))
+            .When(
+                shimSamplingDecision == SamplingDecision.RecordAndSample,
+                b => b.AddSource(ShimTracerName))
+            .When(
+                childActivitySamplingDecision == SamplingDecision.RecordAndSample,
+                b => b.AddSource(ChildActivitySource))
+            .Build();
+
+        ITracer otTracer = new TracerShim(
+            tracerProvider.GetTracer(ShimTracerName),
+            Propagators.DefaultTextMapPropagator);
+
+        // Real usage requires a call OpenTracing.Util.GlobalTracer.Register(otTracer),
+        // however, that can only happen once per process, we don't do it here so we
+        // can run multiple tests in the same process.
+
+        using var parentActivitySource = new ActivitySource(ParentActivitySource);
+        using var childActivitySource = new ActivitySource(ChildActivitySource);
+
+        using (var parentActivity = parentActivitySource.StartActivity(ParentActivityName))
         {
-            var exportedSpans = new List<Activity>();
-
-            const string ParentActivityName = "ParentActivity";
-            const string ShimActivityName = "ShimActivity";
-            const string ChildActivityName = "ChildActivity";
-
-            var testSampler = new TestSampler((samplingParameters) =>
-                samplingParameters.Name switch
-                {
-                    ParentActivityName => parentActivitySamplingDecision,
-                    ShimActivityName => shimSamplingDecision,
-                    ChildActivityName => childActivitySamplingDecision,
-                    _ => SamplingDecision.Drop,
-                });
-
-            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
-                .AddInMemoryExporter(exportedSpans)
-                .SetSampler(testSampler)
-                .When(
-                    parentActivitySamplingDecision == SamplingDecision.RecordAndSample,
-                    b => b.AddSource(ParentActivitySource))
-                .When(
-                    shimSamplingDecision == SamplingDecision.RecordAndSample,
-                    b => b.AddSource(ShimTracerName))
-                .When(
-                    childActivitySamplingDecision == SamplingDecision.RecordAndSample,
-                    b => b.AddSource(ChildActivitySource))
-                .Build();
-
-            ITracer otTracer = new TracerShim(
-                tracerProvider.GetTracer(ShimTracerName),
-                Propagators.DefaultTextMapPropagator);
-
-            // Real usage requires a call OpenTracing.Util.GlobalTracer.Register(otTracer),
-            // however, that can only happen once per process, we don't do it here so we
-            // can run multiple tests in the same process.
-
-            using var parentActivitySource = new ActivitySource(ParentActivitySource);
-            using var childActivitySource = new ActivitySource(ChildActivitySource);
-
-            using (var parentActivity = parentActivitySource.StartActivity(ParentActivityName))
+            using (IScope parentScope = otTracer.BuildSpan(ShimActivityName).StartActive())
             {
-                using (IScope parentScope = otTracer.BuildSpan(ShimActivityName).StartActive())
-                {
-                    parentScope.Span.SetTag("parent", true);
+                parentScope.Span.SetTag("parent", true);
 
-                    using var childActivity = childActivitySource.StartActivity(ChildActivityName);
-                }
-            }
-
-            var expectedExportedSpans = new string[]
-                {
-                    childActivitySamplingDecision == SamplingDecision.RecordAndSample ? ChildActivityName : null,
-                    shimSamplingDecision == SamplingDecision.RecordAndSample ? ShimActivityName : null,
-                    parentActivitySamplingDecision == SamplingDecision.RecordAndSample ? ParentActivityName : null,
-                }
-                .Where(s => s is not null)
-                .ToList();
-
-            for (int i = 0; i < expectedExportedSpans.Count; i++)
-            {
-                Assert.Equal(expectedExportedSpans[i], exportedSpans[i].DisplayName);
-            }
-
-            if (childActivitySamplingDecision == SamplingDecision.RecordAndSample)
-            {
-                if (shimSamplingDecision == SamplingDecision.RecordAndSample)
-                {
-                    Assert.Same(exportedSpans[1], exportedSpans[0].Parent);
-                }
+                using var childActivity = childActivitySource.StartActivity(ChildActivityName);
             }
         }
 
-        private class TestSampler : Sampler
-        {
-            private readonly Func<SamplingParameters, SamplingDecision> shouldSampleDelegate;
-
-            public TestSampler(Func<SamplingParameters, SamplingDecision> shouldSampleDelegate)
+        var expectedExportedSpans = new string[]
             {
-                this.shouldSampleDelegate = shouldSampleDelegate;
+                childActivitySamplingDecision == SamplingDecision.RecordAndSample ? ChildActivityName : null,
+                shimSamplingDecision == SamplingDecision.RecordAndSample ? ShimActivityName : null,
+                parentActivitySamplingDecision == SamplingDecision.RecordAndSample ? ParentActivityName : null,
             }
+            .Where(s => s is not null)
+            .ToList();
 
-            public override SamplingResult ShouldSample(in SamplingParameters samplingParameters)
+        for (int i = 0; i < expectedExportedSpans.Count; i++)
+        {
+            Assert.Equal(expectedExportedSpans[i], exportedSpans[i].DisplayName);
+        }
+
+        if (childActivitySamplingDecision == SamplingDecision.RecordAndSample)
+        {
+            if (shimSamplingDecision == SamplingDecision.RecordAndSample)
             {
-                return new SamplingResult(this.shouldSampleDelegate(samplingParameters));
+                Assert.Same(exportedSpans[1], exportedSpans[0].Parent);
             }
         }
     }
 
-    [System.Diagnostics.CodeAnalysis.SuppressMessage(
-        "StyleCop.CSharp.MaintainabilityRules", "SA1402:File may only contain a single type", Justification = "Local use only")]
-    internal static class ConditionalTracerProviderBuilderExtension
+    private class TestSampler : Sampler
     {
-        public static TracerProviderBuilder When(
-            this TracerProviderBuilder builder,
-            bool condition,
-            Func<TracerProviderBuilder, TracerProviderBuilder> conditionalDelegate)
-        {
-            if (condition)
-            {
-                builder = conditionalDelegate(builder);
-            }
+        private readonly Func<SamplingParameters, SamplingDecision> shouldSampleDelegate;
 
-            return builder;
+        public TestSampler(Func<SamplingParameters, SamplingDecision> shouldSampleDelegate)
+        {
+            this.shouldSampleDelegate = shouldSampleDelegate;
         }
+
+        public override SamplingResult ShouldSample(in SamplingParameters samplingParameters)
+        {
+            return new SamplingResult(this.shouldSampleDelegate(samplingParameters));
+        }
+    }
+}
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage(
+    "StyleCop.CSharp.MaintainabilityRules", "SA1402:File may only contain a single type", Justification = "Local use only")]
+internal static class ConditionalTracerProviderBuilderExtension
+{
+    public static TracerProviderBuilder When(
+        this TracerProviderBuilder builder,
+        bool condition,
+        Func<TracerProviderBuilder, TracerProviderBuilder> conditionalDelegate)
+    {
+        if (condition)
+        {
+            builder = conditionalDelegate(builder);
+        }
+
+        return builder;
     }
 }
