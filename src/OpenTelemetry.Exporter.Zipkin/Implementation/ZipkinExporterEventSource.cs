f0006d59eecba1f095fcff505708ca8aecc0e518
jarosal <52526948+jarosal@users.noreply.github.com>
2023-07-26 20:04:47 +0200

    Update OpenTelemetry.Exporter.Zipkin to use filescoped namespaces (#4699)

--- src/OpenTelemetry.Exporter.Zipkin/Implementation/ZipkinExporterEventSource.cs
+++ src/OpenTelemetry.Exporter.Zipkin/Implementation/ZipkinExporterEventSource.cs
@@ -17,41 +17,40 @@
 using System.Diagnostics.Tracing;
 using OpenTelemetry.Internal;
 
-namespace OpenTelemetry.Exporter.Zipkin.Implementation
+namespace OpenTelemetry.Exporter.Zipkin.Implementation;
+
+/// <summary>
+/// EventSource events emitted from the project.
+/// </summary>
+[EventSource(Name = "OpenTelemetry-Exporter-Zipkin")]
+internal sealed class ZipkinExporterEventSource : EventSource
 {
-    /// <summary>
-    /// EventSource events emitted from the project.
-    /// </summary>
-    [EventSource(Name = "OpenTelemetry-Exporter-Zipkin")]
-    internal sealed class ZipkinExporterEventSource : EventSource
+    public static ZipkinExporterEventSource Log = new();
+
+    [NonEvent]
+    public void FailedExport(Exception ex)
     {
-        public static ZipkinExporterEventSource Log = new();
-
-        [NonEvent]
-        public void FailedExport(Exception ex)
+        if (this.IsEnabled(EventLevel.Error, EventKeywords.All))
         {
-            if (this.IsEnabled(EventLevel.Error, EventKeywords.All))
-            {
-                this.FailedExport(ex.ToInvariantString());
-            }
+            this.FailedExport(ex.ToInvariantString());
         }
+    }
 
-        [Event(1, Message = "Failed to export activities: '{0}'", Level = EventLevel.Error)]
-        public void FailedExport(string exception)
-        {
-            this.WriteEvent(1, exception);
-        }
+    [Event(1, Message = "Failed to export activities: '{0}'", Level = EventLevel.Error)]
+    public void FailedExport(string exception)
+    {
+        this.WriteEvent(1, exception);
+    }
 
-        [Event(2, Message = "Unsupported attribute type '{0}' for '{1}'. Attribute will not be exported.", Level = EventLevel.Warning)]
-        public void UnsupportedAttributeType(string type, string key)
-        {
-            this.WriteEvent(2, type.ToString(), key);
-        }
+    [Event(2, Message = "Unsupported attribute type '{0}' for '{1}'. Attribute will not be exported.", Level = EventLevel.Warning)]
+    public void UnsupportedAttributeType(string type, string key)
+    {
+        this.WriteEvent(2, type.ToString(), key);
+    }
 
-        [Event(3, Message = "{0} environment variable has an invalid value: '{1}'", Level = EventLevel.Warning)]
-        public void InvalidEnvironmentVariable(string key, string value)
-        {
-            this.WriteEvent(3, key, value);
-        }
+    [Event(3, Message = "{0} environment variable has an invalid value: '{1}'", Level = EventLevel.Warning)]
+    public void InvalidEnvironmentVariable(string key, string value)
+    {
+        this.WriteEvent(3, key, value);
     }
 }
--- src/OpenTelemetry.Exporter.Zipkin/Implementation/ZipkinSpan.cs
+++ src/OpenTelemetry.Exporter.Zipkin/Implementation/ZipkinSpan.cs
@@ -18,185 +18,184 @@ using System.Globalization;
 using System.Text.Json;
 using OpenTelemetry.Internal;
 
-namespace OpenTelemetry.Exporter.Zipkin.Implementation
+namespace OpenTelemetry.Exporter.Zipkin.Implementation;
+
+internal readonly struct ZipkinSpan
 {
-    internal readonly struct ZipkinSpan
+    public ZipkinSpan(
+        string traceId,
+        string parentId,
+        string id,
+        string kind,
+        string name,
+        long? timestamp,
+        long? duration,
+        ZipkinEndpoint localEndpoint,
+        ZipkinEndpoint remoteEndpoint,
+        in PooledList<ZipkinAnnotation> annotations,
+        in PooledList<KeyValuePair<string, object>> tags,
+        bool? debug,
+        bool? shared)
     {
-        public ZipkinSpan(
-            string traceId,
-            string parentId,
-            string id,
-            string kind,
-            string name,
-            long? timestamp,
-            long? duration,
-            ZipkinEndpoint localEndpoint,
-            ZipkinEndpoint remoteEndpoint,
-            in PooledList<ZipkinAnnotation> annotations,
-            in PooledList<KeyValuePair<string, object>> tags,
-            bool? debug,
-            bool? shared)
-        {
-            Guard.ThrowIfNullOrWhitespace(traceId);
-            Guard.ThrowIfNullOrWhitespace(id);
+        Guard.ThrowIfNullOrWhitespace(traceId);
+        Guard.ThrowIfNullOrWhitespace(id);
 
-            this.TraceId = traceId;
-            this.ParentId = parentId;
-            this.Id = id;
-            this.Kind = kind;
-            this.Name = name;
-            this.Timestamp = timestamp;
-            this.Duration = duration;
-            this.LocalEndpoint = localEndpoint;
-            this.RemoteEndpoint = remoteEndpoint;
-            this.Annotations = annotations;
-            this.Tags = tags;
-            this.Debug = debug;
-            this.Shared = shared;
+        this.TraceId = traceId;
+        this.ParentId = parentId;
+        this.Id = id;
+        this.Kind = kind;
+        this.Name = name;
+        this.Timestamp = timestamp;
+        this.Duration = duration;
+        this.LocalEndpoint = localEndpoint;
+        this.RemoteEndpoint = remoteEndpoint;
+        this.Annotations = annotations;
+        this.Tags = tags;
+        this.Debug = debug;
+        this.Shared = shared;
+    }
+
+    public string TraceId { get; }
+
+    public string ParentId { get; }
+
+    public string Id { get; }
+
+    public string Kind { get; }
+
+    public string Name { get; }
+
+    public long? Timestamp { get; }
+
+    public long? Duration { get; }
+
+    public ZipkinEndpoint LocalEndpoint { get; }
+
+    public ZipkinEndpoint RemoteEndpoint { get; }
+
+    public PooledList<ZipkinAnnotation> Annotations { get; }
+
+    public PooledList<KeyValuePair<string, object>> Tags { get; }
+
+    public bool? Debug { get; }
+
+    public bool? Shared { get; }
+
+    public void Return()
+    {
+        this.Annotations.Return();
+        this.Tags.Return();
+    }
+
+    public void Write(Utf8JsonWriter writer)
+    {
+        writer.WriteStartObject();
+
+        writer.WriteString(ZipkinSpanJsonHelper.TraceIdPropertyName, this.TraceId);
+
+        if (this.Name != null)
+        {
+            writer.WriteString(ZipkinSpanJsonHelper.NamePropertyName, this.Name);
         }
 
-        public string TraceId { get; }
-
-        public string ParentId { get; }
-
-        public string Id { get; }
-
-        public string Kind { get; }
-
-        public string Name { get; }
-
-        public long? Timestamp { get; }
-
-        public long? Duration { get; }
-
-        public ZipkinEndpoint LocalEndpoint { get; }
-
-        public ZipkinEndpoint RemoteEndpoint { get; }
-
-        public PooledList<ZipkinAnnotation> Annotations { get; }
-
-        public PooledList<KeyValuePair<string, object>> Tags { get; }
-
-        public bool? Debug { get; }
-
-        public bool? Shared { get; }
-
-        public void Return()
+        if (this.ParentId != null)
         {
-            this.Annotations.Return();
-            this.Tags.Return();
+            writer.WriteString(ZipkinSpanJsonHelper.ParentIdPropertyName, this.ParentId);
         }
 
-        public void Write(Utf8JsonWriter writer)
+        writer.WriteString(ZipkinSpanJsonHelper.IdPropertyName, this.Id);
+
+        if (this.Kind != null)
         {
-            writer.WriteStartObject();
+            writer.WriteString(ZipkinSpanJsonHelper.KindPropertyName, this.Kind);
+        }
 
-            writer.WriteString(ZipkinSpanJsonHelper.TraceIdPropertyName, this.TraceId);
+        if (this.Timestamp.HasValue)
+        {
+            writer.WriteNumber(ZipkinSpanJsonHelper.TimestampPropertyName, this.Timestamp.Value);
+        }
 
-            if (this.Name != null)
+        if (this.Duration.HasValue)
+        {
+            writer.WriteNumber(ZipkinSpanJsonHelper.DurationPropertyName, this.Duration.Value);
+        }
+
+        if (this.Debug.HasValue)
+        {
+            writer.WriteBoolean(ZipkinSpanJsonHelper.DebugPropertyName, this.Debug.Value);
+        }
+
+        if (this.Shared.HasValue)
+        {
+            writer.WriteBoolean(ZipkinSpanJsonHelper.SharedPropertyName, this.Shared.Value);
+        }
+
+        if (this.LocalEndpoint != null)
+        {
+            writer.WritePropertyName(ZipkinSpanJsonHelper.LocalEndpointPropertyName);
+            this.LocalEndpoint.Write(writer);
+        }
+
+        if (this.RemoteEndpoint != null)
+        {
+            writer.WritePropertyName(ZipkinSpanJsonHelper.RemoteEndpointPropertyName);
+            this.RemoteEndpoint.Write(writer);
+        }
+
+        if (!this.Annotations.IsEmpty)
+        {
+            writer.WritePropertyName(ZipkinSpanJsonHelper.AnnotationsPropertyName);
+            writer.WriteStartArray();
+
+            foreach (var annotation in this.Annotations)
             {
-                writer.WriteString(ZipkinSpanJsonHelper.NamePropertyName, this.Name);
-            }
-
-            if (this.ParentId != null)
-            {
-                writer.WriteString(ZipkinSpanJsonHelper.ParentIdPropertyName, this.ParentId);
-            }
-
-            writer.WriteString(ZipkinSpanJsonHelper.IdPropertyName, this.Id);
-
-            if (this.Kind != null)
-            {
-                writer.WriteString(ZipkinSpanJsonHelper.KindPropertyName, this.Kind);
-            }
-
-            if (this.Timestamp.HasValue)
-            {
-                writer.WriteNumber(ZipkinSpanJsonHelper.TimestampPropertyName, this.Timestamp.Value);
-            }
-
-            if (this.Duration.HasValue)
-            {
-                writer.WriteNumber(ZipkinSpanJsonHelper.DurationPropertyName, this.Duration.Value);
-            }
-
-            if (this.Debug.HasValue)
-            {
-                writer.WriteBoolean(ZipkinSpanJsonHelper.DebugPropertyName, this.Debug.Value);
-            }
-
-            if (this.Shared.HasValue)
-            {
-                writer.WriteBoolean(ZipkinSpanJsonHelper.SharedPropertyName, this.Shared.Value);
-            }
-
-            if (this.LocalEndpoint != null)
-            {
-                writer.WritePropertyName(ZipkinSpanJsonHelper.LocalEndpointPropertyName);
-                this.LocalEndpoint.Write(writer);
-            }
-
-            if (this.RemoteEndpoint != null)
-            {
-                writer.WritePropertyName(ZipkinSpanJsonHelper.RemoteEndpointPropertyName);
-                this.RemoteEndpoint.Write(writer);
-            }
-
-            if (!this.Annotations.IsEmpty)
-            {
-                writer.WritePropertyName(ZipkinSpanJsonHelper.AnnotationsPropertyName);
-                writer.WriteStartArray();
-
-                foreach (var annotation in this.Annotations)
-                {
-                    writer.WriteStartObject();
-
-                    writer.WriteNumber(ZipkinSpanJsonHelper.TimestampPropertyName, annotation.Timestamp);
-
-                    writer.WriteString(ZipkinSpanJsonHelper.ValuePropertyName, annotation.Value);
-
-                    writer.WriteEndObject();
-                }
-
-                writer.WriteEndArray();
-            }
-
-            if (!this.Tags.IsEmpty || this.LocalEndpoint.Tags != null)
-            {
-                writer.WritePropertyName(ZipkinSpanJsonHelper.TagsPropertyName);
                 writer.WriteStartObject();
 
-                // this will be used when we convert int, double, int[], double[] to string
-                var originalUICulture = Thread.CurrentThread.CurrentUICulture;
-                Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+                writer.WriteNumber(ZipkinSpanJsonHelper.TimestampPropertyName, annotation.Timestamp);
 
-                try
-                {
-                    foreach (var tag in this.LocalEndpoint.Tags ?? Enumerable.Empty<KeyValuePair<string, object>>())
-                    {
-                        if (ZipkinTagTransformer.Instance.TryTransformTag(tag, out var result))
-                        {
-                            writer.WriteString(tag.Key, result);
-                        }
-                    }
-
-                    foreach (var tag in this.Tags)
-                    {
-                        if (ZipkinTagTransformer.Instance.TryTransformTag(tag, out var result))
-                        {
-                            writer.WriteString(tag.Key, result);
-                        }
-                    }
-                }
-                finally
-                {
-                    Thread.CurrentThread.CurrentUICulture = originalUICulture;
-                }
+                writer.WriteString(ZipkinSpanJsonHelper.ValuePropertyName, annotation.Value);
 
                 writer.WriteEndObject();
             }
 
+            writer.WriteEndArray();
+        }
+
+        if (!this.Tags.IsEmpty || this.LocalEndpoint.Tags != null)
+        {
+            writer.WritePropertyName(ZipkinSpanJsonHelper.TagsPropertyName);
+            writer.WriteStartObject();
+
+            // this will be used when we convert int, double, int[], double[] to string
+            var originalUICulture = Thread.CurrentThread.CurrentUICulture;
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+
+            try
+            {
+                foreach (var tag in this.LocalEndpoint.Tags ?? Enumerable.Empty<KeyValuePair<string, object>>())
+                {
+                    if (ZipkinTagTransformer.Instance.TryTransformTag(tag, out var result))
+                    {
+                        writer.WriteString(tag.Key, result);
+                    }
+                }
+
+                foreach (var tag in this.Tags)
+                {
+                    if (ZipkinTagTransformer.Instance.TryTransformTag(tag, out var result))
+                    {
+                        writer.WriteString(tag.Key, result);
+                    }
+                }
+            }
+            finally
+            {
+                Thread.CurrentThread.CurrentUICulture = originalUICulture;
+            }
+
             writer.WriteEndObject();
         }
+
+        writer.WriteEndObject();
     }
 }
