108d27b33d9ea35263a69c6cb7ce8f01638af598
Yun-Ting <yunl@microsoft.com>
2023-07-28 15:16:07 -0700

    test

---
+++ build/mytest.ps1
@@ -0,0 +1,36 @@
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+$trimmingWarningCount = 0
+$aotWarningCount = 0
+foreach($line in Get-Content .\myoutput.log) 
+{
+    Write-Host $line
+    if ($line -like "*Trim analysis warning IL*") 
+    { 
+        $trimmingWarningCount += 1
+    } elseif ($line -like "*AOT analysis warning IL*") 
+    {
+        $aotWarningCount += 1
+    }
+}
+
+$expectedTrimmingWarningCount = 11
+$expectedaotWarningCount = 19
+
+Write-Host "trimmingWarningCount: ", $trimmingWarningCount
+Write-Host "aotWarningCount: ", $aotWarningCount
+
+$testPassed = 0
+if ($trimmingWarningCount -ne $expectedTrimmingWarningCount ) 
+{
+    $testPassed = 1
+    Write-Host "trimmingWarningCount: ", $trimmingWarningCount, "is not as expected. Expected count is:", $expectedTrimmingWarningCount
+}
+
+if ($aotWarningCount -ne $expectedaotWarningCount ) 
+{
+    $testPassed = 1
+    Write-Host "aotWarningCount: ", $aotWarningCount, "is not as expected. Expected count is:", $expectedaotWarningCount 
+}
+
+Exit $testPassed
