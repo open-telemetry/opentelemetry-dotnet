0ea0aef5b09707e9eaa724f6738de1314a33dc7d
Yun-Ting <yunl@microsoft.com>
2023-07-31 14:35:58 -0700

    fix

--- build/assert-publish-aot-testApp-warning-count.ps1
+++ build/assert-publish-aot-testApp-warning-count.ps1
@@ -1,4 +1,5 @@
-$publishOutput = dotnet publish ./test/OpenTelemetry.AotCompatibility.TestApp/OpenTelemetry.AotCompatibility.TestApp.csproj -nodeReuse:false /p:UseSharedCompilation=false
+$rootDirectory = Split-Path $PSScriptRoot -Parent
+$publishOutput = dotnet publish $rootDirectory\test\OpenTelemetry.AotCompatibility.TestApp\OpenTelemetry.AotCompatibility.TestApp.csproj -nodeReuse:false /p:UseSharedCompilation=false
 
 $actualWarningCount = 0
 
@@ -7,6 +8,7 @@ foreach ($line in $($publishOutput -split "`r`n"))
     if ($line -like "*analysis warning IL*") 
     {
         $actualWarningCount += 1
+    }
 }
 
 Write-Host "Actual warning count is:", $actualWarningCount
