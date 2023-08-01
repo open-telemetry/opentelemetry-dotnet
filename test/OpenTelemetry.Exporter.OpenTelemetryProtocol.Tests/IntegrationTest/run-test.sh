e9e8600a3d1d76024e5ebf9d4a1353da30ec4069
Alan West <3676547+alanwest@users.noreply.github.com>
2023-08-01 14:02:15 -0700

    Fix test log output

--- test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/IntegrationTest/run-test.sh
+++ test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/IntegrationTest/run-test.sh
@@ -4,4 +4,4 @@
 cp /cfg/otel-collector.crt /usr/local/share/ca-certificates/
 update-ca-certificates --verbose
 
-dotnet test OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests.dll --TestCaseFilter:CategoryName=CollectorIntegrationTests --logger:console;verbosity=detailed
+dotnet test OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests.dll --TestCaseFilter:CategoryName=CollectorIntegrationTests --logger "console;verbosity=detailed"
