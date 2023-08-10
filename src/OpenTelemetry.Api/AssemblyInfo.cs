65637b0ea3d85432503067c584fdf56399787a98
Mikel Blanchard <mblanchard@macrosssoftware.com>
2023-08-10 10:59:46 -0700

    Update StyleCop and fix warnings using "file" or "required" modifiers.

--- src/OpenTelemetry.Api/AssemblyInfo.cs
+++ src/OpenTelemetry.Api/AssemblyInfo.cs
@@ -34,7 +34,6 @@ using System.Runtime.CompilerServices;
 #endif
 
 #if SIGNED
-#pragma warning disable SA1400 // Access modifier should be declared
 file static class AssemblyInfo
 {
     public const string PublicKey = ", PublicKey=002400000480000094000000060200000024000052534131000400000100010051C1562A090FB0C9F391012A32198B5E5D9A60E9B80FA2D7B434C9E5CCB7259BD606E66F9660676AFC6692B8CDC6793D190904551D2103B7B22FA636DCBB8208839785BA402EA08FC00C8F1500CCEF28BBF599AA64FFB1E1D5DC1BF3420A3777BADFE697856E9D52070A50C3EA5821C80BEF17CA3ACFFA28F89DD413F096F898";
@@ -46,5 +45,4 @@ file static class AssemblyInfo
     public const string PublicKey = "";
     public const string MoqPublicKey = "";
 }
-#pragma warning restore SA1400 // Access modifier should be declared
 #endif
