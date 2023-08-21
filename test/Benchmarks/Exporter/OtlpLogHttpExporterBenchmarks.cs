f38a31a7ce09e9cf50654c7c9ff520f222a82aa0
Vishwesh Bankwar <vibankwa@microsoft.com>
2023-08-21 11:29:03 -0700

    add benchmarks

---
+++ test/Benchmarks/Exporter/OtlpLogHttpExporterBenchmarks.cs
@@ -0,0 +1,103 @@
+// <copyright file="OtlpHttpExporterBenchmarks.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+extern alias OpenTelemetryProtocol;
+using BenchmarkDotNet.Attributes;
+using Benchmarks.Helper;
+using OpenTelemetry;
+using OpenTelemetry.Internal;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Tests;
+using OpenTelemetryProtocol::OpenTelemetry.Exporter;
+using OpenTelemetryProtocol::OpenTelemetry.Exporter.OpenTelemetryProtocol.Implementation;
+using OpenTelemetryProtocol::OpenTelemetry.Exporter.OpenTelemetryProtocol.Implementation.ExportClient;
+
+namespace Benchmarks.Exporter;
+
+public class OtlpLogHttpExporterBenchmarks
+{
+    private readonly byte[] buffer = new byte[1024 * 1024];
+    private IDisposable server;
+    private string serverHost;
+    private int serverPort;
+    private OtlpLogExporter exporter;
+    private LogRecord logRecord;
+    private CircularBuffer<LogRecord> logRecordBatch;
+
+    [Params(1, 10, 100)]
+    public int NumberOfBatches { get; set; }
+
+    [Params(10000)]
+    public int NumberOfLogs { get; set; }
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        this.server = TestHttpServer.RunServer(
+            (ctx) =>
+            {
+                using (Stream receiveStream = ctx.Request.InputStream)
+                {
+                    while (true)
+                    {
+                        if (receiveStream.Read(this.buffer, 0, this.buffer.Length) == 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                ctx.Response.StatusCode = 200;
+                ctx.Response.OutputStream.Close();
+            },
+            out this.serverHost,
+            out this.serverPort);
+
+        var options = new OtlpExporterOptions
+        {
+            Endpoint = new Uri($"http://{this.serverHost}:{this.serverPort}"),
+        };
+        this.exporter = new OtlpLogExporter(
+            options,
+            new SdkLimitOptions(),
+            new OtlpHttpLogExportClient(options, options.HttpClientFactory()));
+
+        this.logRecord = LogRecordHelper.CreateTestLogRecord();
+        this.logRecordBatch = new CircularBuffer<LogRecord>(this.NumberOfLogs);
+    }
+
+    [GlobalCleanup]
+    public void GlobalCleanup()
+    {
+        this.exporter.Shutdown();
+        this.exporter.Dispose();
+        this.server.Dispose();
+    }
+
+    [Benchmark]
+    public void OtlpExporter_Batching()
+    {
+        for (int i = 0; i < this.NumberOfBatches; i++)
+        {
+            for (int c = 0; c < this.NumberOfLogs; c++)
+            {
+                this.logRecordBatch.Add(this.logRecord);
+            }
+
+            this.exporter.Export(new Batch<LogRecord>(this.logRecordBatch, this.NumberOfLogs));
+        }
+    }
+}
