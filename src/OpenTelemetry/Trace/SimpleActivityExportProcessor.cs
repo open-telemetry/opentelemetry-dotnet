ecc511c2c57d5c74235442a8f6d997c40dff224d
Mikel Blanchard <mblanchard@macrosssoftware.com>
2023-09-07 22:38:38 -0700

    Add missing XML comments and nullable decorations for SDK public APIs.

--- src/OpenTelemetry/Trace/SimpleActivityExportProcessor.cs
+++ src/OpenTelemetry/Trace/SimpleActivityExportProcessor.cs
@@ -20,8 +20,15 @@ using System.Diagnostics;
 
 namespace OpenTelemetry;
 
+/// <summary>
+/// Implements processor that exports <see cref="Activity"/> objects at each OnEnd call.
+/// </summary>
 public class SimpleActivityExportProcessor : SimpleExportProcessor<Activity>
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SimpleActivityExportProcessor"/> class.
+    /// </summary>
+    /// <param name="exporter"><inheritdoc cref="SimpleExportProcessor{T}.SimpleExportProcessor" path="/param[@name='exporter']"/>.</param>
     public SimpleActivityExportProcessor(BaseExporter<Activity> exporter)
         : base(exporter)
     {
