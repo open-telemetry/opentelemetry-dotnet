0389e450ff24953eb9a30bd386e8def93f36a2b7
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2023-09-15 10:38:58 +0200

    use dotnet-coverage

--- build/process-codecoverage.ps1
+++ build/process-codecoverage.ps1
@@ -1,16 +1,8 @@
-$rootDirectory = Split-Path $PSScriptRoot -Parent
-[xml]$commonProps = Get-Content -Path $rootDirectory\Directory.Packages.props
-
-$packages = $commonProps.Project.ItemGroup.PackageVersion
-$microsoftCodeCoveragePkgVer = [string]($packages | Where-Object {$_.Include -eq "Microsoft.CodeCoverage"}).Version # This is collected in the format: "[17.4.1]"
-$microsoftCodeCoveragePkgVer = $microsoftCodeCoveragePkgVer.Trim();
-$microsoftCodeCoveragePkgVer = $microsoftCodeCoveragePkgVer.SubString(1, $microsoftCodeCoveragePkgVer.Length - 2) # Removing square brackets
-
 $files = Get-ChildItem "TestResults" -Filter "*.coverage" -Recurse
 Write-Host $env:USERPROFILE
 foreach ($file in $files)
 {
-    $command = $env:USERPROFILE+ '\.nuget\packages\microsoft.codecoverage\' + $microsoftCodeCoveragePkgVer + '\build\netstandard2.0\CodeCoverage\CodeCoverage.exe analyze /output:' + $file.DirectoryName + '\' + $file.Name + '.xml '+ $file.FullName
+    $command = 'dotnet coverage merge --output-format xml --output ' + $file.DirectoryName + '\' + $file.Name + '.xml '+ $file.FullName
     Write-Host $command
     Invoke-Expression $command
 }
