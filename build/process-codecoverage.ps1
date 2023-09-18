cdd8ffda88e90d332f452d6d807ed1d63b0d8c70
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2023-09-18 07:47:58 +0200

    PR feedback - replace reportgenerator by dotnet-coverage

--- build/process-codecoverage.ps1
+++
@@ -1,8 +0,0 @@
-$files = Get-ChildItem "TestResults" -Filter "*.coverage" -Recurse
-Write-Host $env:USERPROFILE
-foreach ($file in $files)
-{
-    $command = 'dotnet coverage merge --output-format xml --output ' + $file.DirectoryName + '\' + $file.Name + '.xml '+ $file.FullName
-    Write-Host $command
-    Invoke-Expression $command
-}
