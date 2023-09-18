01840c0f6ee4b4438a93bb4989682b322028c452
Matthew Hensley <matthew.hensley@grafana.com>
2023-09-18 15:15:48 -0400

    using directive is unnecessary

--- src/OpenTelemetry.Instrumentation.Http/MeterProviderBuilderExtensions.cs
+++ src/OpenTelemetry.Instrumentation.Http/MeterProviderBuilderExtensions.cs
@@ -14,8 +14,6 @@
 // limitations under the License.
 // </copyright>
 
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using OpenTelemetry.Instrumentation.Http;
 using OpenTelemetry.Instrumentation.Http.Implementation;
 using OpenTelemetry.Internal;
