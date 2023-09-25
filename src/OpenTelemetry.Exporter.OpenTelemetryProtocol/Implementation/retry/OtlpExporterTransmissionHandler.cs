04a69b4c2308825a49acc7af920f6c4011d5e653
Vishwesh Bankwar <vibankwa@microsoft.com>
2023-09-25 11:03:03 -0700

    remarks

--- src/OpenTelemetry.Exporter.OpenTelemetryProtocol/Implementation/retry/OtlpExporterTransmissionHandler.cs
+++ src/OpenTelemetry.Exporter.OpenTelemetryProtocol/Implementation/retry/OtlpExporterTransmissionHandler.cs
@@ -26,6 +26,11 @@ internal class OtlpExporterTransmissionHandler<T>
 
     public OtlpExporterOptions Options { get; internal set; }
 
+    /// <summary>
+    /// Sends export request to the server.
+    /// </summary>
+    /// <param name="request">The request to send to the server.</param>
+    /// <returns>True if the request is sent successfully or else false.</returns>
     public virtual bool SubmitRequest(T request)
     {
         try
@@ -39,6 +44,12 @@ internal class OtlpExporterTransmissionHandler<T>
         }
     }
 
+    /// <summary>
+    /// Retries sending request to the server.
+    /// </summary>
+    /// <param name="request">The request to send to the server.</param>
+    /// <param name="exception">Exception encountered when trying to send request.</param>
+    /// <returns>True if the request is sent successfully or else false.</returns>
     protected virtual bool RetryRequest(T request, out Exception exception)
     {
         try
@@ -55,11 +66,22 @@ internal class OtlpExporterTransmissionHandler<T>
         }
     }
 
+    /// <summary>
+    /// Callback to call when encountered exception while sending request to server.
+    /// </summary>
+    /// <param name="request">The request that was attempted to send to the server.</param>
+    /// <param name="exception">Exception that was encountered during request processing.</param>
+    /// <returns>True or False, based on the implementation of handling errors.</returns>
     protected virtual bool OnSubmitRequestExceptionThrown(T request, Exception exception)
     {
         return this.OnHandleDroppedRequest(request);
     }
 
+    /// <summary>
+    /// Action to take when dropping request.
+    /// </summary>
+    /// <param name="request">The request that was attempted to send to the server.</param>
+    /// <returns>True or False, based on the implementation.</returns>
     protected virtual bool OnHandleDroppedRequest(T request)
     {
         return false;
