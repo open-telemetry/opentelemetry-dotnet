b776160c7c0a33726a3a234049df7a19c1fb59e0
Yun-Ting <yunl@microsoft.com>
2023-09-27 14:58:23 -0700

    test

--- build/concurrency-tests-with-Coyote.ps1
+++ build/concurrency-tests-with-Coyote.ps1
@@ -1,5 +1,24 @@
 Write-Host "Test running coyote"
-Write-Host "1. Install Coyote CLI"
-Write-Host "2. run Coyote rewrite"
-Write-Host "3. Execute re-written binary"
-Write-Host "4. Verify test pass"
+
+$rootDirectory = Split-Path $PSScriptRoot -Parent
+
+Write-Host "Install Coyote CLI"
+dotnet tool install --global Microsoft.Coyote.CLI
+
+Write-Host "Build OpenTelemetry.Tests proj"
+dotnet build $rootDirectory/test/OpenTelemetry.Tests.csproj
+
+Write-Host "Run Coyote rewrite"
+coyote rewrite $rootDirectory/test/OpenTelemetry.Tests/coyote.json
+
+Write-Host "Execute re-written binary"
+$Output = dotnet test $rootDirectory/test/OpenTelemetry.Tests/bin/Debug/net7.0/OpenTelemetry.Tests.dll --filter MultithreadedLongHistogramTest_Coyote
+
+Write-Host "Verify test pass"
+foreach ($line in $($Output -split "`r`n"))
+{
+    if ($line -contains "*pass*")
+    {
+        Write-Host "PASSED!"
+    }
+}
