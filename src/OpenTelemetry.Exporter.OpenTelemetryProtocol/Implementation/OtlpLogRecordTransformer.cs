d73f493af05be9fa83a485ee3838f43270569f15
Vishwesh Bankwar <vibankwa@microsoft.com>
2023-10-02 23:14:05 -0700

    file header

--- src/OpenTelemetry.Exporter.OpenTelemetryProtocol/Implementation/OtlpLogRecordTransformer.cs
+++ src/OpenTelemetry.Exporter.OpenTelemetryProtocol/Implementation/OtlpLogRecordTransformer.cs
@@ -1,4 +1,4 @@
-// <copyright file="OtlpLogRecordProcessor.cs" company="OpenTelemetry Authors">
+// <copyright file="OtlpLogRecordTransformer.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
