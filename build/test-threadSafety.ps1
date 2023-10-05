5b96500b29b8e1a3a063aaacaeef054a3f2c6936
Yun-Ting <yunl@microsoft.com>
2023-10-05 10:53:24 -0700

    cleanups and net8

--- build/test-threadSafety.ps1
+++ build/test-threadSafety.ps1
@@ -12,7 +12,7 @@ Write-Host "Run Coyote rewrite"
 coyote rewrite $rootDirectory/test/OpenTelemetry.Tests/coyote.json
 
 Write-Host "Execute re-written binary"
-$Output = dotnet test $rootDirectory/test/OpenTelemetry.Tests/bin/Debug/net7.0/OpenTelemetry.Tests.dll --filter MultithreadedLongHistogramTest_Coyote
+$Output = dotnet test $rootDirectory/test/OpenTelemetry.Tests/bin/Debug/net8.0/OpenTelemetry.Tests.dll --filter MultithreadedLongHistogramTest_Coyote
 
 Write-Host "Verify test pass"
 foreach ($line in $($Output -split "`r`n"))
