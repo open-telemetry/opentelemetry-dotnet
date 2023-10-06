f47ed19e5299db3a34c59921533497d28b69115c
Yun-Ting <yunl@microsoft.com>
2023-10-06 15:26:53 -0700

    cleanups

--- build/test-threadSafety.ps1
+++ build/test-threadSafety.ps1
@@ -1,9 +1,9 @@
 param(
   [Parameter(Mandatory=$true)][string]$coyoteVersion="",
-  [Parameter(Mandatory=$true)][string]$targetFramework
+  [Parameter(Mandatory=$true)][string]$targetFramework,
+  [Parameter(Mandatory=$true)][string]$methodNames,
 )
 
-Write-Host "Test running Coyote."
 $rootDirectory = Split-Path $PSScriptRoot -Parent
 
 Write-Host "Install Coyote CLI."
@@ -14,9 +14,7 @@ dotnet build $rootDirectory\test\OpenTelemetry.Tests\OpenTelemetry.Tests.csproj
 
 $artifactsPath = Join-Path $rootDirectory "test\OpenTelemetry.Tests\bin\Debug\$targetFramework"
 
-Write-Host "ArtifactsPath is:", $artifactsPath
-
-Write-Host "Generating Coyote rewriting options JSON file."
+Write-Host "Generate Coyote rewriting options JSON file."
 $assemblies = Get-ChildItem $artifactsPath -Filter OpenTelemetry*.dll | ForEach-Object {$_.Name}
 
 $RewriteOptionsJson = @{}
@@ -24,24 +22,14 @@ $RewriteOptionsJson = @{}
 [void]$RewriteOptionsJson.Add("Assemblies", $assemblies)
 $RewriteOptionsJson | ConvertTo-Json -Compress | Set-Content "$rootDirectory\test\OpenTelemetry.Tests\rewrite.coyote.json"
 
-Write-Host "Rewritten Json file:"
-$obj = Get-Content -Path $rootDirectory\test\OpenTelemetry.Tests\rewrite.coyote.json -Raw | ConvertFrom-Json
-Write-Host $obj
-
 Write-Host "Run Coyote rewrite."
 coyote rewrite $rootDirectory\test\OpenTelemetry.Tests\rewrite.coyote.json
-Write-Host "Done re-written."
 
 Write-Host "Execute re-written binary."
-# test name can be passed in
-$Output = dotnet test $artifactsPath\OpenTelemetry.Tests.dll --filter MultithreadedLongHistogramTest_Coyote
+$Output = dotnet test $artifactsPath\OpenTelemetry.Tests.dll --filter $methodNames
 
-Write-Host "Verify test pass."
+Write-Host "Coyote test output:"
 foreach ($line in $($Output -split "`r`n"))
 {
     Write-Host $line
-    if ($line -contains "*pass*")
-    {
-        Write-Host "PASSED!"
-    }
 }
