1d98cb1c93e87802de228720f92b3df26528c070
Reiley Yang <reyang@microsoft.com>
2023-10-06 15:52:20 -0700

    fix build

--- docs/logs/getting-started-console/Program.cs
+++ docs/logs/getting-started-console/Program.cs
@@ -48,7 +48,6 @@ public static partial class ApplicationLogs
     [LoggerMessage(LogLevel.Critical, "A `{productType}` recall notice was published for `{brandName} {productDescription}` produced by `{companyName}` ({recallReasonDescription}).")]
     public static partial void FoodRecallNotice(
         this ILogger logger,
-        LogLevel logLevel,
         string brandName,
         string productDescription,
         string productType,
