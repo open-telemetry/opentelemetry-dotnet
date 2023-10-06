c48df7fc35e563ee84e77f7dff6a60b0e98fd0e8
Yun-Ting <yunl@microsoft.com>
2023-10-06 15:03:48 -0700

    update shell

--- build/test-threadSafety.ps1
+++ build/test-threadSafety.ps1
@@ -10,9 +10,11 @@ Write-Host "Install Coyote CLI."
 dotnet tool install --global Microsoft.Coyote.CLI
 
 Write-Host "Build OpenTelemetry.Tests project."
-dotnet build $rootDirectory/test/OpenTelemetry.Tests/OpenTelemetry.Tests.csproj
+dotnet build $rootDirectory\test\OpenTelemetry.Tests\OpenTelemetry.Tests.csproj
 
-$artifactsPath = Join-Path $rootDirectory "test/OpenTelemetry.Tests/bin/Debug/$targetNetFramework"
+$artifactsPath = Join-Path $rootDirectory "test\OpenTelemetry.Tests\bin\Debug\$targetFramework"
+
+Write-Host "ArtifactsPath is:", $artifactsPath
 
 Write-Host "Generating Coyote rewriting options JSON file."
 $assemblies = Get-ChildItem $artifactsPath -Filter OpenTelemetry*.dll | ForEach-Object {$_.Name}
@@ -20,17 +22,19 @@ $assemblies = Get-ChildItem $artifactsPath -Filter OpenTelemetry*.dll | ForEach-
 $RewriteOptionsJson = @{}
 [void]$RewriteOptionsJson.Add("AssembliesPath", $artifactsPath)
 [void]$RewriteOptionsJson.Add("Assemblies", $assemblies)
-$RewriteOptionsJson | ConvertTo-Json -Compress | Set-Content "$rootDirectory/test/OpenTelemetry.Tests/rewrite.coyote.json"
+$RewriteOptionsJson | ConvertTo-Json -Compress | Set-Content "$rootDirectory\test\OpenTelemetry.Tests\rewrite.coyote.json"
 
-Write-Host "Rewritten Json file:", $RewriteOptionsJson
+Write-Host "Rewritten Json file:"
+$obj = Get-Content -Path $rootDirectory\test\OpenTelemetry.Tests\rewrite.coyote.json -Raw | ConvertFrom-Json
+Write-Host $obj
 
 Write-Host "Run Coyote rewrite."
-coyote rewrite $rootDirectory/test/OpenTelemetry.Tests/rewrite.coyote.json
+coyote rewrite $rootDirectory\test\OpenTelemetry.Tests\rewrite.coyote.json
 Write-Host "Done re-written."
 
 Write-Host "Execute re-written binary."
 # test name can be passed in
-$Output = dotnet test $artifactsPath/OpenTelemetry.Tests.dll --filter MultithreadedLongHistogramTest_Coyote
+$Output = dotnet test $artifactsPath\OpenTelemetry.Tests.dll --filter MultithreadedLongHistogramTest_Coyote
 
 Write-Host "Verify test pass."
 foreach ($line in $($Output -split "`r`n"))
