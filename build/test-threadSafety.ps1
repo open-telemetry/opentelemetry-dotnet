e203f2fc43da1acb595f26ab3bfa6e6d4d2913cf
Mikel Blanchard <mblanchard@macrosssoftware.com>
2023-10-20 15:46:45 -0700

    Tweak.

--- build/test-threadSafety.ps1
+++ build/test-threadSafety.ps1
@@ -14,9 +14,9 @@ Write-Host "Install Coyote CLI."
 dotnet tool install --global Microsoft.Coyote.CLI
 
 Write-Host "Build $testProjectName project."
-dotnet build "$rootDirectory\test\$testProjectName\$testProjectName.csproj" --configuration $configuration
+dotnet build "$rootDirectory/test/$testProjectName/$testProjectName.csproj" --configuration $configuration
 
-$artifactsPath = Join-Path $rootDirectory "test\$testProjectName\bin\$configuration\$targetFramework"
+$artifactsPath = Join-Path $rootDirectory "test/$testProjectName/bin/$configuration/$targetFramework"
 
 Write-Host "Generate Coyote rewriting options JSON file."
 $assemblies = Get-ChildItem $artifactsPath -Filter OpenTelemetry*.dll | ForEach-Object {$_.Name}
@@ -24,13 +24,13 @@ $assemblies = Get-ChildItem $artifactsPath -Filter OpenTelemetry*.dll | ForEach-
 $RewriteOptionsJson = @{}
 [void]$RewriteOptionsJson.Add("AssembliesPath", $artifactsPath)
 [void]$RewriteOptionsJson.Add("Assemblies", $assemblies)
-$RewriteOptionsJson | ConvertTo-Json -Compress | Set-Content -Path "$rootDirectory\test\$testProjectName\rewrite.coyote.json" -Verbose
+$RewriteOptionsJson | ConvertTo-Json -Compress | Set-Content -Path "$rootDirectory/test/$testProjectName/rewrite.coyote.json"
 
 Write-Host "Run Coyote rewrite."
 coyote rewrite "$rootDirectory/test/$testProjectName/rewrite.coyote.json"
 
 Write-Host "Execute re-written binary."
-$Output = dotnet test "$artifactsPath\$testProjectName.dll" --framework $targetFramework --filter CategoryName=$categoryName
+$Output = dotnet test "$artifactsPath/$testProjectName.dll" --framework $targetFramework --filter CategoryName=$categoryName
 
 Write-Host "Coyote test output:"
 foreach ($line in $($Output -split "`r`n"))
