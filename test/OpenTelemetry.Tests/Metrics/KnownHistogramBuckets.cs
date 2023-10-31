a1670f0462cabc21a7200a3b9244f870fc2a7bbe
James Newton-King <james@newtonking.com>
2023-10-31 09:28:32 +0800

    Fix build

--- test/OpenTelemetry.Tests/Metrics/KnownHistogramBuckets.cs
+++ test/OpenTelemetry.Tests/Metrics/KnownHistogramBuckets.cs
@@ -1,4 +1,4 @@
-// <copyright file="AggregatorTestsBase.cs" company="OpenTelemetry Authors">
+// <copyright file="KnownHistogramBuckets.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
