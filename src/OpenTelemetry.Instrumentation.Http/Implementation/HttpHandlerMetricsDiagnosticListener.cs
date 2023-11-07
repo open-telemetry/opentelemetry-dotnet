80a834b7ea924c253bd56dbcb92f74d91b1072c5
Vishwesh Bankwar <vishweshbankwar@users.noreply.github.com>
2023-11-06 16:06:36 -0800

    Update src/OpenTelemetry.Instrumentation.Http/Implementation/HttpHandlerMetricsDiagnosticListener.cs
    Co-authored-by: Timothy Mothra <tilee@microsoft.com>

--- src/OpenTelemetry.Instrumentation.Http/Implementation/HttpHandlerMetricsDiagnosticListener.cs
+++ src/OpenTelemetry.Instrumentation.Http/Implementation/HttpHandlerMetricsDiagnosticListener.cs
@@ -183,7 +183,7 @@ internal sealed class HttpHandlerMetricsDiagnosticListener : ListenerHandler
         // The AOT-annotation DynamicallyAccessedMembers in System.Net.Http library ensures that top-level properties on the payload object are always preserved.
         // see https://github.com/dotnet/runtime/blob/f9246538e3d49b90b0e9128d7b1defef57cd6911/src/libraries/System.Net.Http/src/System/Net/Http/DiagnosticsHandler.cs#L325
 #if NET6_0_OR_GREATER
-        [UnconditionalSuppressMessage("Trimming", "IL2026", Justification = "The eSystem.Net.Http library guarantees that top-level properties are preserved")]
+        [UnconditionalSuppressMessage("Trimming", "IL2026", Justification = "The System.Net.Http library guarantees that top-level properties are preserved")]
 #endif
         static bool TryFetchResponse(object payload, out HttpResponseMessage response) =>
             StopResponseFetcher.TryFetch(payload, out response) && response != null;
