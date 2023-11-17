5b6281fb4bf4bb8d6f21a3daf239acc0ec30f88e
Mikel Blanchard <mblanchard@macrosssoftware.com>
2023-11-16 21:02:14 -0800

    Rename landing doc to be plural.

--- src/Shared/DiagnosticDefinitions.cs
+++ src/Shared/DiagnosticDefinitions.cs
@@ -20,7 +20,7 @@ namespace OpenTelemetry.Internal;
 
 internal static class DiagnosticDefinitions
 {
-    public const string ExperimentalApiUrlFormat = "https://github.com/open-telemetry/opentelemetry-dotnet/tree/main/docs/diagnostics/EXPERIMENTAL_API.md#{0}";
+    public const string ExperimentalApiUrlFormat = "https://github.com/open-telemetry/opentelemetry-dotnet/tree/main/docs/diagnostics/EXPERIMENTAL_APIS.md#{0}";
 
     public const string LoggerProviderExperimentalApi = "OTEL1000";
     public const string LogBridgeApiExperimentalApi = "OTEL1001";
