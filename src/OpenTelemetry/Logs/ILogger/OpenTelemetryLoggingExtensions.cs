8a467209dd30ba0317f9031ed4efa20de44554d3
Mikel Blanchard <mblanchard@macrosssoftware.com>
2023-11-29 14:33:37 -0800

    Fix ordering of configuration delegate passed to logging extensions.

--- src/OpenTelemetry/Logs/ILogger/OpenTelemetryLoggingExtensions.cs
+++ src/OpenTelemetry/Logs/ILogger/OpenTelemetryLoggingExtensions.cs
@@ -173,18 +173,18 @@ public static class OpenTelemetryLoggingExtensions
 
         var services = builder.Services;
 
-        if (configureOptions != null)
-        {
-            // TODO: Move this below the RegisterLoggerProviderOptions call so
-            // that user-supplied delegate fires AFTER the options are bound to
-            // Logging:OpenTelemetry configuration.
-            services.Configure(configureOptions);
-        }
-
         // Note: This will bind logger options element (eg "Logging:OpenTelemetry") to OpenTelemetryLoggerOptions
         RegisterLoggerProviderOptions(services);
 
         services.AddOpenTelemetrySharedProviderBuilderServices();
+
+        if (configureOptions != null)
+        {
+            // Note: Order is important here so that user-supplied delegate
+            // fires AFTER the options are bound to Logging:OpenTelemetry
+            // configuration.
+            services.Configure(configureOptions);
+        }
 
         var loggingBuilder = new LoggerProviderBuilderBase(services).ConfigureBuilder(
             (sp, logging) =>
