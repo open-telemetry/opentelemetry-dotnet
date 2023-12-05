63b8c02b753877ea7834cd4572bbaad748ae4143
Vishwesh Bankwar <vibankwa@microsoft.com>
2023-12-05 11:31:26 -0800

    test app

---
+++ TestAspNetCoreInstrumentationPerf/InstrumentationOptions.cs
@@ -0,0 +1,8 @@
+namespace TestAspNetCoreInstrumentationPerf;
+
+public class InstrumentationOptions
+{
+    public bool EnableTraceInstrumentation { get; set; }
+
+    public bool EnableMetricInstrumentation { get; set; }
+}
---
+++ TestAspNetCoreInstrumentationPerf/Program.cs
@@ -0,0 +1,42 @@
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using TestAspNetCoreInstrumentationPerf;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var instrumentationOptions = new InstrumentationOptions();
+builder.Configuration.GetSection("InstrumentationOptions").Bind(instrumentationOptions);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+if (instrumentationOptions.EnableMetricInstrumentation)
+{
+    builder.Services.AddOpenTelemetry().WithMetrics(builder => builder.AddAspNetCoreInstrumentation().AddReader(new PeriodicExportingMetricReader(new MyExporter<Metric>("TestExporter"))));
+}
+
+if (instrumentationOptions.EnableTraceInstrumentation)
+{
+    builder.Services.AddOpenTelemetry().WithTracing(builder => builder.AddAspNetCoreInstrumentation());
+}
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
