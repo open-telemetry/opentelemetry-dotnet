a963ec809aac676ce9418eb008c9a4ef592eca26
Nils Gruson <ngruson@hotmail.com>
2023-12-06 03:55:47 +0100

    Removed Moq dependency from Instrumentation.AspNetCore.Tests (#5123)

--- test/OpenTelemetry.Instrumentation.AspNetCore.Tests/BasicTests.cs
+++ test/OpenTelemetry.Instrumentation.AspNetCore.Tests/BasicTests.cs
@@ -24,7 +24,6 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Moq;
 using OpenTelemetry.Context.Propagation;
 using OpenTelemetry.Instrumentation.AspNetCore.Implementation;
 using OpenTelemetry.Tests;
@@ -205,14 +204,11 @@ public sealed class BasicTests
             var expectedTraceId = ActivityTraceId.CreateRandom();
             var expectedSpanId = ActivitySpanId.CreateRandom();
 
-            var propagator = new Mock<TextMapPropagator>();
-            propagator.Setup(m => m.Extract(It.IsAny<PropagationContext>(), It.IsAny<HttpRequest>(), It.IsAny<Func<HttpRequest, string, IEnumerable<string>>>())).Returns(
-                new PropagationContext(
-                    new ActivityContext(
-                        expectedTraceId,
-                        expectedSpanId,
-                        ActivityTraceFlags.Recorded),
-                    default));
+            var propagator = new CustomTextMapPropagator
+            {
+                TraceId = expectedTraceId,
+                SpanId = expectedSpanId,
+            };
 
             // Arrange
             using (var testFactory = this.factory
@@ -220,7 +216,7 @@ public sealed class BasicTests
                     {
                         builder.ConfigureTestServices(services =>
                         {
-                            Sdk.SetDefaultTextMapPropagator(propagator.Object);
+                            Sdk.SetDefaultTextMapPropagator(propagator);
                             var tracerProviderBuilder = Sdk.CreateTracerProviderBuilder();
 
                             if (addSampler)
