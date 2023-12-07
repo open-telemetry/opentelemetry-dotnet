54b5bdc9f1370e7c61962275cb5a00047e2399eb
Mikel Blanchard <mblanchard@macrosssoftware.com>
2023-12-07 11:07:47 -0800

    Code review.

--- src/Shared/DiagnosticDefinitions.cs
+++ src/Shared/DiagnosticDefinitions.cs
@@ -7,7 +7,7 @@ namespace OpenTelemetry.Internal;
 
 internal static class DiagnosticDefinitions
 {
-    public const string ExperimentalApiUrlFormat = "https://github.com/open-telemetry/opentelemetry-dotnet/tree/main/docs/diagnostics/EXPERIMENTAL_APIS.md#{0}";
+    public const string ExperimentalApiUrlFormat = "https://github.com/open-telemetry/opentelemetry-dotnet/tree/main/docs/diagnostics/experimental-apis/README.md#{0}";
 
     public const string LoggerProviderExperimentalApi = "OTEL1000";
     public const string LogBridgeApiExperimentalApi = "OTEL1001";
