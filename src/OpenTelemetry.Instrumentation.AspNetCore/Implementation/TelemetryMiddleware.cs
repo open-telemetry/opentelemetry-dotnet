b25f24f6c3a8217e9d2dda38183283e6c7b4d5c7
Vishwesh Bankwar <vibankwa@microsoft.com>
2023-12-13 09:00:15 -0800

    middleware

--- src/OpenTelemetry.Instrumentation.AspNetCore/Implementation/TelemetryMiddleware.cs
+++ src/OpenTelemetry.Instrumentation.AspNetCore/Implementation/TelemetryMiddleware.cs
@@ -1,18 +1,5 @@
-// <copyright file="TelemetryMiddleware.cs" company="OpenTelemetry Authors">
 // Copyright The OpenTelemetry Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// </copyright>
+// SPDX-License-Identifier: Apache-2.0
 
 using Microsoft.AspNetCore.Http;
 
@@ -27,16 +14,16 @@ namespace OpenTelemetry.Instrumentation.AspNetCore.Implementation;
 /// <param name="options">trace options.</param>
 public class TelemetryMiddleware(AspNetCoreTraceInstrumentationOptions options) : IMiddleware
 {
-    internal readonly HttpInListener InListener = new HttpInListener(options);
+    internal readonly HttpInListener HttpInListener = new HttpInListener(options);
 
     /// <inheritdoc/>
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
         try
         {
-            this.InListener.OnEventWritten("Microsoft.AspNetCore.Hosting.HttpRequestIn.Start", context);
+            this.HttpInListener.OnEventWritten("Microsoft.AspNetCore.Hosting.HttpRequestIn.Start", context);
             await next(context).ConfigureAwait(false);
-            this.InListener.OnEventWritten("Microsoft.AspNetCore.Hosting.HttpRequestIn.Stop", context);
+            this.HttpInListener.OnEventWritten("Microsoft.AspNetCore.Hosting.HttpRequestIn.Stop", context);
         }
         catch
         {
