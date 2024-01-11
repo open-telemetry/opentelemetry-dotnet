39f648a0394756a9e3903803d21fc19cf60121ef
Reiley Yang <reyang@microsoft.com>
2024-01-11 13:03:03 -0800

    Complex object logging example

---
+++ docs/logs/complex-objects/Program.cs
@@ -0,0 +1,38 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
+
+var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddOpenTelemetry(logging =>
+    {
+        logging.AddConsoleExporter();
+    });
+});
+
+var logger = loggerFactory.CreateLogger<Program>();
+
+var foodRecallNotice = new FoodRecallNotice
+{
+    BrandName = "Contoso",
+    ProductDescription = "Salads",
+    ProductType = "Food & Beverages",
+    RecallReasonDescription = "due to a possible health risk from Listeria monocytogenes",
+    CompanyName = "Contoso Fresh Vegetables, Inc.",
+};
+
+logger.FoodRecallNotice(foodRecallNotice);
+
+// Dispose logger factory before the application ends.
+// This will flush the remaining logs and shutdown the logging pipeline.
+loggerFactory.Dispose();
+
+public static partial class ApplicationLogs
+{
+    [LoggerMessage(LogLevel.Critical)]
+    public static partial void FoodRecallNotice(
+        this ILogger logger,
+        [LogProperties] FoodRecallNotice foodRecallNotice);
+}
