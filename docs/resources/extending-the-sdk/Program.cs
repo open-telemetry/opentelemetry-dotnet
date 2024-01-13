22b8231b350dc4067eff695cd35318082857ed32
Luiz Lelis <luizhlelis@gmail.com>
2024-01-13 16:18:16 -0300

    chore: formating code

--- docs/resources/extending-the-sdk/Program.cs
+++ docs/resources/extending-the-sdk/Program.cs
@@ -33,9 +33,8 @@ public class Program
         {
             builder.AddOpenTelemetry(options =>
             {
-                options.SetResourceBuilder(ResourceBuilder
-                    .CreateDefault().AddDetector(
-                        new MyResourceDetector()));
+                options.SetResourceBuilder(ResourceBuilder.CreateDefault().AddDetector(
+                    new MyResourceDetector()));
             });
         });
 
