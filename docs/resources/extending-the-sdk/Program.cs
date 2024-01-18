89e538293bcc3605ae256bf39b0a1c0b1b2e9ffe
Luiz Lelis <luizhlelis@gmail.com>
2024-01-17 22:29:33 -0300

    docs: `resource-detector` should not be tied to tracing (#5188)
    Co-authored-by: Mikel Blanchard <mblanchard@macrosssoftware.com>
    Co-authored-by: Alan West <3676547+alanwest@users.noreply.github.com>

---
+++ docs/resources/extending-the-sdk/Program.cs
@@ -0,0 +1,61 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace ExtendingTheSdk;
+
+public class Program
+{
+    private static readonly ActivitySource DemoSource = new("OTel.Demo");
+    private static readonly Meter MeterDemoSource = new("OTel.Demo");
+
+    public static void Main()
+    {
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddSource("OTel.Demo")
+            .SetResourceBuilder(ResourceBuilder.CreateEmpty().AddDetector(
+                new MyResourceDetector()))
+            .Build();
+
+        using var meterProvider = Sdk.CreateMeterProviderBuilder()
+            .SetResourceBuilder(ResourceBuilder.CreateEmpty().AddDetector(
+                new MyResourceDetector()))
+            .Build();
+
+        using var loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder.AddOpenTelemetry(options =>
+            {
+                options.SetResourceBuilder(ResourceBuilder.CreateDefault().AddDetector(
+                    new MyResourceDetector()));
+            });
+        });
+
+        using (var foo = DemoSource.StartActivity("Foo"))
+        {
+            using (var bar = DemoSource.StartActivity("Bar"))
+            {
+                using (var baz = DemoSource.StartActivity("Baz"))
+                {
+                }
+            }
+        }
+
+        var counter = MeterDemoSource.CreateCounter<long>("counter");
+        for (var i = 0; i < 20000; i++)
+        {
+            counter.Add(1, new KeyValuePair<string, object?>("tag1", "value1"), new KeyValuePair<string, object?>("tag2", "value2"));
+        }
+
+        var logger = loggerFactory.CreateLogger("OTel.Demo");
+        logger
+            .LogInformation("Hello from {Name} {Price}", "tomato", 2.99);
+    }
+}
