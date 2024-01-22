22cdf298342de6636a57990d79bdae4b9d3f6d53
Reiley Yang <reyang@microsoft.com>
2024-01-22 12:14:16 -0800

    update test result

--- test/Benchmarks/Metrics/MetricsBenchmarks.cs
+++ test/Benchmarks/Metrics/MetricsBenchmarks.cs
@@ -9,27 +9,49 @@ using OpenTelemetry.Metrics;
 using OpenTelemetry.Tests;
 
 /*
-BenchmarkDotNet v0.13.10, Windows 11 (10.0.23424.1000)
-Intel Core i7-9700 CPU 3.00GHz, 1 CPU, 8 logical and 8 physical cores
-.NET SDK 8.0.100
-  [Host]     : .NET 8.0.0 (8.0.23.53103), X64 RyuJIT AVX2
-  DefaultJob : .NET 8.0.0 (8.0.23.53103), X64 RyuJIT AVX2
+BenchmarkDotNet v0.13.10, Windows 11 (10.0.22621.3007/22H2/2022Update/SunValley2)
+11th Gen Intel Core i7-1185G7 3.00GHz, 1 CPU, 8 logical and 4 physical cores
+.NET SDK 8.0.101
+  [Host]     : .NET 8.0.1 (8.0.123.58001), X64 RyuJIT AVX2
+  DefaultJob : .NET 8.0.1 (8.0.123.58001), X64 RyuJIT AVX2
 
 
-| Method                    | AggregationTemporality | Mean      | Error    | StdDev   | Allocated |
-|-------------------------- |----------------------- |----------:|---------:|---------:|----------:|
-| CounterHotPath            | Cumulative             |  15.37 ns | 0.072 ns | 0.064 ns |         - |
-| CounterWith1LabelsHotPath | Cumulative             |  60.67 ns | 0.764 ns | 0.677 ns |         - |
-| CounterWith3LabelsHotPath | Cumulative             | 112.98 ns | 0.843 ns | 0.704 ns |         - |
-| CounterWith5LabelsHotPath | Cumulative             | 196.83 ns | 1.632 ns | 1.447 ns |         - |
-| CounterWith6LabelsHotPath | Cumulative             | 225.96 ns | 2.676 ns | 2.503 ns |         - |
-| CounterWith7LabelsHotPath | Cumulative             | 249.96 ns | 3.459 ns | 3.066 ns |         - |
-| CounterHotPath            | Delta                  |  19.83 ns | 0.158 ns | 0.148 ns |         - |
-| CounterWith1LabelsHotPath | Delta                  |  59.88 ns | 0.251 ns | 0.235 ns |         - |
-| CounterWith3LabelsHotPath | Delta                  | 124.24 ns | 1.490 ns | 1.394 ns |         - |
-| CounterWith5LabelsHotPath | Delta                  | 203.75 ns | 3.755 ns | 5.504 ns |         - |
-| CounterWith6LabelsHotPath | Delta                  | 226.50 ns | 2.036 ns | 1.805 ns |         - |
-| CounterWith7LabelsHotPath | Delta                  | 253.83 ns | 1.247 ns | 0.973 ns |         - |
+| Method                                | AggregationTemporality | Mean      | Error    | StdDev    | Gen0   | Allocated |
+|-------------------------------------- |----------------------- |----------:|---------:|----------:|-------:|----------:|
+| CounterHotPath                        | Cumulative             |  11.27 ns | 0.173 ns |  0.145 ns |      - |         - |
+| CounterWith1LabelsHotPath             | Cumulative             |  43.72 ns | 0.266 ns |  0.248 ns |      - |         - |
+| CounterWith2LabelsHotPath             | Cumulative             |  65.90 ns | 1.334 ns |  1.248 ns |      - |         - |
+| CounterWith3LabelsHotPath             | Cumulative             |  88.97 ns | 1.785 ns |  1.984 ns |      - |         - |
+| CounterWith4LabelsHotPath             | Cumulative             | 122.00 ns | 2.131 ns |  1.994 ns | 0.0138 |      88 B |
+| CounterWith5LabelsHotPath             | Cumulative             | 146.73 ns | 2.557 ns |  2.392 ns | 0.0165 |     104 B |
+| CounterWith6LabelsHotPath             | Cumulative             | 163.22 ns | 2.136 ns |  1.783 ns | 0.0191 |     120 B |
+| CounterWith7LabelsHotPath             | Cumulative             | 184.53 ns | 1.324 ns |  1.238 ns | 0.0215 |     136 B |
+| CounterWith1LabelsHotPathUsingTagList | Cumulative             |  58.27 ns | 1.157 ns |  1.504 ns |      - |         - |
+| CounterWith2LabelsHotPathUsingTagList | Cumulative             |  92.87 ns | 0.550 ns |  0.488 ns |      - |         - |
+| CounterWith3LabelsHotPathUsingTagList | Cumulative             | 120.31 ns | 0.739 ns |  0.617 ns |      - |         - |
+| CounterWith4LabelsHotPathUsingTagList | Cumulative             | 132.98 ns | 2.181 ns |  2.240 ns |      - |         - |
+| CounterWith5LabelsHotPathUsingTagList | Cumulative             | 154.56 ns | 2.685 ns |  2.380 ns |      - |         - |
+| CounterWith6LabelsHotPathUsingTagList | Cumulative             | 171.36 ns | 2.738 ns |  2.286 ns |      - |         - |
+| CounterWith7LabelsHotPathUsingTagList | Cumulative             | 194.81 ns | 1.894 ns |  1.582 ns |      - |         - |
+| CounterWith8LabelsHotPathUsingTagList | Cumulative             | 214.39 ns | 1.339 ns |  1.187 ns |      - |         - |
+| CounterWith9LabelsHotPathUsingTagList | Cumulative             | 300.38 ns | 3.945 ns |  3.690 ns | 0.0710 |     448 B |
+| CounterHotPath                        | Delta                  |  14.11 ns | 0.257 ns |  0.228 ns |      - |         - |
+| CounterWith1LabelsHotPath             | Delta                  |  49.15 ns | 0.295 ns |  0.246 ns |      - |         - |
+| CounterWith2LabelsHotPath             | Delta                  |  68.99 ns | 0.477 ns |  0.398 ns |      - |         - |
+| CounterWith3LabelsHotPath             | Delta                  |  93.35 ns | 1.294 ns |  1.080 ns |      - |         - |
+| CounterWith4LabelsHotPath             | Delta                  | 141.40 ns | 2.846 ns |  6.539 ns | 0.0138 |      88 B |
+| CounterWith5LabelsHotPath             | Delta                  | 163.34 ns | 3.189 ns |  3.917 ns | 0.0165 |     104 B |
+| CounterWith6LabelsHotPath             | Delta                  | 181.62 ns | 3.582 ns |  4.125 ns | 0.0191 |     120 B |
+| CounterWith7LabelsHotPath             | Delta                  | 201.33 ns | 2.700 ns |  2.108 ns | 0.0215 |     136 B |
+| CounterWith1LabelsHotPathUsingTagList | Delta                  |  75.56 ns | 1.457 ns |  1.496 ns |      - |         - |
+| CounterWith2LabelsHotPathUsingTagList | Delta                  |  91.48 ns | 1.852 ns |  2.714 ns |      - |         - |
+| CounterWith3LabelsHotPathUsingTagList | Delta                  | 129.23 ns | 2.608 ns |  3.298 ns |      - |         - |
+| CounterWith4LabelsHotPathUsingTagList | Delta                  | 150.55 ns | 2.433 ns |  2.498 ns |      - |         - |
+| CounterWith5LabelsHotPathUsingTagList | Delta                  | 191.60 ns | 3.119 ns |  2.918 ns |      - |         - |
+| CounterWith6LabelsHotPathUsingTagList | Delta                  | 196.49 ns | 2.874 ns |  2.400 ns |      - |         - |
+| CounterWith7LabelsHotPathUsingTagList | Delta                  | 224.42 ns | 4.482 ns |  8.196 ns |      - |         - |
+| CounterWith8LabelsHotPathUsingTagList | Delta                  | 243.75 ns | 4.861 ns |  9.482 ns |      - |         - |
+| CounterWith9LabelsHotPathUsingTagList | Delta                  | 331.22 ns | 6.493 ns | 11.373 ns | 0.0710 |     448 B |
 */
 
 namespace Benchmarks.Metrics;
