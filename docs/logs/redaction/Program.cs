3fb2ec50d0370dbe52ab216cfe898a8086040a8b
Reiley Yang <reyang@microsoft.com>
2024-01-26 13:45:25 -0800

    Log correlation/redaction doc improvements (#5262)
    Co-authored-by: Mikel Blanchard <mblanchard@macrosssoftware.com>

--- docs/logs/redaction/Program.cs
+++ docs/logs/redaction/Program.cs
@@ -22,7 +22,7 @@ logger.FoodPriceChanged("<secret>", 9.99);
 // This will flush the remaining logs and shutdown the logging pipeline.
 loggerFactory.Dispose();
 
-public static partial class ApplicationLogs
+internal static partial class LoggerExtensions
 {
     [LoggerMessage(LogLevel.Information, "Food `{name}` price changed to `{price}`.")]
     public static partial void FoodPriceChanged(this ILogger logger, string name, double price);
