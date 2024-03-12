ec50616f383c001bfffefa18cb84a398675026bd
Vishwesh Bankwar <Vishwesh.Bankwar@gmail.com>
2024-03-12 09:28:45 -0700

    rmv unused file

--- src/OpenTelemetry.Exporter.OpenTelemetryProtocol/Implementation/OtlpExporterExperimentalEnvVarKeyDefinitions.cs
+++
@@ -1,8 +0,0 @@
-// Copyright The OpenTelemetry Authors
-// SPDX-License-Identifier: Apache-2.0
-
-namespace OpenTelemetry.Exporter.OpenTelemetryProtocol.Implementation;
-
-internal class OtlpExporterExperimentalEnvVarKeyDefinitions
-{
-}
