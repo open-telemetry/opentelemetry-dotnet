af3e76e7dd3cdaa6ba2161cb158d9c13e27e5379
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2024-04-02 08:49:12 +0200

    Instrumentation.GrpcNetClient

--- src/OpenTelemetry.Instrumentation.GrpcNetClient/Implementation/GrpcClientDiagnosticListener.cs
+++ src/OpenTelemetry.Instrumentation.GrpcNetClient/Implementation/GrpcClientDiagnosticListener.cs
@@ -16,8 +16,8 @@ internal sealed class GrpcClientDiagnosticListener : ListenerHandler
 {
     internal static readonly AssemblyName AssemblyName = typeof(GrpcClientDiagnosticListener).Assembly.GetName();
     internal static readonly string ActivitySourceName = AssemblyName.Name;
-    internal static readonly Version Version = AssemblyName.Version;
-    internal static readonly ActivitySource ActivitySource = new(ActivitySourceName, Version.ToString());
+    internal static readonly string Version = SignalVersionHelper.GetVersion<GrpcClientDiagnosticListener>();
+    internal static readonly ActivitySource ActivitySource = new(ActivitySourceName, Version);
 
     private const string OnStartEvent = "Grpc.Net.Client.GrpcOut.Start";
     private const string OnStopEvent = "Grpc.Net.Client.GrpcOut.Stop";
