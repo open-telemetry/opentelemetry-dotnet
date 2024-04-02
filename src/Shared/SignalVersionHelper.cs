a71856c3fb16de00e485876386cf2be113e62b06
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2024-04-02 08:36:17 +0200

    Include SignalVersionHelper

---
+++ src/Shared/SignalVersionHelper.cs
@@ -0,0 +1,14 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+using System.Reflection;
+
+namespace OpenTelemetry.Instrumentation;
+
+internal static class SignalVersionHelper
+{
+    public static string GetVersion<T>()
+    {
+        return typeof(T).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()!.InformationalVersion.Split('+')[0];
+    }
+}
