dde68fa33198c541b31a4a0286a93ae57976b1fd
Martin Costello <martin@martincostello.com>
2024-04-08 20:34:43 +0100

    Improve AoT validation and fix OpenTelemetryProtocol AoT warnings (#5520)
    Co-authored-by: Eric Erhardt <eric.erhardt@microsoft.com>
    Co-authored-by: Mikel Blanchard <mblanchard@macrosssoftware.com>

--- src/Shared/Options/DelegatingOptionsFactory.cs
+++ src/Shared/Options/DelegatingOptionsFactory.cs
@@ -16,6 +16,9 @@
 #nullable enable
 
 using System.Diagnostics;
+#if NET6_0_OR_GREATER
+using System.Diagnostics.CodeAnalysis;
+#endif
 using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.Extensions.Options;
@@ -24,7 +27,11 @@ namespace Microsoft.Extensions.Options;
 /// Implementation of <see cref="IOptionsFactory{TOptions}"/>.
 /// </summary>
 /// <typeparam name="TOptions">The type of options being requested.</typeparam>
+#if NET6_0_OR_GREATER
+internal sealed class DelegatingOptionsFactory<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)] TOptions> :
+#else
 internal sealed class DelegatingOptionsFactory<TOptions> :
+#endif
     IOptionsFactory<TOptions>
     where TOptions : class
 {
