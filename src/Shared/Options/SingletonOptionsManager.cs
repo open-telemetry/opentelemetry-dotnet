0bbebb53678003213275b64cf1ff3f62ec8257cc
Mikel Blanchard <mblanchard@macrosssoftware.com>
2024-04-12 11:17:50 -0700

    [logs] Mitigate unwanted object creation during configuration reload (#5514)
    Co-authored-by: Reiley Yang <reyang@microsoft.com>

---
+++ src/Shared/Options/SingletonOptionsManager.cs
@@ -0,0 +1,47 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+#nullable enable
+
+#if NET6_0_OR_GREATER
+using System.Diagnostics.CodeAnalysis;
+#endif
+
+namespace Microsoft.Extensions.Options;
+
+#if NET6_0_OR_GREATER
+internal sealed class SingletonOptionsManager<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicParameterlessConstructor)] TOptions> : IOptionsMonitor<TOptions>, IOptionsSnapshot<TOptions>
+#else
+internal sealed class SingletonOptionsManager<TOptions> : IOptionsMonitor<TOptions>, IOptionsSnapshot<TOptions>
+#endif
+    where TOptions : class
+{
+    private readonly TOptions instance;
+
+    public SingletonOptionsManager(IOptions<TOptions> options)
+    {
+        this.instance = options.Value;
+    }
+
+    public TOptions CurrentValue => this.instance;
+
+    public TOptions Value => this.instance;
+
+    public TOptions Get(string? name) => this.instance;
+
+    public IDisposable? OnChange(Action<TOptions, string?> listener)
+        => NoopChangeNotification.Instance;
+
+    private sealed class NoopChangeNotification : IDisposable
+    {
+        private NoopChangeNotification()
+        {
+        }
+
+        public static NoopChangeNotification Instance { get; } = new();
+
+        public void Dispose()
+        {
+        }
+    }
+}
