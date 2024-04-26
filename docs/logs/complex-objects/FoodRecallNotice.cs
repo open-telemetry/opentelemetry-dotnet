afa7488a259293066d1e7a28e63e176553270a07
Reiley Yang <reyang@microsoft.com>
2024-04-26 12:15:15 -0700

    readonly

--- docs/logs/complex-objects/FoodRecallNotice.cs
+++ docs/logs/complex-objects/FoodRecallNotice.cs
@@ -1,15 +1,15 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-public struct FoodRecallNotice
+public readonly struct FoodRecallNotice
 {
-    public string? BrandName { get; set; }
+    public string? BrandName { get; init; }
 
-    public string? ProductDescription { get; set; }
+    public string? ProductDescription { get; init; }
 
-    public string? ProductType { get; set; }
+    public string? ProductType { get; init; }
 
-    public string? RecallReasonDescription { get; set; }
+    public string? RecallReasonDescription { get; init; }
 
-    public string? CompanyName { get; set; }
+    public string? CompanyName { get; init; }
 }
