c2558da11f5d1f83431a5023f83b0fa4833692e6
Reiley Yang <reyang@microsoft.com>
2024-04-30 10:03:10 -0700

    [docs-logs] Use struct in the complex type logging demo (#5577)
    Co-authored-by: Cijo Thomas <cijo.thomas@gmail.com>

--- docs/logs/complex-objects/FoodRecallNotice.cs
+++ docs/logs/complex-objects/FoodRecallNotice.cs
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-public class FoodRecallNotice
+public struct FoodRecallNotice
 {
     public string? BrandName { get; set; }
 
