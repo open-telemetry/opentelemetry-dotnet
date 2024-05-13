cf6283992b53c1a0d46637fd1994e4987922c52a
Reiley Yang <reyang@microsoft.com>
2024-05-13 16:38:58 -0700

    rename

--- build/scripts/add-labels.ps1
+++ build/scripts/add-labels.ps1
@@ -3,10 +3,10 @@ param(
   [Parameter(Mandatory=$true)][string]$issueBody
 )
 
-$match = [regex]::Match($issueBody, '^[#]+ Component\s*(OpenTelemetry(?:\.\w+)+)')
+$match = [regex]::Match($issueBody, '^[#]+ Package\s*(OpenTelemetry(?:\.\w+)*)')
 if ($match.Success -eq $false)
 {
     Return
 }
 
-gh issue edit $issueNumber --add-label $("comp:" + $match.Groups[1].Value)
+gh issue edit $issueNumber --add-label $("pkg:" + $match.Groups[1].Value)
