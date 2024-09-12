80c3f2527885a3a5a3aadbc7b480c3ff8a7354f3
Sandy Chen <Yuxuan.Chen@morganstanley.com>
2024-09-13 02:36:40 +0900

    feat(tests): merge certificate generation logic into PowerShell script

--- test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/IntegrationTest/run-test.sh
+++ test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/IntegrationTest/run-test.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Trust the self-signed certificated used by the collector
-cp /cfg/otel-collector.crt /usr/local/share/ca-certificates/
+cp /obj/Debug/otel-test-ca-cert.pem /usr/local/share/ca-certificates/
 update-ca-certificates --verbose
 
 dotnet test OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests.dll --TestCaseFilter:CategoryName=CollectorIntegrationTests --logger "console;verbosity=detailed"
