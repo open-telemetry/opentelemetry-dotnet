f6c4b0062813cf674578cdbdd9afeffc8f996b59
Sandy Chen <Yuxuan.Chen@morganstanley.com>
2024-09-20 14:02:57 +0900

    Improve Integration Test by Generating TLS/mTLS Certificates via MSBuild

--- test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/IntegrationTest/run-test.sh
+++ test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/IntegrationTest/run-test.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 set -e
 
-if [ ! -f /cfg/certs/otel-test-ca-cert.pem ]; then
-  echo "CA certificate not found!"
-  exit 1
-fi
-
 # Trust the self-signed certificate used by the collector
 cp /cfg/certs/otel-test-ca-cert.pem /usr/local/share/ca-certificates/otel-test-ca-cert.crt
 update-ca-certificates --verbose
-
-sleep 100000
 
 dotnet test OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests.dll --TestCaseFilter:CategoryName=CollectorIntegrationTests --logger "console;verbosity=detailed"
