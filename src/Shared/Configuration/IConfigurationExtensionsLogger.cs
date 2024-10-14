019f0d6459fc5c43c733e4debdc31c71e7174ee5
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2024-10-14 10:43:16 +0200

    remove #nullable enable from files

--- src/Shared/Configuration/IConfigurationExtensionsLogger.cs
+++ src/Shared/Configuration/IConfigurationExtensionsLogger.cs
@@ -1,8 +1,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-#nullable enable
-
 namespace Microsoft.Extensions.Configuration;
 
 internal interface IConfigurationExtensionsLogger
--- test/OpenTelemetry.Tests/Shared/EventSourceTestHelper.cs
+++ test/OpenTelemetry.Tests/Shared/EventSourceTestHelper.cs
@@ -1,8 +1,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-#nullable enable
-
 using System.Diagnostics.Tracing;
 using System.Globalization;
 using System.Reflection;
