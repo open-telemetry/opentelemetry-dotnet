8a0beb9f1ae2a571d6b0367f1cd0262f17829a47
Rajkumar Rangaraj <rajrang@microsoft.com>
2024-11-28 14:10:36 -0800

    Lint fixes

--- test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/OtlpRetryTests.cs
+++ test/OpenTelemetry.Exporter.OpenTelemetryProtocol.Tests/OtlpRetryTests.cs
@@ -118,23 +118,23 @@ public class OtlpRetryTests
 
         public static IEnumerable<object[]> GetGrpcTestCases()
         {
-            yield return new[] { new GrpcRetryTestCase("Cancelled", new GrpcRetryAttempt[] { new(Grpc.StatusCode.Cancelled) }) };
-            yield return new[] { new GrpcRetryTestCase("DeadlineExceeded", new GrpcRetryAttempt[] { new(Grpc.StatusCode.DeadlineExceeded) }) };
-            yield return new[] { new GrpcRetryTestCase("Aborted", new GrpcRetryAttempt[] { new(Grpc.StatusCode.Aborted) }) };
-            yield return new[] { new GrpcRetryTestCase("OutOfRange", new GrpcRetryAttempt[] { new(Grpc.StatusCode.OutOfRange) }) };
-            yield return new[] { new GrpcRetryTestCase("DataLoss", new GrpcRetryAttempt[] { new(Grpc.StatusCode.DataLoss) }) };
-            yield return new[] { new GrpcRetryTestCase("Unavailable", new GrpcRetryAttempt[] { new(Grpc.StatusCode.Unavailable) }) };
+            yield return new[] { new GrpcRetryTestCase("Cancelled", new GrpcRetryAttempt[] { new(StatusCode.Cancelled) }) };
+            yield return new[] { new GrpcRetryTestCase("DeadlineExceeded", new GrpcRetryAttempt[] { new(StatusCode.DeadlineExceeded) }) };
+            yield return new[] { new GrpcRetryTestCase("Aborted", new GrpcRetryAttempt[] { new(StatusCode.Aborted) }) };
+            yield return new[] { new GrpcRetryTestCase("OutOfRange", new GrpcRetryAttempt[] { new(StatusCode.OutOfRange) }) };
+            yield return new[] { new GrpcRetryTestCase("DataLoss", new GrpcRetryAttempt[] { new(StatusCode.DataLoss) }) };
+            yield return new[] { new GrpcRetryTestCase("Unavailable", new GrpcRetryAttempt[] { new(StatusCode.Unavailable) }) };
 
-            yield return new[] { new GrpcRetryTestCase("OK", new GrpcRetryAttempt[] { new(Grpc.StatusCode.OK, expectedSuccess: false) }) };
-            yield return new[] { new GrpcRetryTestCase("PermissionDenied", new GrpcRetryAttempt[] { new(Grpc.StatusCode.PermissionDenied, expectedSuccess: false) }) };
-            yield return new[] { new GrpcRetryTestCase("Unknown", new GrpcRetryAttempt[] { new(Grpc.StatusCode.Unknown, expectedSuccess: false) }) };
+            yield return new[] { new GrpcRetryTestCase("OK", new GrpcRetryAttempt[] { new(StatusCode.OK, expectedSuccess: false) }) };
+            yield return new[] { new GrpcRetryTestCase("PermissionDenied", new GrpcRetryAttempt[] { new(StatusCode.PermissionDenied, expectedSuccess: false) }) };
+            yield return new[] { new GrpcRetryTestCase("Unknown", new GrpcRetryAttempt[] { new(StatusCode.Unknown, expectedSuccess: false) }) };
 
-            yield return new[] { new GrpcRetryTestCase("ResourceExhausted w/o RetryInfo", new GrpcRetryAttempt[] { new(Grpc.StatusCode.ResourceExhausted, expectedSuccess: false) }) };
-            yield return new[] { new GrpcRetryTestCase("ResourceExhausted w/ RetryInfo", new GrpcRetryAttempt[] { new(Grpc.StatusCode.ResourceExhausted, throttleDelay: GetThrottleDelayString(new Duration { Seconds = 2 }), expectedNextRetryDelayMilliseconds: 3000) }) };
+            yield return new[] { new GrpcRetryTestCase("ResourceExhausted w/o RetryInfo", new GrpcRetryAttempt[] { new(StatusCode.ResourceExhausted, expectedSuccess: false) }) };
+            yield return new[] { new GrpcRetryTestCase("ResourceExhausted w/ RetryInfo", new GrpcRetryAttempt[] { new(StatusCode.ResourceExhausted, throttleDelay: GetThrottleDelayString(new Duration { Seconds = 2 }), expectedNextRetryDelayMilliseconds: 3000) }) };
 
-            yield return new[] { new GrpcRetryTestCase("Unavailable w/ RetryInfo", new GrpcRetryAttempt[] { new(Grpc.StatusCode.Unavailable, throttleDelay: GetThrottleDelayString(Duration.FromTimeSpan(TimeSpan.FromMilliseconds(2000))), expectedNextRetryDelayMilliseconds: 3000) }) };
+            yield return new[] { new GrpcRetryTestCase("Unavailable w/ RetryInfo", new GrpcRetryAttempt[] { new(StatusCode.Unavailable, throttleDelay: GetThrottleDelayString(Duration.FromTimeSpan(TimeSpan.FromMilliseconds(2000))), expectedNextRetryDelayMilliseconds: 3000) }) };
 
-            yield return new[] { new GrpcRetryTestCase("Expired deadline", new GrpcRetryAttempt[] { new(Grpc.StatusCode.Unavailable, deadlineExceeded: true, expectedSuccess: false) }) };
+            yield return new[] { new GrpcRetryTestCase("Expired deadline", new GrpcRetryAttempt[] { new(StatusCode.Unavailable, deadlineExceeded: true, expectedSuccess: false) }) };
 
             yield return new[]
             {
@@ -142,11 +142,11 @@ public class OtlpRetryTests
                     "Exponential backoff",
                     new GrpcRetryAttempt[]
                     {
-                        new(Grpc.StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 1500),
-                        new(Grpc.StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 2250),
-                        new(Grpc.StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 3375),
-                        new(Grpc.StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 5000),
-                        new(Grpc.StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 5000),
+                        new(StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 1500),
+                        new(StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 2250),
+                        new(StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 3375),
+                        new(StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 5000),
+                        new(StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 5000),
                     },
                     expectedRetryAttempts: 5),
             };
@@ -157,11 +157,11 @@ public class OtlpRetryTests
                     "Retry until non-retryable status code encountered",
                     new GrpcRetryAttempt[]
                     {
-                        new(Grpc.StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 1500),
-                        new(Grpc.StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 2250),
-                        new(Grpc.StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 3375),
-                        new(Grpc.StatusCode.PermissionDenied, expectedSuccess: false),
-                        new(Grpc.StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 5000),
+                        new(StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 1500),
+                        new(StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 2250),
+                        new(StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 3375),
+                        new(StatusCode.PermissionDenied, expectedSuccess: false),
+                        new(StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 5000),
                     },
                     expectedRetryAttempts: 4),
             };
@@ -173,15 +173,15 @@ public class OtlpRetryTests
                     "Exponential backoff after throttling",
                     new GrpcRetryAttempt[]
                     {
-                        new(Grpc.StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 1500),
-                        new(Grpc.StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 2250),
-                        new(Grpc.StatusCode.Unavailable, throttleDelay: GetThrottleDelayString(Duration.FromTimeSpan(TimeSpan.FromMilliseconds(500))), expectedNextRetryDelayMilliseconds: 750),
-                        new(Grpc.StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 1125),
-                        new(Grpc.StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 1688),
-                        new(Grpc.StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 2532),
-                        new(Grpc.StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 3798),
-                        new(Grpc.StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 5000),
-                        new(Grpc.StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 5000),
+                        new(StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 1500),
+                        new(StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 2250),
+                        new(StatusCode.Unavailable, throttleDelay: GetThrottleDelayString(Duration.FromTimeSpan(TimeSpan.FromMilliseconds(500))), expectedNextRetryDelayMilliseconds: 750),
+                        new(StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 1125),
+                        new(StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 1688),
+                        new(StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 2532),
+                        new(StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 3798),
+                        new(StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 5000),
+                        new(StatusCode.Unavailable, expectedNextRetryDelayMilliseconds: 5000),
                     },
                     expectedRetryAttempts: 9),
             };
@@ -218,13 +218,13 @@ public class OtlpRetryTests
             internal ExportClientGrpcResponse Response;
 
             internal GrpcRetryAttempt(
-                Grpc.StatusCode statusCode,
+                StatusCode statusCode,
                 bool deadlineExceeded = false,
                 string? throttleDelay = null,
                 int expectedNextRetryDelayMilliseconds = 1500,
                 bool expectedSuccess = true)
             {
-                var status = new Grpc.Status(statusCode, "Error");
+                var status = new Status(statusCode, "Error");
 
                 // Using arbitrary +1 hr for deadline for test purposes.
                 var deadlineUtc = deadlineExceeded ? DateTime.UtcNow.AddSeconds(-1) : DateTime.UtcNow.AddHours(1);
