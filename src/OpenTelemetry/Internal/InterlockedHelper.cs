421234598df603a808213426faaa6ff502fed780
Pent Ploompuu <peploomp@microsoft.com>
2025-01-07 10:35:49 +0200

    Remove spin wait from InterlockedHelper.Add

--- src/OpenTelemetry/Internal/InterlockedHelper.cs
+++ src/OpenTelemetry/Internal/InterlockedHelper.cs
@@ -10,36 +10,20 @@ internal static class InterlockedHelper
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void Add(ref double location, double value)
     {
-        // Note: Not calling InterlockedHelper.Read here on purpose because it
-        // is too expensive for fast/happy-path. If the first attempt fails
-        // we'll end up in an Interlocked.CompareExchange loop anyway.
         double currentValue = Volatile.Read(ref location);
-
-        var returnedValue = Interlocked.CompareExchange(ref location, currentValue + value, currentValue);
-        if (returnedValue != currentValue)
+        while (true)
         {
-            AddRare(ref location, value);
+            var returnedValue = Interlocked.CompareExchange(ref location, currentValue + value, currentValue);
+            if (returnedValue == currentValue)
+            {
+                break;
+            }
+
+            currentValue = returnedValue;
         }
     }
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static double Read(ref double location)
         => Interlocked.CompareExchange(ref location, 0, 0);
-
-    [MethodImpl(MethodImplOptions.NoInlining)]
-    private static void AddRare(ref double location, double value)
-    {
-        var sw = default(SpinWait);
-        while (true)
-        {
-            sw.SpinOnce();
-
-            double currentValue = Volatile.Read(ref location);
-            var returnedValue = Interlocked.CompareExchange(ref location, currentValue + value, currentValue);
-            if (returnedValue == currentValue)
-            {
-                break;
-            }
-        }
-    }
 }
