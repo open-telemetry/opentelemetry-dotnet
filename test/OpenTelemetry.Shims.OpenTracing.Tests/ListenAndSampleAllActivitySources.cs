5201147e14869d7d4dc0aba0ed73432b511a3d83
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2025-03-03 12:32:35 +0100

    CA1034

--- test/OpenTelemetry.Shims.OpenTracing.Tests/ListenAndSampleAllActivitySources.cs
+++ test/OpenTelemetry.Shims.OpenTracing.Tests/ListenAndSampleAllActivitySources.cs
@@ -1,35 +1,9 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-using System.Diagnostics;
 using Xunit;
 
 namespace OpenTelemetry.Shims.OpenTracing.Tests;
 
 [CollectionDefinition(nameof(ListenAndSampleAllActivitySources))]
-public sealed class ListenAndSampleAllActivitySources : ICollectionFixture<ListenAndSampleAllActivitySources.Fixture>
-{
-    public sealed class Fixture : IDisposable
-    {
-        private readonly ActivityListener listener;
-
-        public Fixture()
-        {
-            Activity.DefaultIdFormat = ActivityIdFormat.W3C;
-            Activity.ForceDefaultIdFormat = true;
-
-            this.listener = new ActivityListener
-            {
-                ShouldListenTo = _ => true,
-                Sample = (ref ActivityCreationOptions<ActivityContext> options) => ActivitySamplingResult.AllDataAndRecorded,
-            };
-
-            ActivitySource.AddActivityListener(this.listener);
-        }
-
-        public void Dispose()
-        {
-            this.listener.Dispose();
-        }
-    }
-}
+public sealed class ListenAndSampleAllActivitySources : ICollectionFixture<ListenAndSampleAllActivitySourcesFixture>;
---
+++ test/OpenTelemetry.Shims.OpenTracing.Tests/ListenAndSampleAllActivitySourcesFixture.cs
@@ -0,0 +1,30 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+using System.Diagnostics;
+
+namespace OpenTelemetry.Shims.OpenTracing.Tests;
+
+public sealed class ListenAndSampleAllActivitySourcesFixture : IDisposable
+{
+    private readonly ActivityListener listener;
+
+    public ListenAndSampleAllActivitySourcesFixture()
+    {
+        Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+        Activity.ForceDefaultIdFormat = true;
+
+        this.listener = new ActivityListener
+        {
+            ShouldListenTo = _ => true,
+            Sample = (ref ActivityCreationOptions<ActivityContext> options) => ActivitySamplingResult.AllDataAndRecorded,
+        };
+
+        ActivitySource.AddActivityListener(this.listener);
+    }
+
+    public void Dispose()
+    {
+        this.listener.Dispose();
+    }
+}
