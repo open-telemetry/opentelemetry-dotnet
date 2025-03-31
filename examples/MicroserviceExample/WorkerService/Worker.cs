99d1d475b6ff9a8a1448a613a306f368b06cf801
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2025-03-31 10:26:41 +0200

    Drop empty override methods

--- examples/MicroserviceExample/WorkerService/Worker.cs
+++ examples/MicroserviceExample/WorkerService/Worker.cs
@@ -14,16 +14,6 @@ internal class Worker : BackgroundService
         this.messageReceiver = messageReceiver;
     }
 
-    public override Task StartAsync(CancellationToken cancellationToken)
-    {
-        return base.StartAsync(cancellationToken);
-    }
-
-    public override async Task StopAsync(CancellationToken cancellationToken)
-    {
-        await base.StopAsync(cancellationToken).ConfigureAwait(false);
-    }
-
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         stoppingToken.ThrowIfCancellationRequested();
