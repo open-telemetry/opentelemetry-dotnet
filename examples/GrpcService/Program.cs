3c0a4a96ad10ed90ed7c1c49c402ce732dd028a0
Yevhenii Solomchenko <ysolomchenko@splunk.com>
2025-04-02 10:27:45 +0200

    Use Minimal Hosting Model in GrpcService

--- examples/GrpcService/Program.cs
+++ examples/GrpcService/Program.cs
@@ -1,22 +1,59 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-namespace Examples.GrpcService;
+using Examples.GrpcService;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
-internal static class Program
-{
-    public static void Main(string[] args)
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddGrpc();
+
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracerBuilder =>
     {
-        CreateHostBuilder(args).Build().Run();
-    }
+        tracerBuilder.ConfigureResource(r => r.AddService(
+            builder.Configuration.GetValue("ServiceName", defaultValue: "otel-test")))
+            .AddAspNetCoreInstrumentation();
 
-    // Additional configuration is required to successfully run gRPC on macOS.
-    // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-                webBuilder.UseUrls("https://localhost:44335");
-            });
+        var exporter = builder.Configuration.GetValue("UseExporter", defaultValue: "console").ToUpperInvariant();
+        switch (exporter)
+        {
+            case "OTLP":
+                tracerBuilder.AddOtlpExporter(otlpOptions =>
+                {
+                    otlpOptions.Endpoint = new Uri(builder.Configuration.GetValue("Otlp:Endpoint", defaultValue: "http://localhost:4317"));
+                });
+                break;
+            case "ZIPKIN":
+                tracerBuilder.AddZipkinExporter(zipkinOptions =>
+                {
+                    zipkinOptions.Endpoint = new Uri(builder.Configuration.GetValue("Zipkin:Endpoint", defaultValue: "http://localhost:9411/api/v2/spans"));
+                });
+                break;
+            default:
+                tracerBuilder.AddConsoleExporter();
+                break;
+        }
+    });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseRouting();
+
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", async context =>
+{
+    await context.Response.WriteAsync(
+        "Communication with gRPC endpoints must be made through a gRPC client. " +
+        "To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909").ConfigureAwait(true);
+});
+
+app.Run();
