aab3953044865ec8d17c7e7f5da7a6f8b69b85c5
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2025-04-09 10:10:44 +0200

    CA1052

--- test/TestApp.AspNetCore/Program.cs
+++ test/TestApp.AspNetCore/Program.cs
@@ -3,52 +3,46 @@
 
 using TestApp.AspNetCore;
 
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddMvc();
+
+builder.Services.AddSingleton<HttpClient>();
+
+builder.Services.AddSingleton(
+    new CallbackMiddleware.CallbackMiddlewareImpl());
+
+builder.Services.AddSingleton(
+    new ActivityMiddleware.ActivityMiddlewareImpl());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-
-        builder.Services.AddSwaggerGen();
-
-        builder.Services.AddMvc();
-
-        builder.Services.AddSingleton<HttpClient>();
-
-        builder.Services.AddSingleton(
-            new CallbackMiddleware.CallbackMiddlewareImpl());
-
-        builder.Services.AddSingleton(
-            new ActivityMiddleware.ActivityMiddlewareImpl());
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.UseMiddleware<CallbackMiddleware>();
-
-        app.UseMiddleware<ActivityMiddleware>();
-
-        app.AddTestMiddleware();
-
-        app.Run();
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseMiddleware<CallbackMiddleware>();
+
+app.UseMiddleware<ActivityMiddleware>();
+
+app.AddTestMiddleware();
+
+app.Run();
