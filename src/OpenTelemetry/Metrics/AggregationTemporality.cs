a3434ceed821848e1201c99bcba6a8d5a99c3bf2
Piotr Kiełkowicz <pkiekowicz@splunk.com>
2025-04-24 08:11:44 +0200

    Disable CA1008

--- src/OpenTelemetry/Metrics/AggregationTemporality.cs
+++ src/OpenTelemetry/Metrics/AggregationTemporality.cs
@@ -7,7 +7,9 @@ namespace OpenTelemetry.Metrics;
 /// Enumeration used to define the aggregation temporality for a <see
 /// cref="Metric"/>.
 /// </summary>
+#pragma warning disable CA1008 // Enums should have zero value
 public enum AggregationTemporality : byte
+#pragma warning enable CA1008 // Enums should have zero value
 {
     /// <summary>
     /// Cumulative.
