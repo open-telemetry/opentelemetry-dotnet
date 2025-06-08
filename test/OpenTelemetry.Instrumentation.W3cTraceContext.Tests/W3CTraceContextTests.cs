66c31ed699caa321365de34d01a297e72a24da76
martincostello <martin@martincostello.com>
2025-06-08 16:04:19 +0100

    Move workaround
    Move the workaround to a static constructor so it runs early enough to affect the tests.

--- test/OpenTelemetry.Instrumentation.W3cTraceContext.Tests/W3CTraceContextTests.cs
+++ test/OpenTelemetry.Instrumentation.W3cTraceContext.Tests/W3CTraceContextTests.cs
@@ -24,6 +24,14 @@ public sealed class W3CTraceContextTests : IDisposable
     private readonly HttpClient httpClient = new();
     private readonly ITestOutputHelper output;
 
+#if NET10_0_OR_GREATER
+    static W3CTraceContextTests()
+    {
+        // See https://learn.microsoft.com/en-us/dotnet/core/compatibility/core-libraries/10.0/default-trace-context-propagator
+        DistributedContextPropagator.Current = DistributedContextPropagator.CreatePreW3CPropagator();
+    }
+#endif
+
     public W3CTraceContextTests(ITestOutputHelper output)
     {
         this.output = output;
@@ -35,9 +43,6 @@ public sealed class W3CTraceContextTests : IDisposable
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "xUnit1026:Theory methods should use all of their parameters", Justification = "Need to use SkipUnlessEnvVarFoundTheory")]
     public void W3CTraceContextTestSuiteAsync(string value)
     {
-        // See https://learn.microsoft.com/en-us/dotnet/core/compatibility/core-libraries/10.0/default-trace-context-propagator
-        DistributedContextPropagator.Current = DistributedContextPropagator.CreatePreW3CPropagator();
-
         // configure SDK
         using var tracerProvider = Sdk.CreateTracerProviderBuilder()
             .AddAspNetCoreInstrumentation()
