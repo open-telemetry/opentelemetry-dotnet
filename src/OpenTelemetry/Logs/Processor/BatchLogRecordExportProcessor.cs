6b971e595de6826014dfb1c5ad2cbc4edb05f81a
Jerome Laban <jerome@platform.uno>
2025-07-17 07:50:08 -0400

    chore: Reorder test parameters in xml doc

--- src/OpenTelemetry/Logs/Processor/BatchLogRecordExportProcessor.cs
+++ src/OpenTelemetry/Logs/Processor/BatchLogRecordExportProcessor.cs
@@ -39,11 +39,11 @@ public class BatchLogRecordExportProcessor : BatchExportProcessor<LogRecord>
     /// Initializes a new instance of the <see cref="BatchLogRecordExportProcessor"/> class.
     /// </summary>
     /// <param name="exporter">Log record exporter.</param>
-    /// <param name="useThreads">Enables the use of <see cref="Thread" /> when true, <see cref="Task"/> when false.</param>
     /// <param name="maxQueueSize">The maximum queue size. After the size is reached data are dropped. The default value is 2048.</param>
     /// <param name="scheduledDelayMilliseconds">The delay interval in milliseconds between two consecutive exports. The default value is 5000.</param>
     /// <param name="exporterTimeoutMilliseconds">How long the export can run before it is cancelled. The default value is 30000.</param>
     /// <param name="maxExportBatchSize">The maximum batch size of every export. It must be smaller or equal to maxQueueSize. The default value is 512.</param>
+    /// <param name="useThreads">Enables the use of <see cref="Thread" /> when true, <see cref="Task"/> when false.</param>
     public BatchLogRecordExportProcessor(
         BaseExporter<LogRecord> exporter,
         int maxQueueSize = DefaultMaxQueueSize,
