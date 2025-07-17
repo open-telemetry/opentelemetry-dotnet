22cab86df1317e7f2e5fce45d7fa0d6c5e288210
Jerome Laban <jerome@platform.uno>
2025-07-17 09:42:51 -0400

    chore: Remove unused namespace

--- test/OpenTelemetry.Tests/Metrics/MetricPointReclaimTests.cs
+++ test/OpenTelemetry.Tests/Metrics/MetricPointReclaimTests.cs
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 using System.Diagnostics.Metrics;
-using Microsoft.ApplicationInsights.Extensibility.Implementation;
 using OpenTelemetry.Tests;
 using Xunit;
 
