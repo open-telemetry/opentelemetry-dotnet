8c99fd9a7451685153fb6938ddf05b4ed0f76d00
hannahhaering <hannah.haering96@gmail.com>
2025-09-19 16:05:11 -0400

    added support for gzip compression in example console application

--- examples/Console/Program.cs
+++ examples/Console/Program.cs
@@ -125,6 +125,9 @@ internal sealed class OtlpOptions
 
     [Option('p', "protocol", HelpText = "Transport protocol used by exporter. Supported values: grpc and http/protobuf.", Default = "grpc")]
     public string? Protocol { get; set; }
+
+    [Option('c', "compression", HelpText = "Compression algorithm used by exporter. Supported values: none and gzip.", Default = "None")]
+    public string? Compression { get; set; }
 }
 
 [Verb("logs", HelpText = "Specify the options required to test Logs")]
