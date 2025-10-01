d04866428119a95d9b9ea2b2607bdb65e2ed243f
hannahhaering <hannah.haering96@gmail.com>
2025-09-30 23:56:48 -0400

    fix reset scale if delta for exponential histogram

--- test/OpenTelemetry.Tests/Metrics/AggregatorTests.cs
+++ test/OpenTelemetry.Tests/Metrics/AggregatorTests.cs
@@ -352,10 +352,12 @@ public class AggregatorTests
         var count = metricPoint.GetHistogramCount();
         var sum = metricPoint.GetHistogramSum();
         var hasMinMax = metricPoint.TryGetHistogramMinMaxValues(out var min, out var max);
+        var firstScale = metricPoint.GetExponentialHistogramData().Scale;
 
         AssertExponentialBucketsAreCorrect(expectedHistogram, metricPoint.GetExponentialHistogramData());
         Assert.Equal(50, sum);
         Assert.Equal(6, count);
+        Assert.True(firstScale <= Metric.DefaultExponentialHistogramMaxScale);
 
         if (aggregationType == AggregationType.Base2ExponentialHistogramWithMinMax)
         {
@@ -373,12 +375,14 @@ public class AggregatorTests
         count = metricPoint.GetHistogramCount();
         sum = metricPoint.GetHistogramSum();
         hasMinMax = metricPoint.TryGetHistogramMinMaxValues(out min, out max);
+        var secondScale = metricPoint.GetExponentialHistogramData().Scale;
 
         if (aggregationTemporality == AggregationTemporality.Cumulative)
         {
             AssertExponentialBucketsAreCorrect(expectedHistogram, metricPoint.GetExponentialHistogramData());
             Assert.Equal(50, sum);
             Assert.Equal(6, count);
+            Assert.Equal(firstScale, secondScale);
 
             if (aggregationType == AggregationType.Base2ExponentialHistogramWithMinMax)
             {
@@ -393,10 +397,11 @@ public class AggregatorTests
         }
         else
         {
-            expectedHistogram.Reset();
+            expectedHistogram.Reset(true);
             AssertExponentialBucketsAreCorrect(expectedHistogram, metricPoint.GetExponentialHistogramData());
             Assert.Equal(0, sum);
             Assert.Equal(0, count);
+            Assert.Equal(expectedHistogram.Scale, secondScale);
 
             if (aggregationType == AggregationType.Base2ExponentialHistogramWithMinMax)
             {
