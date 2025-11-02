b10b5496a84cc7eb4986394f7d3955fc647072e0
hannahhaering <hannah.haering96@gmail.com>
2025-11-02 15:44:40 -0500

    added tests for double metrics

--- test/OpenTelemetry.Tests/Metrics/MetricReaderTests.cs
+++ test/OpenTelemetry.Tests/Metrics/MetricReaderTests.cs
@@ -10,12 +10,17 @@ namespace OpenTelemetry.Metrics.Tests;
 public class MetricReaderTests
 {
     [Theory]
-    [InlineData("counter", AggregationTemporality.Delta)]
-    [InlineData("histogram", AggregationTemporality.Delta)]
-    [InlineData("updowncounter", AggregationTemporality.Cumulative)]
-    [InlineData("observablecounter", AggregationTemporality.Cumulative)]
-    [InlineData("observableupdowncounter", AggregationTemporality.Cumulative)]
-    public void LowMemoryTemporality_UsesCorrectAggregationTemporality(string instrumentName, AggregationTemporality expectedTemporality)
+    [InlineData("counter", typeof(long), AggregationTemporality.Delta)]
+    [InlineData("counter", typeof(double), AggregationTemporality.Delta)]
+    [InlineData("histogram", typeof(long), AggregationTemporality.Delta)]
+    [InlineData("histogram", typeof(double), AggregationTemporality.Delta)]
+    [InlineData("updowncounter", typeof(long), AggregationTemporality.Cumulative)]
+    [InlineData("updowncounter", typeof(double), AggregationTemporality.Cumulative)]
+    [InlineData("observablecounter", typeof(long), AggregationTemporality.Cumulative)]
+    [InlineData("observablecounter", typeof(double), AggregationTemporality.Cumulative)]
+    [InlineData("observableupdowncounter", typeof(long), AggregationTemporality.Cumulative)]
+    [InlineData("observableupdowncounter", typeof(double), AggregationTemporality.Cumulative)]
+    public void LowMemoryTemporality_UsesCorrectAggregationTemporality(string instrumentName, Type valueType, AggregationTemporality expectedTemporality)
     {
         var metrics = new List<Metric>();
         using var meter = new Meter(Utils.GetCurrentMethodName());
@@ -30,26 +35,63 @@ public class MetricReaderTests
         switch (instrumentName)
         {
             case "counter":
-                var counter = meter.CreateCounter<long>("test_counter");
-                counter.Add(1);
+                if (valueType == typeof(long))
+                {
+                    meter.CreateCounter<long>("test_counter").Add(1);
+                }
+                else
+                {
+                    meter.CreateCounter<double>("test_counter").Add(1);
+                }
+
                 break;
 
             case "histogram":
-                var histogram = meter.CreateHistogram<long>("test_histogram");
-                histogram.Record(1);
+                if (valueType == typeof(long))
+                {
+                    meter.CreateHistogram<long>("test_histogram").Record(1);
+                }
+                else
+                {
+                    meter.CreateHistogram<double>("test_histogram").Record(1);
+                }
+
                 break;
 
             case "updowncounter":
-                var upDown = meter.CreateUpDownCounter<long>("test_updown");
-                upDown.Add(1);
+                if (valueType == typeof(long))
+                {
+                    meter.CreateUpDownCounter<long>("test_updown").Add(1);
+                }
+                else
+                {
+                    meter.CreateUpDownCounter<double>("test_updown").Add(1);
+                }
+
                 break;
 
             case "observablecounter":
-                meter.CreateObservableCounter("test_observable_counter", () => new Measurement<long>(1));
+                if (valueType == typeof(long))
+                {
+                    meter.CreateObservableCounter("test_observable_counter", () => new Measurement<long>(1));
+                }
+                else
+                {
+                    meter.CreateObservableCounter("test_observable_counter", () => new Measurement<double>(1));
+                }
+
                 break;
 
             case "observableupdowncounter":
-                meter.CreateObservableUpDownCounter("test_observable_updown", () => new Measurement<long>(1));
+                if (valueType == typeof(long))
+                {
+                    meter.CreateObservableUpDownCounter("test_observable_updown", () => new Measurement<long>(1));
+                }
+                else
+                {
+                    meter.CreateObservableUpDownCounter("test_observable_updown", () => new Measurement<double>(1));
+                }
+
                 break;
         }
 
