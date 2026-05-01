b31d1185c2673abd6f552b99f62fb51424a0d7f9
martincostello <martin@martincostello.com>
2026-05-01 21:33:03 +0100

    [Exporter.Prometheus] Fix SA1507 warning
    Fix-up merge.

--- src/OpenTelemetry.Exporter.Prometheus.HttpListener/Internal/PrometheusSerializer.cs
+++ src/OpenTelemetry.Exporter.Prometheus.HttpListener/Internal/PrometheusSerializer.cs
@@ -554,7 +554,6 @@ internal static partial class PrometheusSerializer
         return WriteUnicodeNoEscape(buffer, cursor, 0xFFFD);
     }
 
-
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static int WriteUnixTimeSeconds(byte[] buffer, int cursor, DateTimeOffset value) =>
 #if NET
