a3956876057b00a390c1e2a84e36964036e3f02d
Omri SirComp <omribz156@gmail.com>
2026-05-17 12:20:48 +0300

    Update exporter extension DI sample

--- docs/trace/extending-the-sdk/MyExporterExtensions.cs
+++ docs/trace/extending-the-sdk/MyExporterExtensions.cs
@@ -1,18 +1,40 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using OpenTelemetry;
 using OpenTelemetry.Trace;
 
 internal static class MyExporterExtensions
 {
-    public static TracerProviderBuilder AddMyExporter(this TracerProviderBuilder builder)
+    public static TracerProviderBuilder AddMyExporter(
+        this TracerProviderBuilder builder,
+        string? name = null,
+        Action<MyExporterOptions>? configure = null)
     {
         if (builder == null)
         {
             throw new ArgumentNullException(nameof(builder));
         }
 
-        return builder.AddProcessor(new BatchActivityExportProcessor(new MyExporter()));
+        name ??= Options.DefaultName;
+
+        if (configure != null)
+        {
+            builder.ConfigureServices(services => services.Configure(name, configure));
+        }
+
+        return builder.AddProcessor(serviceProvider =>
+        {
+            var options = serviceProvider.GetRequiredService<IOptionsMonitor<MyExporterOptions>>().Get(name);
+
+            return new BatchActivityExportProcessor(new MyExporter(options.Name));
+        });
     }
+}
+
+internal sealed class MyExporterOptions
+{
+    public string Name { get; set; } = "MyExporter";
 }
