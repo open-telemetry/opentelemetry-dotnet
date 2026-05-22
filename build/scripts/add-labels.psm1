3a7cb1d54d11e58b4f9bcd419a4a7814b4c629fc
martincostello <martin@martincostello.com>
2026-05-22 16:12:24 +0100

    [Infra] Fix PowerShell lint warnings
    Fix lint warnings.

--- build/scripts/add-labels.psm1
+++ build/scripts/add-labels.psm1
@@ -44,7 +44,6 @@ function AddLabelsOnPullRequestsBasedOnFilesChanged {
   {
     $fileChanged = $fileChanged.ToLower()
     $fullFileName = [System.IO.Path]::GetFileName($fileChanged)
-    $fileName = [System.IO.Path]::GetFileNameWithoutExtension($fileChanged)
     $fileExtension = [System.IO.Path]::GetExtension($fileChanged)
 
     if ($fileChanged.StartsWith('src/') -or $fileChanged.StartsWith('test/'))
@@ -57,7 +56,7 @@ function AddLabelsOnPullRequestsBasedOnFilesChanged {
       $rawProjectName = $match.Groups[1].Value
       if ($rawProjectName.Contains(".benchmarks") -or $rawProjectName.Contains(".stress"))
       {
-        $added = $labelsToAdd.Add("perf")
+        $labelsToAdd.Add("perf")
       }
 
       $projectName = $rawProjectName.Replace(".tests", "").Replace(".benchmarks", "").Replace(".stress", "")
@@ -66,7 +65,7 @@ function AddLabelsOnPullRequestsBasedOnFilesChanged {
         continue
       }
 
-      $added = $visitedProjects.Add($projectName);
+      $visitedProjects.Add($projectName)
 
       foreach ($repoLabel in $repoLabels)
       {
@@ -80,7 +79,7 @@ function AddLabelsOnPullRequestsBasedOnFilesChanged {
             }
             if ($package -eq $projectName)
             {
-                $added = $labelsToAdd.Add($repoLabel.name)
+                $labelsToAdd.Add($repoLabel.name)
                 break
             }
         }
@@ -91,7 +90,7 @@ function AddLabelsOnPullRequestsBasedOnFilesChanged {
         $fileChanged.StartsWith('docs/') -or
         $fileChanged.StartsWith('examples/'))
     {
-        $added = $labelsToAdd.Add("documentation")
+        $labelsToAdd.Add("documentation")
     }
 
     if ($fileChanged.StartsWith('build/') -or
@@ -101,17 +100,17 @@ function AddLabelsOnPullRequestsBasedOnFilesChanged {
         $fileExtension -eq ".targets" -or
         $fileChanged.StartsWith('test/openTelemetry.aotcompatibility'))
     {
-        $added = $labelsToAdd.Add("infra")
+        $labelsToAdd.Add("infra")
     }
 
     if ($fileChanged.StartsWith('test/benchmarks'))
     {
-        $added = $labelsToAdd.Add("perf")
+        $labelsToAdd.Add("perf")
     }
 
     if ($fullFileName -eq 'directory.packages.props')
     {
-        $added = $labelsToAdd.Add("dependencies")
+        $labelsToAdd.Add("dependencies")
     }
   }
 
@@ -119,12 +118,12 @@ function AddLabelsOnPullRequestsBasedOnFilesChanged {
   {
      if ($labelsToAdd.Contains($labelOnPullRequest.name))
      {
-        $removed = $labelsToAdd.Remove($labelOnPullRequest.name)
+        $labelsToAdd.Remove($labelOnPullRequest.name)
      }
      elseif ($labelOnPullRequest.name.StartsWith($labelPackagePrefix) -or
         $managedLabels.Contains($labelOnPullRequest.name))
      {
-        $added = $labelsToRemove.Add($labelOnPullRequest.name)
+        $labelsToRemove.Add($labelOnPullRequest.name)
      }
   }
 
