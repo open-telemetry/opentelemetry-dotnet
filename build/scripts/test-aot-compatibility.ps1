400453070c011e4e7295efa72e6f353d1909932b
Martin Costello <martin@martincostello.com>
2026-05-25 06:48:55 +0100

    [Infra] Lint PowerShell scripts (#7343)

--- build/scripts/test-aot-compatibility.ps1
+++ build/scripts/test-aot-compatibility.ps1
@@ -1,49 +1,56 @@
 param([string]$targetNetFramework)
 
+$ErrorActionPreference = "Stop"
+$InformationPreference = "Continue"
+$WarningPreference = "Continue"
+
 $rootDirectory = Get-Location
 
 $publishOutput = dotnet publish $rootDirectory/test/OpenTelemetry.AotCompatibility.TestApp/OpenTelemetry.AotCompatibility.TestApp.csproj --framework $targetNetFramework -nodeReuse:false /p:UseSharedCompilation=false /p:ExposeExperimentalFeatures=true
 
 $actualWarningCount = 0
+$testPassed = 0
 
 foreach ($line in $($publishOutput -split "`r`n"))
 {
     if (($line -like "*analysis warning IL*") -or ($line -like "*analysis error IL*"))
     {
-        Write-Host $line
+        Write-Warning $line
         $actualWarningCount += 1
     }
 }
 
-Write-Host "Actual warning count is:", $actualWarningCount
+Write-Information "Actual warning count is: $actualWarningCount"
 $expectedWarningCount = 0
 
 if ($LastExitCode -ne 0)
 {
-    Write-Host "There was an error while publishing AotCompatibility Test App. LastExitCode is:", $LastExitCode
-    Write-Host $publishOutput
+    $testPassed = 1
+    Write-Warning "There was an error while publishing AotCompatibility Test App. LastExitCode is: $LastExitCode"
+    Write-Warning $publishOutput
 }
 
 $app = $IsWindows ? "./OpenTelemetry.AotCompatibility.TestApp.exe" : "./OpenTelemetry.AotCompatibility.TestApp"
 
 Push-Location $rootDirectory/artifacts/publish/OpenTelemetry.AotCompatibility.TestApp/release_$targetNetFramework
 
-Write-Host "Executing test App..."
+Write-Information "Executing test App..."
 & $app
-Write-Host "Finished executing test App"
+Write-Information "Finished executing test App"
 
 if ($LastExitCode -ne 0)
 {
-  Write-Host "There was an error while executing AotCompatibility Test App. LastExitCode is:", $LastExitCode
+    $testPassed = 1
+    Write-Warning "There was an error while executing AotCompatibility Test App. LastExitCode is: $LastExitCode"
+    Write-Warning $publishOutput
 }
 
 Pop-Location
 
-$testPassed = 0
 if ($actualWarningCount -ne $expectedWarningCount)
 {
     $testPassed = 1
-    Write-Host "Actual warning count:", $actualWarningCount, "is not as expected. Expected warning count is:", $expectedWarningCount
+    Write-Warning "Actual warning count: $actualWarningCount is not as expected. Expected warning count is: $expectedWarningCount"
 }
 
 Exit $testPassed
