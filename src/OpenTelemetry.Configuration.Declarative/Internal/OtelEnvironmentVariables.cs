2eaa8f62d1702587952b790d4fc0a29c7ce97d57
Steve Gordon <sgordon@hotmail.co.uk>
2026-06-22 13:23:58 +0100

    Review feedback

--- src/OpenTelemetry.Configuration.Declarative/Internal/OtelEnvironmentVariables.cs
+++ src/OpenTelemetry.Configuration.Declarative/Internal/OtelEnvironmentVariables.cs
@@ -7,14 +7,6 @@ namespace OpenTelemetry.Configuration.Declarative;
 /// OTel specification-defined environment variable names read by the SDK via
 /// <see cref="Microsoft.Extensions.Configuration.IConfiguration"/>.
 /// </summary>
-/// <remarks>
-/// Canonical definitions exist in the core <c>OpenTelemetry</c> package as internal constants.
-/// These are duplicated here because the core SDK's internal types are not accessible
-/// from this package without an <c>InternalsVisibleTo</c> entry. Once the declarative config
-/// API stabilises, the core SDK could expose these as public constants, or they could be moved
-/// to the shared directory and this file should be removed. See
-/// <see href="https://github.com/open-telemetry/opentelemetry-dotnet/issues/6380">#6380</see>.
-/// </remarks>
 internal static class OtelEnvironmentVariables
 {
     /// <summary>
