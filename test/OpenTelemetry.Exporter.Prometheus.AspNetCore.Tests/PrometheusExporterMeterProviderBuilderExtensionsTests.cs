9f5af4ec74af0d90298c05d2e1d951548e39839e
martincostello <martin@martincostello.com>
2026-07-24 10:50:34 +0100

    [Exporter.Prometheus] Increase code coverage
    Add some additional code coverage for the Prometheus exporters.

--- test/OpenTelemetry.Exporter.Prometheus.AspNetCore.Tests/PrometheusExporterMeterProviderBuilderExtensionsTests.cs
+++ test/OpenTelemetry.Exporter.Prometheus.AspNetCore.Tests/PrometheusExporterMeterProviderBuilderExtensionsTests.cs
@@ -43,4 +43,62 @@ public sealed class PrometheusExporterMeterProviderBuilderExtensionsTests
 
         Assert.Equal(PrometheusTranslationStrategy.NoTranslation, options.ExporterOptions.TranslationStrategy);
     }
+
+    [Fact]
+    public void DisableTotalNameSuffixForCounters_DelegatesToExporterOptions()
+    {
+        var options = new PrometheusAspNetCoreOptions { DisableTotalNameSuffixForCounters = true };
+
+        Assert.True(options.ExporterOptions.DisableTotalNameSuffixForCounters);
+        Assert.True(options.DisableTotalNameSuffixForCounters);
+    }
+
+    [Fact]
+    public void ScopeInfoEnabled_DelegatesToExporterOptions()
+    {
+        var options = new PrometheusAspNetCoreOptions { ScopeInfoEnabled = false };
+
+        Assert.False(options.ExporterOptions.ScopeInfoEnabled);
+        Assert.False(options.ScopeInfoEnabled);
+    }
+
+    [Fact]
+    public void ScrapeResponseCacheDurationMilliseconds_DelegatesToExporterOptions()
+    {
+        var options = new PrometheusAspNetCoreOptions { ScrapeResponseCacheDurationMilliseconds = 42 };
+
+        Assert.Equal(42, options.ExporterOptions.ScrapeResponseCacheDurationMilliseconds);
+        Assert.Equal(42, options.ScrapeResponseCacheDurationMilliseconds);
+    }
+
+    [Fact]
+    public void TargetInfoEnabled_DelegatesToExporterOptions()
+    {
+        var options = new PrometheusAspNetCoreOptions { TargetInfoEnabled = false };
+
+        Assert.False(options.ExporterOptions.TargetInfoEnabled);
+        Assert.False(options.TargetInfoEnabled);
+    }
+
+    [Fact]
+    public void ResourceConstantLabels_DelegatesToExporterOptions()
+    {
+        Func<string, bool> predicate = static _ => true;
+
+        var options = new PrometheusAspNetCoreOptions { ResourceConstantLabels = predicate };
+
+        Assert.Same(predicate, options.ExporterOptions.ResourceConstantLabels);
+        Assert.Same(predicate, options.ResourceConstantLabels);
+    }
+
+    [Fact]
+    public void MaxScrapeResponseSizeBytes_DelegatesToExporterOptions()
+    {
+        const int Value = PrometheusExporterOptions.InitialScrapeResponseSizeBytes * 4;
+
+        var options = new PrometheusAspNetCoreOptions { MaxScrapeResponseSizeBytes = Value };
+
+        Assert.Equal(Value, options.ExporterOptions.MaxScrapeResponseSizeBytes);
+        Assert.Equal(Value, options.MaxScrapeResponseSizeBytes);
+    }
 }
