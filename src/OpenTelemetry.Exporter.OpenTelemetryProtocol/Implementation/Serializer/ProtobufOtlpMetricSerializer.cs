6ee6b3c0f23092814a552d8fa6838e38108232e1
Matthew Hensley <130569+matt-hensley@users.noreply.github.com>
2026-08-19 16:00:28 +0000

    [OTLP] Scan histogram bounds once (#7647)
    Co-authored-by: Martin Costello <martin@martincostello.com>

--- src/OpenTelemetry.Exporter.OpenTelemetryProtocol/Implementation/Serializer/ProtobufOtlpMetricSerializer.cs
+++ src/OpenTelemetry.Exporter.OpenTelemetryProtocol/Implementation/Serializer/ProtobufOtlpMetricSerializer.cs
@@ -603,15 +603,9 @@ internal static class ProtobufOtlpMetricSerializer
 
         static int WriteExplicitBounds(byte[] buffer, int writePosition, double[] values)
         {
-            var length = 0;
+            Debug.Assert(!Array.Exists(values, static value => double.IsInfinity(value)), "Explicit bounds must not contain positive or negative infinity.");
 
-            for (var i = 0; i < values.Length; i++)
-            {
-                if (values[i] != double.PositiveInfinity)
-                {
-                    length++;
-                }
-            }
+            var length = values.Length;
 
             if (length > 0)
             {
@@ -623,11 +617,7 @@ internal static class ProtobufOtlpMetricSerializer
 
                 for (var i = 0; i < values.Length; i++)
                 {
-                    var value = values[i];
-                    if (value != double.PositiveInfinity)
-                    {
-                        writePosition = ProtobufSerializer.WriteDouble(buffer, writePosition, value);
-                    }
+                    writePosition = ProtobufSerializer.WriteDouble(buffer, writePosition, values[i]);
                 }
             }
 
