94262f18f2c29a069db90326e6e9a3c9739b249e
Steve Gordon <sgordon@hotmail.co.uk>
2026-09-02 15:58:01 +0100

    Fix compiler error

--- src/OpenTelemetry.Extensions.Hosting/OpenTelemetryHostApplicationBuilderExtensions.cs
+++ src/OpenTelemetry.Extensions.Hosting/OpenTelemetryHostApplicationBuilderExtensions.cs
@@ -109,5 +109,4 @@ public static class OpenTelemetryHostApplicationBuilderExtensions
                     environment,
                     serviceProvider.GetService<IConfiguration>()));
     }
-
 }
